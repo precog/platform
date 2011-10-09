f6d622579a8e8c3544a346e2eab27ccae4b37709
Daniel Spiewak <daniel@reportgrid.com>
2011-10-09 15:43:38 -0500

    Added utility methods for creating atoms

--- src/test/scala/com/reportgrid/quirrel/util/AtomSpecs.scala
+++ src/test/scala/com/reportgrid/quirrel/util/AtomSpecs.scala
@@ -6,49 +6,48 @@ import org.specs2.ScalaCheck
 import org.specs2.mutable._
 
 object AtomSpecs extends Specification with ScalaCheck {
+  import Atom._
   import Prop._
   
   "reference atoms" should {
     "store and return value after update" in check { i: Int =>
-      val atom = new Atom[Int]
-      atom() = i
-      atom() mustEqual i
-      atom() mustEqual i
+      val a = atom[Int]
+      a() = i
+      a() mustEqual i
+      a() mustEqual i
     }
     
     "throw exception for unset value" in {
-      val atom = new Atom[Int]
-      atom() must throwA[RuntimeException]
+      val a = atom[Int]
+      a() must throwA[RuntimeException]
     }
     
     "throw exception for multiply-set value" in {
-      val atom = new Atom[Int]
-      atom() = 42
-      (atom() = 12) must throwA[RuntimeException]
-      atom() mustEqual 42
+      val a = atom[Int]
+      a() = 42
+      (a() = 12) must throwA[RuntimeException]
+      a() mustEqual 42
     }
     
     "self-populate once on access" in check { i: Int =>
       var count = 0
-      val atom = new Atom[Int] {
-        override def populate() = {
-          count += 1
-          this() = i
-        }
+      lazy val a: Atom[Int] = atom[Int] {
+        count += 1
+        a() = i
       }
       
-      atom() mustEqual i
-      atom() mustEqual i
+      a() mustEqual i
+      a() mustEqual i
       
       count mustEqual 1
     }
     
     "detect recursive self-population" in {
-      val atom = new Atom[Int] {
-        override def populate() = this() += 42
+      lazy val a: Atom[Int] = atom[Int] {
+        a() += 42
       }
       
-      atom() must throwA[RuntimeException]
+      a() must throwA[RuntimeException]
     }
     
     // TODO spec multi-thread behavior
@@ -56,29 +55,29 @@ object AtomSpecs extends Specification with ScalaCheck {
   
   "aggregate atoms" should {
     "store all values and return" in check { xs: Set[Int] =>
-      val atom = new SetAtom[Int]
-      xs foreach (atom +=)
+      val a = new SetAtom[Int]
+      xs foreach (a +=)
       
-      atom() mustEqual xs
-      atom() mustEqual xs
+      a() mustEqual xs
+      a() mustEqual xs
     }
     
     "concatenate values and return" in check { (i: Int, xs: Set[Int]) =>
-      val atom = new SetAtom[Int]
-      atom += i
-      atom ++= xs
+      val a = new SetAtom[Int]
+      a += i
+      a ++= xs
       
-      atom() mustEqual (xs + i)
-      atom() mustEqual (xs + i)
+      a() mustEqual (xs + i)
+      a() mustEqual (xs + i)
     }
     
     "throw exception for mutation following force" in {
-      val atom = new SetAtom[Int]
-      atom += 42
-      atom() mustEqual Set(42)
+      val a = new SetAtom[Int]
+      a += 42
+      a() mustEqual Set(42)
       
-      (atom += 12) must throwA[RuntimeException]
-      atom() mustEqual Set(42)
+      (a += 12) must throwA[RuntimeException]
+      a() mustEqual Set(42)
     }
   }
 }
