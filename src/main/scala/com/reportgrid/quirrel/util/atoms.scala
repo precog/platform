18d1c1fe572fd80dedfd6b0a9666f71e85072d31
Daniel Spiewak <daniel@reportgrid.com>
2011-10-09 15:07:45 -0500

    Implemented reference atoms

---
+++ src/main/scala/com/reportgrid/quirrel/util/atoms.scala
@@ -0,0 +1,131 @@
+package com.reportgrid.quirrel
+package util
+
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.locks.ReentrantLock
+
+trait Source[+A] {
+  def apply(): A
+}
+
+trait Sink[-A] {
+  def update(a: A)
+}
+
+class Atom[A] extends Source[A] with Sink[A] {
+  
+  @volatile
+  private var value: A = null.asInstanceOf[A]
+  
+  @volatile
+  private var isSet = false
+  
+  @volatile
+  private var setterThread: Thread = null
+  
+  private val lock = new ReentrantLock
+  private val semaphore = new AnyRef
+  
+  protected def populate() {
+    error("Cannot self-populate atom")
+  }
+  
+  def update(a: A) {
+    lock.lock()
+    try {
+      if (isSet) {
+        error("Cannot multiply-assign atom")
+      }
+      
+      value = a
+      isSet = true
+      setterThread = null
+    } finally {
+      lock.unlock()
+    }
+  }
+  
+  def apply(): A = {
+    if (isSet) {
+      value
+    } else {
+      lock.lock()
+      try {
+        if (isSet) {
+          value
+        } else if (setterThread != null) {
+          if (setterThread == Thread.currentThread) {
+            error("Recursive atom definition detected")
+          } else {
+            lock.unlock()
+            try {
+              semaphore synchronized {
+                semaphore.wait()
+              }
+            } finally {
+              lock.lock()
+            }
+            value
+          }
+        } else {
+          setterThread = Thread.currentThread
+          lock.unlock()
+          try {
+            populate()
+          } finally {
+            lock.lock()
+          }
+          value
+        }
+      } finally {
+        lock.unlock()
+      }
+    }
+    value
+  }
+}
+
+
+trait Aggregate[A, Coll[_]] extends Source[Coll[A]] {
+  private val ref = new AtomicReference(init)
+  
+  @volatile
+  private var isForced = false
+  
+  def +=(a: A)
+  def ++=(s: Coll[A])
+  
+  def apply() = {
+    isForced = true
+    ref.get
+  }
+  
+  protected final def swap(f: Coll[A] => Coll[A]) {
+    var successful = false
+    while (!isForced && !successful) {
+      val set = ref.get
+      val set2 = f(set)
+      successful = ref.compareAndSet(set, set2)
+    }
+    
+    if (isForced) {
+      error("Aggregate has already been forced; cannot subsequently extend")
+    }
+  }
+  
+  protected def init: Coll[A]
+}
+
+class SetAtom[A] extends Aggregate[A, Set] {
+  def +=(a: A) = swap { _ + a }
+  def ++=(s: Set[A]) = swap { _ ++ s }
+  
+  protected def init = Set[A]()
+}
+
+class MapAtom[A, B] extends Aggregate[(A, B), ({ type L[_] = Map[A, B] })#L] {
+  def +=(pair: (A, B)) = swap { _ + pair }
+  def ++=(m: Map[A, B]) = swap { _ ++ m }
+  
+  protected def init = Map[A, B]()
+}
