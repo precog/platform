011bf3e5497e544eedaeb392390d994a3ce67a87
Daniel Spiewak <daniel@reportgrid.com>
2011-10-11 11:55:17 -0500

    Implemented simple REPL to front the parser

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 scalaVersion := "2.9.1"
 
 libraryDependencies ++= Seq(
+  "jline" % "jline" % "0.9.9",
   "edu.uwm.cs" %% "gll-combinators" % "1.5-SNAPSHOT",
   "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test" withSources,
   "org.specs2" %% "specs2" % "1.5" % "test" withSources)
---
+++ src/main/scala/com/reportgrid/quirrel/REPL.scala
@@ -0,0 +1,52 @@
+package com.reportgrid.quirrel
+
+import edu.uwm.cs.gll.LineStream
+
+import jline.ConsoleReader
+import jline.Terminal
+
+import parser._
+
+trait REPL {
+  val Prompt = "quirrel> "
+  val Follow = "       | "
+  
+  def parse(str: String) = {
+    val parser = new Parser {
+      def input = LineStream(str)
+    }
+    parser.root
+  }
+  
+  def run() {
+    Terminal.setupTerminal().initializeTerminal()
+    
+    val reader = new ConsoleReader
+    
+    var next = readNext(reader)
+    while (!next.trim.startsWith(":q")) {
+      try {
+        parse(next)
+      } catch {
+        case e @ ParseException(failures) => 
+          println(e.mkString)
+      }
+      next = readNext(reader)
+    }
+  }
+  
+  def readNext(reader: ConsoleReader) = {
+    var input = reader.readLine(Prompt)
+    var line = reader.readLine(Follow)
+    while (line.trim != "") {
+      input += '\n' + line
+      line = reader.readLine(Follow)
+    }
+    input
+  }
+}
+
+object Console extends App {
+  val repl = new REPL {}
+  repl.run()
+}
