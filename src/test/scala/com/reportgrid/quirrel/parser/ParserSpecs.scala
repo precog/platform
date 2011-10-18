8371652d9ff0dbe20f9699e4e7a0413bc5d50e81
Daniel Spiewak <daniel@reportgrid.com>
2011-10-18 16:56:16 -0600

    Removed deceptive and dangerous Tree type alias

--- src/test/scala/com/reportgrid/quirrel/parser/ParserSpecs.scala
+++ src/test/scala/com/reportgrid/quirrel/parser/ParserSpecs.scala
@@ -807,5 +807,5 @@ object ParserSpecs extends Specification with ScalaCheck with Parser with StubPa
     }
   }
   
-  def parse(str: String): Tree = parse(LineStream(str))
+  def parse(str: String): Expr = parse(LineStream(str))
 }
