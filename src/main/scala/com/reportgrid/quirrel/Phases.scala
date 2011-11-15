05f1a7a2ac1992ab07201eb7954fe956a7de6977
Daniel Spiewak <daniel@reportgrid.com>
2011-11-15 13:10:34 -0600

    Extracted some of the boiler-plate associated with attributes into a common util function

--- src/main/scala/com/reportgrid/quirrel/Phases.scala
+++ src/main/scala/com/reportgrid/quirrel/Phases.scala
@@ -4,7 +4,7 @@ trait Phases {
   type Expr
   type Error
   
-  private type Phase = Expr => Set[Error]
+  type Phase = Expr => Set[Error]
   
   private val Phases: List[Phase] =
     bindNames _ :: checkProvenance _ :: Nil
