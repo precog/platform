189a6cede83788f98bf691d8b3d22ffe207be19c
Derek Chen-Becker <derek@reportgrid.com>
2011-12-13 16:51:54 -0700

    Updated build to reference our Nexus instance

--- project/Build.scala
+++ project/Build.scala
@@ -21,12 +21,18 @@ import sbt._
 import Keys._
 
 object PlatformBuild extends Build {
+  val nexusSettings = Seq(
+    resolvers ++= Seq("ReportGrid repo" at            "http://devci01.reportgrid.com:8081/content/repositories/releases",
+                      "ReportGrid snapshot repo" at   "http://devci01.reportgrid.com:8081/content/repositories/snapshots"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
+  )
+
   lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel, storage, bytecode)
   
-  lazy val bytecode = Project(id = "bytecode", base = file("bytecode"))
-  lazy val quirrel = Project(id = "quirrel", base = file("quirrel")) dependsOn bytecode
-  lazy val storage = Project(id = "storage", base = file("storage"))
+  lazy val bytecode = Project(id = "bytecode", base = file("bytecode")).settings(nexusSettings : _*)
+  lazy val quirrel = Project(id = "quirrel", base = file("quirrel")).settings(nexusSettings : _*) dependsOn bytecode
+  lazy val storage = Project(id = "storage", base = file("storage")).settings(nexusSettings : _*)
   
-  lazy val daze = Project(id = "daze", base = file("daze")) dependsOn bytecode // (bytecode, storage)
+  lazy val daze = Project(id = "daze", base = file("daze")).settings(nexusSettings : _*) dependsOn bytecode // (bytecode, storage)
 }
 
