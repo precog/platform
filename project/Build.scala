408d40a10955b4aee337638985df026d0d7e9fd5
Derek Chen-Becker <github@chen-becker.org>
2011-12-13 10:30:52 -0700

    Added storage module into master build

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,8 @@ import sbt._
 import Keys._
 
 object PlatformBuild extends Build {
-  lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel)
+  lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel, storage)
   lazy val quirrel = Project(id = "quirrel", base = file("quirrel"))
+  lazy val storage = Project(id = "storage", base = file("storage/leveldbstore"))
 }
 
