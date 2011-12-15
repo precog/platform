b9d6d59fd0511a7a94438ac5a8c339fdfb2c22bb
John A. De Goes <john@degoes.net>
2011-12-15 12:11:58 -0700

    add match cases for emitter

--- quirrel/src/main/scala/com/querio/quirrel/emitter/Emitter.scala
+++ quirrel/src/main/scala/com/querio/quirrel/emitter/Emitter.scala
@@ -22,10 +22,66 @@ package com.querio.quirrel.emitter
 import com.querio.quirrel.parser.AST
 import com.querio.bytecode.{Instructions}
 
-trait Emitter extends AST {
+trait Emitter extends AST with Instructions {
+  import instructions._
 
-  /*def emit(expr: Expr): Vector[Instruction] = {
+  def emit(expr: Expr): Vector[Instruction] = {
+    expr match {
+      case ast.New(loc, child) => 
+      
+      case ast.Relate(loc, from: Expr, to: Expr, in: Expr) => 
+      
+      case t @ ast.TicVar(loc, id) => 
+      
+      case ast.StrLit(loc, value) => 
+      
+      case ast.NumLit(loc, value) => 
+      
+      case ast.BoolLit(loc, value) => 
+      
+      case ast.ObjectDef(loc, props) => 
+      
+      case ast.ArrayDef(loc, values) => 
+      
+      case ast.Descent(loc, child, property) => 
+      
+      case ast.Deref(loc, left, right) => 
+      
+      case d @ ast.Dispatch(loc, name, actuals) => 
+      
+      case ast.Operation(loc, left, op, right) => 
+      
+      case ast.Add(loc, left, right) => 
+      
+      case ast.Sub(loc, left, right) => 
+
+      case ast.Mul(loc, left, right) => 
+      
+      case ast.Div(loc, left, right) => 
+      
+      case ast.Lt(loc, left, right) => 
+      
+      case ast.LtEq(loc, left, right) => 
+      
+      case ast.Gt(loc, left, right) => 
+      
+      case ast.GtEq(loc, left, right) => 
+      
+      case ast.Eq(loc, left, right) => 
+      
+      case ast.NotEq(loc, left, right) => 
+      
+      case ast.Or(loc, left, right) => 
+      
+      case ast.And(loc, left, right) =>
+      
+      case ast.Comp(loc, child) =>
+      
+      case ast.Neg(loc, child) => 
+      
+      case ast.Paren(loc, child) => 
+    }
 
     null
-  }*/
+  }
 }
