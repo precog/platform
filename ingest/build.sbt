dc7470aa4fa991edf50ac6f0ba329f73a9553ce9
Nick Matheson <nick@reportgrid.com>
2011-12-18 09:48:14 -0700

    Squashed commit of the following:
    commit 4e2d480e0d001536a9cf8761ef26e4edbe94dc6b
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Sun Dec 18 09:47:15 2011 -0700
    
        Removed swp file and added *.swp to .gitignore
    
    commit 402a24186842655e5eb5eed40255a3668b521b21
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Sun Dec 18 09:44:43 2011 -0700
    
        Moved example functionality to simple unit test
    
    commit b33775d6b2f88c7b699df90698ba4b50be3d2251
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Sun Dec 18 09:06:51 2011 -0700
    
        Added initial ingest api classes/generators and made requested build fixes

---
+++ ingest/build.sbt
@@ -0,0 +1,29 @@
+/*
+ *  ____    ____    _____    ____    ___     ____ 
+ * |  _ \  |  _ \  | ____|  / ___|  / _/    / ___|        Precog (R)
+ * | |_) | | |_) | |  _|   | |     | |  /| | |  _         Advanced Analytics Engine for NoSQL Data
+ * |  __/  |  _ <  | |___  | |___  |/ _| | | |_| |        Copyright (C) 2010 - 2013 SlamData, Inc.
+ * |_|     |_| \_\ |_____|  \____|   /__/   \____|        All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 
+ * 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+ * the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this 
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+libraryDependencies ++= Seq(
+      "se.scalablesolutions.akka" % "akka-actor"          % "1.2",
+      "se.scalablesolutions.akka" % "akka-typed-actor"    % "1.2",
+      "org.apache"                %% "kafka-core"         % "0.7.5",
+      "joda-time"                 % "joda-time"           % "1.6.2",
+      "org.scalaz"                %% "scalaz-core"        % "6.0.2",
+      "ch.qos.logback"            % "logback-classic"     % "1.0.0",
+      "org.scala-tools.testing"   %% "scalacheck"         % "1.9",
+      "org.specs2"                %% "specs2"             % "1.7-SNAPSHOT"  % "test"
+)
---
+++ ingest/src/main/scala/com/querio/ingest/api/Consumer.scala
@@ -0,0 +1,212 @@
+/*
+ *  ____    ____    _____    ____    ___     ____ 
+ * |  _ \  |  _ \  | ____|  / ___|  / _/    / ___|        Precog (R)
+ * | |_) | | |_) | |  _|   | |     | |  /| | |  _         Advanced Analytics Engine for NoSQL Data
+ * |  __/  |  _ <  | |___  | |___  |/ _| | | |_| |        Copyright (C) 2010 - 2013 SlamData, Inc.
+ * |_|     |_| \_\ |_____|  \____|   /__/   \____|        All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 
+ * 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+ * the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this 
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.querio.ingest.api
+
+import blueeyes.json.JPath
+import blueeyes.json.JsonAST._
+import blueeyes.json.Printer
+
+import blueeyes.json.xschema.{ ValidatedExtraction, Extractor, Decomposer }
+import blueeyes.json.xschema.Extractor._
+import blueeyes.json.xschema.DefaultSerialization._
+import blueeyes.json.xschema.Extractor._
+
+import scalaz._
+import Scalaz._
+import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicInteger
+import java.nio.charset.Charset
+import java.nio.charset.CharsetDecoder
+import java.nio.charset.CharsetEncoder
+import blueeyes.json.JsonParser
+
+trait EventReceivers {
+  def find(address: MailboxAddress): List[EventReceiver]
+}
+
+trait EventReceiver {
+  def get(): Event
+}
+
+case class MailboxAddress(id: Long)
+
+case class Event(path: String, token: String, content: JValue)
+
+class EventSerialization {
+  implicit val EventDecomposer: Decomposer[Event] = new Decomposer[Event] {
+    override def decompose(event: Event): JValue = JObject(
+      List(
+        JField("path", event.path.serialize),
+        JField("token", event.token.serialize),
+        JField("content", event.content.serialize)))
+  }
+
+  implicit val EventExtractor: Extractor[Event] = new Extractor[Event] with ValidatedExtraction[Event] {
+    override def validated(obj: JValue): Validation[Error, Event] =
+      ((obj \ "path").validated[String] |@|
+        (obj \ "token").validated[String] |@|
+        (obj \ "content").validated[JValue]).apply(Event(_, _, _))
+  }  
+}
+
+object Event extends EventSerialization
+
+abstract class IngestMessage
+
+class IngestMessageSerialization {
+  implicit val IngestMessageDecomposer: Decomposer[IngestMessage] = new Decomposer[IngestMessage] {
+    override def decompose(ingestMessage: IngestMessage): JValue = ingestMessage match {
+      case sm @ SyncMessage(_, _, _)  => SyncMessage.SyncMessageDecomposer.apply(sm)
+      case em @ EventMessage(_, _, _) => EventMessage.EventMessageDecomposer.apply(em)
+    }
+  }
+}
+
+object IngestMessage extends IngestMessageSerialization
+
+case class SyncMessage(producerId: Int, syncId: Int, eventIds: List[Int])  extends IngestMessage
+
+class SyncMessages(producerId: Int, initialId: Int = 1) {
+  
+  private val nextId = new AtomicInteger(initialId)
+  
+  val start: SyncMessage = SyncMessage(producerId, 0, List.empty)
+  def next(eventIds: List[Int]): SyncMessage = SyncMessage(producerId, nextId.getAndIncrement(), eventIds)
+  def stop(eventIds: List[Int] = List.empty) = SyncMessage(producerId, Int.MaxValue, eventIds)
+}
+
+object SyncMessage extends SyncMessageSerialization {
+  val start = SyncMessage(_: Int, 0, List.empty)
+  def finish = SyncMessage(_: Int, Int.MaxValue, _: List[Int])
+}
+
+trait SyncMessageSerialization {
+  implicit val SyncMessageDecomposer: Decomposer[SyncMessage] = new Decomposer[SyncMessage] {
+    override def decompose(eventMessage: SyncMessage): JValue = JObject(
+      List(
+        JField("producerId", eventMessage.producerId.serialize),
+        JField("syncId", eventMessage.syncId.serialize),
+        JField("eventIds", eventMessage.eventIds.serialize)))
+  }
+
+  implicit val SyncMessageExtractor: Extractor[SyncMessage] = new Extractor[SyncMessage] with ValidatedExtraction[SyncMessage] {
+    override def validated(obj: JValue): Validation[Error, SyncMessage] =
+      ((obj \ "producerId").validated[Int] |@|
+        (obj \ "syncId").validated[Int] |@|
+        (obj \ "eventIds").validated[List[Int]]).apply(SyncMessage(_, _, _))
+  }  
+}
+
+case class EventMessage(producerId: Int, eventId: Int, event: Event) extends IngestMessage
+
+trait EventMessageSerialization {
+  implicit val EventMessageDecomposer: Decomposer[EventMessage] = new Decomposer[EventMessage] {
+    override def decompose(eventMessage: EventMessage): JValue = JObject(
+      List(
+        JField("producerId", eventMessage.producerId.serialize),
+        JField("eventId", eventMessage.eventId.serialize),
+        JField("event", eventMessage.event.serialize)))
+  }
+
+  implicit val EventMessageExtractor: Extractor[EventMessage] = new Extractor[EventMessage] with ValidatedExtraction[EventMessage] {
+    override def validated(obj: JValue): Validation[Error, EventMessage] =
+      ((obj \ "producerId").validated[Int] |@|
+        (obj \ "eventId").validated[Int] |@|
+        (obj \ "event").validated[Event]).apply(EventMessage(_, _, _))
+  }
+}
+
+object EventMessage extends EventMessageSerialization
+
+object IngestMessageSerialization {
+  
+  private val stopByte: Byte = 0x00
+  private val jsonEventFlag: Byte = 0x01
+  private val jsonSyncFlag: Byte = 0x02
+  private val magicByte: Byte = -123
+  
+  def write(buffer: ByteBuffer, msg: IngestMessage) {
+    (msg match {
+      case SyncMessage(_, _, _)     => writeSync _
+      case EventMessage(_, _, _)    => writeEvent _
+    })(msg)(buffer)
+  }
+  
+  def writeSync(msg: IngestMessage): ByteBuffer => ByteBuffer = (writeHeader(_: ByteBuffer, jsonSyncFlag)) andThen 
+                                                                (writeMessage(_: ByteBuffer, msg))
+
+  def writeEvent(msg: IngestMessage): ByteBuffer => ByteBuffer = (writeHeader(_: ByteBuffer, jsonEventFlag)) andThen 
+                                                                 (writeMessage(_: ByteBuffer, msg))
+  
+  def writeHeader(buffer: ByteBuffer, encodingFlag: Byte): ByteBuffer = {
+    buffer.put(magicByte)
+    buffer.put(encodingFlag)
+    buffer.put(stopByte)
+    buffer
+  }
+
+  val charset = Charset.forName("UTF-8")
+  
+  def writeMessage(buffer: ByteBuffer, msg: IngestMessage): ByteBuffer = {
+    val msgBuffer = charset.encode(Printer.compact(Printer.render(msg.serialize)))
+    buffer.put(msgBuffer)
+    buffer
+  }
+  
+  def readMessage(buffer: ByteBuffer): Validation[String, IngestMessage] = {
+    val magic = buffer.get()
+    if(magic != magicByte) {
+      failure("Invaild message bad magic byte.")
+    } else {
+      val msgType = buffer.get()
+      val stop    = buffer.get()
+      
+      (stop, msgType) match {
+        case (stop, flag) if stop == stopByte && flag == jsonEventFlag => parseEvent(buffer)
+        case (stop, flag) if stop == stopByte && flag == jsonSyncFlag  => parseSync(buffer)
+        case _                                                         => failure("Unrecognized message type")
+      }
+    }
+  }
+  
+  def parseSync = (parseJValue _) andThen (jvalueToSync _)
+  
+  def parseEvent = (parseJValue _) andThen (jvalueToEvent _)
+  
+  def jvalueToSync(jvalue: JValue): Validation[String, IngestMessage] = {
+    jvalue.validated[SyncMessage] match {
+      case Failure(e)  => Failure(e.message)
+      case Success(sm) => Success(sm)
+    }
+  }
+  
+  def jvalueToEvent(jvalue: JValue): Validation[String, IngestMessage] = {
+    jvalue.validated[EventMessage] match {
+      case Failure(e)  => Failure(e.message)
+      case Success(em) => Success(em)
+    }
+  }
+  
+  def parseJValue(buffer: ByteBuffer): JValue = {
+    val decoder = charset.newDecoder()
+    val charBuffer = decoder.decode(buffer)
+    JsonParser.parse(charBuffer.toString())
+  }
+}
