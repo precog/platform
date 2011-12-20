45295483451cae20783d9c5851f00c1c8d9c44cb
Nick Matheson <nick@reportgrid.com>
2011-12-20 13:09:42 -0700

    Squashed commit of the following:
    commit f00d31bd522f5fc01c69db56c1ea3d9546aa25fd
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Tue Dec 20 13:08:31 2011 -0700
    
        Added configuration to webapp and updated dev and prod configs
    
    commit c888395fbdcb8b4ff5354312a467b0998a4fb1d1
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Tue Dec 20 13:00:08 2011 -0700
    
        Updated test ingest producers to work from config file.
    
    commit 0b88bfca7f1269c0cb14fdd055369d177d1d120d
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Tue Dec 20 12:27:16 2011 -0700
    
        Added ability to start from arbitrary event id
    
    commit 15998a78ecc25d7debac1bdb07264a0d875f5c1c
    Author: Nick Matheson <nick@reportgrid.com>
    Date:   Tue Dec 20 12:26:44 2011 -0700
    
        Added close logic to MessageSender trait and implementations

--- ingest/src/main/scala/com/querio/ingest/util/IngestProducer.scala
+++ ingest/src/main/scala/com/querio/ingest/util/IngestProducer.scala
@@ -3,6 +3,7 @@ package com.querio.ingest.util
 import scala.collection.mutable.ListBuffer
 
 import java.util.Properties
+import java.io._
 
 import com.querio.ingest.api._
 import com.querio.ingest.service._
@@ -17,12 +18,15 @@ import blueeyes.core.http.HttpResponse
 import blueeyes.core.service.HttpClient
 import blueeyes.core.service.engines.HttpClientXLightWeb
 
-abstract class IngestProducer extends RealisticIngestMessage {
-  
-  def main(args: Array[String]) {
-    val messages = if(args.length >= 1) args(0).toInt else 1000
-    val delay = if(args.length >= 2) args(1).toInt else 100
-    val threadCount = if(args.length >= 3) args(2).toInt else 1
+abstract class IngestProducer(args: Array[String]) extends RealisticIngestMessage {
+
+  lazy val config = loadConfig(args)
+
+  lazy val messages = config.getProperty("messages", "1000").toInt
+  lazy val delay = config.getProperty("delay", "100").toInt
+  lazy val threadCount = config.getProperty("threads", "1").toInt
+
+  def run() {
 
     val start = System.nanoTime
 
@@ -43,15 +47,48 @@ abstract class IngestProducer extends RealisticIngestMessage {
     println("Time: %.02f".format((System.nanoTime - start) / 1000000000.0))
   }
 
+  def loadConfig(args: Array[String]): Properties = {
+    if(args.length != 1) usage() 
+    
+    val config = new Properties()
+    val file = new File(args(0))
+    
+    if(!file.exists) usage() 
+    
+    config.load(new FileReader(file))
+    config
+  }
+
+  def usage() {
+    println(usageMessage)
+    sys.exit(1)
+  }
+
+  def usageMessage = 
+    """
+Usage: command {properties file}
+
+Properites:
+messages - number of messages to produce (default: 1000)
+delay - delay between messages (<0 indicates no delay) (default: 100)
+threads - number of producer threads (default: 1)
+    """
+  
   def send(event: Event): Unit
 }
 
-object WebappIngestProducer extends IngestProducer {
+object WebappIngestProducer {
+  def main(args: Array[String]) =  new WebappIngestProducer(args).run()
+}
 
-  val base = "http://localhost:30050/vfs/"
+class WebappIngestProducer(args: Array[String]) extends IngestProducer(args) {
+
+
+  lazy val base = config.getProperty("serviceUrl", "http://localhost:30050/vfs/")
   val client = new HttpClientXLightWeb 
 
   def send(event: Event) {
+
     val f: Future[HttpResponse[JValue]] = client.path(base)
                                                 .query("tokenId", event.tokens(0))
                                                 .contentType(application/json)
@@ -61,12 +98,20 @@ object WebappIngestProducer extends IngestProducer {
       println("Error tracking data: " + f.error)
     }
   }
-  
+
+  override def usageMessage = super.usageMessage + """
+serviceUrl - base url for web application (default: http://localhost:30050/vfs/)
+  """
 }
 
-object DirectIngestProducer extends IngestProducer {
- 
-  val testTopic = "test-topic-0"
+object DirectIngestProducer {
+  def main(args: Array[String]) = new WebappIngestProducer(args).run()
+}
+
+class DirectIngestProducer(args: Array[String]) extends IngestProducer(args) {
+
+  lazy val testTopic = config.getProperty("topicId", "test-topic-0")
+  lazy val zookeeperHosts = config.getProperty("zookeeperHosts", "127.0.0.1:2181")
   lazy val store = kafkaStore(testTopic)
 
   def send(event: Event) {
@@ -75,7 +120,7 @@ object DirectIngestProducer extends IngestProducer {
 
   def kafkaStore(topic: String): EventStore = {
     val props = new Properties()
-    props.put("zk.connect", "127.0.0.1:2181")
+    props.put("zk.connect", zookeeperHosts) 
     props.put("serializer.class", "com.querio.ingest.api.IngestMessageCodec")
     
     val messageSenderMap = Map() + (MailboxAddress(0L) -> new KafkaMessageSender(topic, props))
@@ -84,11 +129,16 @@ object DirectIngestProducer extends IngestProducer {
 
    
 
-    val qz = QuerioZookeeper.testQuerioZookeeper("127.0.0.1:2181")
+    val qz = QuerioZookeeper.testQuerioZookeeper(zookeeperHosts)
     val producerId = qz.acquireProducerId
     qz.close
     new DefaultEventStore(producerId,
                           new ConstantEventRouter(defaultAddresses),
                           new MappedMessageSenders(messageSenderMap))
   }
+  
+  override def usageMessage = super.usageMessage + """
+topicId - kafka topic to publish events to (default: test-topic-0 )
+zookeeperHosts - comma delimeted list of zookeeper hosts (default: 127.0.0.1:2181)
+  """
 }
