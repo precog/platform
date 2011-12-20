41da8571f49fcb98535a3ee0694f29e05ffa5d7d
Nick Matheson <nick@reportgrid.com>
2011-12-20 14:05:59 -0700

    Added configuration to IngestConsumer and minor code cleanup

--- ingest/src/main/scala/com/querio/ingest/util/IngestProducer.scala
+++ ingest/src/main/scala/com/querio/ingest/util/IngestProducer.scala
@@ -22,7 +22,7 @@ package com.querio.ingest.util
 import scala.collection.mutable.ListBuffer
 
 import java.util.Properties
-import java.io._
+import java.io.{File, FileReader}
 
 import com.querio.ingest.api._
 import com.querio.ingest.service._
@@ -63,6 +63,8 @@ abstract class IngestProducer(args: Array[String]) extends RealisticIngestMessag
     threads.foreach(_.start)
     threads.foreach(_.join)
 
+    close
+
     println("Time: %.02f".format((System.nanoTime - start) / 1000000000.0))
   }
 
@@ -94,6 +96,7 @@ threads - number of producer threads (default: 1)
     """
   
   def send(event: Event): Unit
+  def close(): Unit = ()
 }
 
 object WebappIngestProducer {
@@ -124,31 +127,37 @@ serviceUrl - base url for web application (default: http://localhost:30050/vfs/)
 }
 
 object DirectIngestProducer {
-  def main(args: Array[String]) = new WebappIngestProducer(args).run()
+  def main(args: Array[String]) = new DirectIngestProducer(args).run()
 }
 
 class DirectIngestProducer(args: Array[String]) extends IngestProducer(args) {
 
-  lazy val testTopic = config.getProperty("topicId", "test-topic-0")
+  lazy val testTopic = config.getProperty("topicId", "test-topic-1")
   lazy val zookeeperHosts = config.getProperty("zookeeperHosts", "127.0.0.1:2181")
   lazy val store = kafkaStore(testTopic)
+  var sender: MessageSender = null
 
   def send(event: Event) {
     store.save(genEvent.sample.get)
   }
 
+
+
   def kafkaStore(topic: String): EventStore = {
     val props = new Properties()
     props.put("zk.connect", zookeeperHosts) 
     props.put("serializer.class", "com.querio.ingest.api.IngestMessageCodec")
-    
-    val messageSenderMap = Map() + (MailboxAddress(0L) -> new KafkaMessageSender(topic, props))
+  
+    sender = new KafkaMessageSender(topic, props)
+
+    val messageSenderMap = Map() + (MailboxAddress(0L) -> sender)
     
     val defaultAddresses = List(MailboxAddress(0))
 
    
 
     val qz = QuerioZookeeper.testQuerioZookeeper(zookeeperHosts)
+    qz.setup
     val producerId = qz.acquireProducerId
     qz.close
     new DefaultEventStore(producerId,
@@ -157,7 +166,11 @@ class DirectIngestProducer(args: Array[String]) extends IngestProducer(args) {
   }
   
   override def usageMessage = super.usageMessage + """
-topicId - kafka topic to publish events to (default: test-topic-0 )
+topicId - kafka topic to publish events to (default: test-topic-1 )
 zookeeperHosts - comma delimeted list of zookeeper hosts (default: 127.0.0.1:2181)
   """
+
+  override def close() {
+    sender.close.await
+  }
 }
