ae4c0ff306bd4e37a37d09bb931f0ad0fa680f08
Derek Chen-Becker <github@chen-becker.org>
2011-12-20 22:34:45 -0700

    Fixed column spec and spec run script
    Also added a logback test config to constrain log output to a file

--- storage/src/test/scala/com/reportgrid/storage/leveldb/ColumnSpec.scala
+++ storage/src/test/scala/com/reportgrid/storage/leveldb/ColumnSpec.scala
@@ -1,6 +1,7 @@
 package com.reportgrid.storage.leveldb
 
 import java.io.File
+import java.nio.ByteBuffer
 
 import org.scalacheck.{Arbitrary,Gen}
 import org.specs2.ScalaCheck
@@ -8,6 +9,11 @@ import org.specs2.matcher.ThrownMessages
 import org.specs2.mutable.{BeforeAfter,Specification}
 import org.specs2.specification.Scope
 import Bijection._
+
+import scalaz.syntax.traverse._ 
+import scalaz.std.list._ 
+import scalaz.effect.IO 
+import scalaz.iteratee.Iteratee._
 
 import com.weiglewilczek.slf4s.Logging
 
@@ -46,15 +52,49 @@ class ColumnSpec extends Specification with ScalaCheck with ThrownMessages with 
       initial.map(_.close.unsafePerformIO).flatMap(_ => LevelDBProjection(dataDir)).isSuccess must_== true
     }
 
+    "Properly persist and restore values" in new columnSetup {
+      val size = 1000
+      val seed = 42l
+      val db = LevelDBProjection(dataDir, Some(ProjectionComparator.Long))
+      db.isSuccess must_== true
+      db.map { c =>
+        // Setup our PRNG
+        val r = new java.util.Random(seed)
+    
+        // Create a set of values to insert based on the size
+        val values = Array.fill(size)(r.nextLong)
+    
+        logger.info("Inserting %d values".format(size))
+    
+        // Insert values, syncing every 10%
+        val inserts = values.grouped(size / 10).toList.map { a =>
+          val (first, last) = a.splitAt(a.length - 1)
+          for { 
+            _ <- first.toList.map(v => c.insert(v, ByteBuffer.wrap(v.as[Array[Byte]]))).sequence[IO, Unit]
+            _ <- IO { logger.info("  Syncing insert")} 
+            _ <- last.toList.map(v => c.insert(v, ByteBuffer.wrap(v.as[Array[Byte]]), true)).sequence[IO, Unit]
+          } yield ()
+        }
 
-//    "Properly persist ID lists" in {
-//      check { (v : BigDecimal, ids : List[Long]) =>
-//        ids.foreach(c.insert(_, v.underlying))
-//        
-//        val resultIds = c.getIds(v.underlying)
-//
-//        resultIds.size must_== ids.size
-//      }
-//    }
+        def report(vals: List[Long]) = IO {
+          logger.info("Read " + vals.size + " distinct values")
+      
+          vals.forall {
+            v => values.contains(v)
+          } must_== true
+        }
+
+        val runTest = for {
+          _       <- inserts.sequence[IO, Unit]
+          allVals <- (fold[Unit, Long, IO, List[Long]](Nil)((a, e) => e :: a) >>== c.getAllIds) apply (_ => IO(Nil))
+          _       <- report(allVals)
+          _       <- c.close
+        } yield {
+          logger.info("Test complete, shutting down")
+        }
+
+        runTest.unsafePerformIO
+      }
+    }
   }
 }
