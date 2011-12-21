b1a8ad921a3cc8f9dc7e88f787e906dec1c3e2aa
Derek Chen-Becker <github@chen-becker.org>
2011-12-20 22:34:45 -0700

    Fixed column spec and spec run script
    Also added a logback test config to constrain log output to a file

--- scripts/run_storage_spec.sh
+++ scripts/run_storage_spec.sh
@@ -19,15 +19,10 @@
 ## 
 #!/bin/bash
 
-cd `dirname $0`/..
+cd `dirname $0`/../storage
 
-WORKDIR=/tmp/foo
-CLASSPATH=storage/target/leveldb-assembly-0.0.1-SNAPSHOT.jar:storage/target/scala-2.9.1/test-classes
+CLASSPATH=target/leveldb-assembly-0.0.1-SNAPSHOT.jar:target/scala-2.9.1/test-classes:$HOME/.ivy2/cache/org.specs2/specs2-scalaz-core_2.9.1/jars/specs2-scalaz-core_2.9.1-6.0.1.jar:$HOME/.ivy2/cache/org.specs2/specs2_2.9.1/jars/specs2_2.9.1-1.7-SNAPSHOT.jar
 
-rm -rf $WORKDIR
-mkdir $WORKDIR
-java -cp $CLASSPATH com.reportgrid.storage.leveldb.ReadTest $WORKDIR 1000 42
-
-for spec in "$@"; do
-  java -cp $CLASSPATH:/home/software/ivyrepo/cache/org.specs2/specs2-scalaz-core_2.9.1/jars/specs2-scalaz-core_2.9.1-6.0.1.jar:/home/software/ivyrepo/cache/org.specs2/specs2_2.9.1/jars/specs2_2.9.1-1.7-SNAPSHOT.jar specs2.run com.reportgrid.${spec}
+for spec in leveldb.ColumnSpec ; do
+  java -cp $CLASSPATH specs2.run com.reportgrid.storage.${spec}
 done
