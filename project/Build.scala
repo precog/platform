8885dca09c50393a9983d3ddbf1d2c36223a526e
Nick Matheson <nick@reportgrid.com>
2012-01-12 12:21:46 -0700

    Fixed unit test build errors and removed ingest from build until compile errors resolved.

--- project/Build.scala
+++ project/Build.scala
@@ -49,7 +49,7 @@ object PlatformBuild extends Build {
     credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
   )
 
-  lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel, storage, bytecode, daze, ingest)
+  lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel, storage, bytecode, daze) //, ingest)
   
   lazy val bytecode = Project(id = "bytecode", base = file("bytecode")).settings(nexusSettings: _*) dependsOn util
   lazy val quirrel = Project(id = "quirrel", base = file("quirrel")).settings(nexusSettings: _*) dependsOn (bytecode, util)
