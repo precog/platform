01948f508f67ca6a9dfddf869b7cf84f6082b0de
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2012-01-15 11:54:46 -0700

    Dumping condeps. After a couple of hours of fighting with builds, it's just not worth it.

--- project/Build.scala
+++ project/Build.scala
@@ -23,16 +23,6 @@ import sbtassembly.Plugin.AssemblyKeys._
 import sbt.NameFilter._
 
 object PlatformBuild extends Build {
-  val scalaz = com.samskivert.condep.Depends(
-    ("scalaz", "core",     "org.scalaz"                  %% "scalaz-core"      % "7.0-SNAPSHOT" changing()),
-    ("scalaz", "effect",   "org.scalaz"                  %% "scalaz-effect"    % "7.0-SNAPSHOT" changing()),
-    ("scalaz", "iteratee", "org.scalaz"                  %% "scalaz-iteratee"  % "7.0-SNAPSHOT" changing())
-  )
-
-  val blueeyesDeps = com.samskivert.condep.Depends( 
-    ("blueeyes",      "mongo", "com.reportgrid"  %% "blueeyes-mongo"   % "0.6.0-SNAPSHOT" changing())
-  )
-
   val clientLibDeps = com.samskivert.condep.Depends(
     ("client-libraries", null, "com.reportgrid"                  %% "scala-client" % "0.3.1")
   )
@@ -45,6 +35,7 @@ object PlatformBuild extends Build {
                       "Typesafe Repository"               at "http://repo.typesafe.com/typesafe/releases/",
                       "Scala Tools"                       at "http://scala-tools.org/repo-releases/",
                       "Scala-Tools Snapshots"             at "http://scala-tools.org/repo-snapshots/",
+                      "Maven Repo 1"                      at "http://repo1.maven.org/maven2/",
                       "Guiceyfruit"                       at "http://guiceyfruit.googlecode.com/svn/repo/releases/"),
 
     credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
@@ -52,20 +43,17 @@ object PlatformBuild extends Build {
 
   lazy val platform = Project(id = "platform", base = file(".")) aggregate(quirrel, storage, bytecode, daze, ingest)
   
-  lazy val bytecode = Project(id = "bytecode", base = file("bytecode")).settings(nexusSettings: _*) dependsOn util
-  lazy val quirrel = Project(id = "quirrel", base = file("quirrel")).settings(nexusSettings: _*) dependsOn (bytecode, util)
-  
-  lazy val daze = Project(id = "daze", base = file("daze")).settings(nexusSettings : _*) dependsOn bytecode // (bytecode, storage)
-  
-  val commonSettings = nexusSettings ++ Seq(libraryDependencies ++= blueeyesDeps.libDeps)
-  lazy val common = blueeyesDeps.addDeps(Project(id = "common", base = file("common")).settings(commonSettings: _*))
+  lazy val common   = Project(id = "common", base = file("common")).settings(nexusSettings : _*)
+  lazy val util     = Project(id = "util", base = file("util")).settings(nexusSettings: _*)
 
-  val storageSettings = nexusSettings ++ Seq(libraryDependencies ++= scalaz.libDeps)
-  lazy val storage = scalaz.addDeps(Project(id = "storage", base = file("storage")).settings(storageSettings : _*).dependsOn(common))
+  lazy val bytecode = Project(id = "bytecode", base = file("bytecode")).settings(nexusSettings: _*) dependsOn util
+  lazy val quirrel  = Project(id = "quirrel", base = file("quirrel")).settings(nexusSettings: _*) dependsOn (bytecode, util)
+  
+  lazy val daze     = Project(id = "daze", base = file("daze")).settings(nexusSettings : _*) dependsOn bytecode // (bytecode, storage)
+  
+  lazy val storage  = Project(id = "storage", base = file("storage")).settings(nexusSettings : _*).dependsOn(common)
   
   val ingestSettings = sbtassembly.Plugin.assemblySettings ++ nexusSettings ++ Seq(libraryDependencies ++= clientLibDeps.libDeps)
-  lazy val ingest = clientLibDeps.addDeps(Project(id = "ingest", base = file("ingest")).settings(ingestSettings: _*).dependsOn(common))
-  
-  lazy val util = Project(id = "util", base = file("util")).settings(nexusSettings: _*)
+  lazy val ingest   = Project(id = "ingest", base = file("ingest")).settings(ingestSettings: _*).dependsOn(common)
 }
 
