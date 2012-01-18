3f967284df21bcbb9fd9fe86e7736cad1e07e7d7
Kris Nuttycombe <kris@reportgrid.com>
2012-01-18 23:14:10 +0000

    Updates to daze api.

--- daze/src/main/scala/com/reportgrid/daze/API.scala
+++ daze/src/main/scala/com/reportgrid/daze/API.scala
@@ -17,52 +17,43 @@
  * program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package com.reportgrid.daze
+package com.querio.daze
 
-import java.nio.ByteBuffer
-import scalaz._
+case class IdentitySource(sources: Set[ProjectionDescriptor])
+case class EventMatcher(order: Order[DEvent], merge: (Vector[Identity], Vector[Identity]) => Vector[Identity])
 
-// QualifiedSelector(path: String, sel: JPath, valueType: ValueType)
+case class DatasetEnum[X, E, F[_]](enum: EnumeratorP[X, E, F], identityDerivation: Vector[IdentitySource])
 
-trait EventTypes {
-  type RawEvent = (Seq[Long], ByteBuffer)
+// QualifiedSelector(path: String, sel: JPath, valueType: EType)
+trait StorageEngineInsertAPI
+
+trait StorageEngineQueryAPI {
+  def column(path: Path): DatasetEnum[X, DEvent, IO]
+
+  //def column(path: String, selector: JPath, valueType: EType): DatasetEnum[X, DEvent, IO]
+  //def columnRange(interval: Interval[ByteBuffer])(path: String, selector: JPath, valueType: EType): DatasetEnum[X, (Seq[Long], ByteBuffer), IO]
 }
 
-trait StorageEngineInsertAPI {
+trait StorageEngineAPI extends StorageEngineInsertAPI with StorageEngineQueryAPI 
+
+
+trait OperationsAPI {
+  def cogroup[X, F[_]: Monad](enum1: DatasetEnum[X, DEvent, F], enum2: DatasetEnum[X, DEvent, F])(implicit matcher: EventMatcher): DatasetEnum[X, Either3[DEvent, (DEvent, DEvent), DEvent], F]
+
+  def crossLeft[X, F[_]: Monad](enum1: DatasetEnum[X, DEvent, F], enum2: DatasetEnum[X, DEvent, F]): DatasetEnum[X, DEvent, F]
+
+  def crossRight[X, F[_]: Monad](enum1: DatasetEnum[X, DEvent, F], enum2: DatasetEnum[X, DEvent, F]): DatasetEnum[X, DEvent, F]
+
+  def join[X, F[_]: Monad](enum1: DatasetEnum[X, DEvent, F], enum2: DatasetEnum[X, DEvent, F])(implicit matcher: EventMatcher): DatasetEnum[X, (DEvent, DEvent), F]
+
+  def merge[X, F[_]: Monad](enum1: DatasetEnum[X, DEvent, F], enum2: DatasetEnum[X, DEvent, F])(implicit matcher: EventMatcher): DatasetEnum[X, DEvent, F]
+
+  def sort[X, F[_]: Monad](enum: DatasetEnum[X, DEvent, F], identityIndex: Int): DatasetEnum[X, DEvent, F]
+
+  def vmap[X, F[_]: Monad](enum: DatasetEnum[X, DEvent, F])(f: SValue => SValue): DatasetEnum[X, DEvent, F]
+  def pmap[X, F[_]: Monad](enum: DatasetEnum[X, (DEvent, DEvent), F])(f: (SValue, SValue) => SValue): DatasetEnum[X, DEvent, F]
+
+  def filter[X, F[_]: Monad](enum: DatasetEnum[X, (DEvent, DEvent), F])(pred: (SValue, SValue) => Option[SValue]): DatasetEnum[X, DEvent, F]
+
+  def lift[X, F[_]: Monad](value: SValue): DatasetEnum[X, DEvent, F]
 }
-
-//trait StorageEngineQueryAPI extends EventTypes {
-//  def column(path: String, selector: JPath, valueType: ValueType): DatasetEnum[X, RawEvent, IO]
-//
-//  def columnRange(interval: Interval[ByteBuffer])(path: String, selector: JPath, valueType: ValueType): DatasetEnum[X, (Seq[Long], ByteBuffer), IO]
-//}
-//
-//trait StorageEngineAPI extends StorageEngineInsertAPI with StorageEngineQueryAPI
-//
-//// ProjectionDescriptor describes PHYSICAL FORMAT of values in the byte buffer
-//case class ValuesDescriptor(values: Seq[(ValueFormat, Set[Metadata])])
-//case class DatasetEnum[X, E, F[_]](enum: EnumeratorP[X, E, F], identityDesc: Seq[Set[ColumnDescriptor]], valuesDesc: ValuesDescriptor)
-//
-//trait OperationsAPI extends EventTypes {
-//  def cogroup[X, F[_]: Monad](enum1: DatasetEnum[X, RawEvent, F], enum2: DatasetEnum[X, RawEvent, F]): DatasetEnum[X, Either3[RawEvent, RawEvent, RawEvent], F]
-//
-//  def crossLeft[X, F[_]: Monad](enum1: DatasetEnum[X, RawEvent, F], enum2: DatasetEnum[X, RawEvent, F]): DatasetEnum[X, RawEvent, F]
-//
-//  def crossRight[X, F[_]: Monad](enum1: DatasetEnum[X, RawEvent, F], enum2: DatasetEnum[X, RawEvent, F]): DatasetEnum[X, RawEvent, F]
-//
-//  def matched[X, F[_]: Monad](enum1: DatasetEnum[X, RawEvent, F], enum2: DatasetEnum[X, RawEvent, F]): DatasetEnum[X, RawEvent, F]
-//
-//  def merge[X, F[_]: Monad](enum1: DatasetEnum[X, RawEvent, F], enum2: DatasetEnum[X, RawEvent, F]): DatasetEnum[X, Either[RawEvent, RawEvent], F]
-//
-//  def sortIdentity[X, F[_]: Monad](enum: DatasetEnum[X, RawEvent, F], identityIndex: Int): DatasetEnum[X, RawEvent, F]
-//
-//  def mapValues[X, F[_]: Monad](enum: DatasetEnum[X, RawEvent, F], d: (ValueDescriptor, ByteBuffer => ByteBuffer)): DatasetEnum[X, RawEvent, F]
-//
-//  def mapIdentities[X, F[_]: Monad](enum: DatasetEnum[X, RawEvent, F], d: (Seq[Set[ColumnDescriptor]], Seq[Long] => Seq[Long])): DatasetEnum[X, RawEvent, F]
-//}
-
-  
-  
-
-
-// vim: set ts=4 sw=4 et:
