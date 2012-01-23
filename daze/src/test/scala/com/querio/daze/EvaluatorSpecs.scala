5740d246bb6889f11ac3c265fcac5c09d704afbf
Daniel Spiewak <daniel@reportgrid.com>
2012-01-23 15:24:10 -0600

    Implemented skeleton test harness for fullProjection

--- daze/src/test/scala/com/querio/daze/EvaluatorSpecs.scala
+++ daze/src/test/scala/com/querio/daze/EvaluatorSpecs.scala
@@ -23,6 +23,7 @@ package daze
 import org.specs2.mutable._
 
 object EvaluatorSpecs extends Specification with Evaluator {
+  import Function._
   import IterV._
   
   import dag._
@@ -39,6 +40,72 @@ object EvaluatorSpecs extends Specification with Evaluator {
       val (_, sv) = result.head
       sv must beLike {
         case SDecimal(d) => d mustEqual 42
+      }
+    }
+  }
+  
+  override object query extends StorageEngineQueryAPI {
+    private var pathIds = Map[Path, Int]()
+    private var currentId = 0
+    
+    def fullProjection[X](path: Path): DatasetEnum[X, SEvent, IO] =
+      DatasetEnum(readJSON[X](path))
+    
+    private def readJSON[X](path: Path) = {
+      val src = Source.fromInputStream(Class getResourceAsStream path.elements.mkString("/", "/", ".json"))
+      val stream = Stream from 0 map scaleId(path) zip (src.getLines map parseJSON toStream) map tupled(wrapSEvent)
+      Iteratee.enumPStream[X](stream)
+    }
+    
+    private def scaleId(path: Path)(seed: Int): Long = {
+      val scalar = synchronized {
+        if (!(pathIds contains path)) {
+          pathIds += (path -> currentId)
+          currentId += 1
+        }
+        
+        pathIds(path)
+      }
+      
+      (scalar.toLong << 32) | seed
+    }
+    
+    private def parseJSON(str: String): JValue =
+      JsonParser parse str
+    
+    private def wrapSEvent(id: Long, value: JValue): SEvent =
+      (Vector(id), wrapSValue(value))
+    
+    private def wrapSValue(value: JValue): SValue = new SValue {
+      def fold(
+          obj: Map[String, SValue] => A,
+          arr: Vector[SValue] => A,
+          str: String => A,
+          bool: Boolean => A,
+          long: Long => A,
+          double: Double => A,
+          num: BigDecimal => A,
+          nul: => A): A = value match {
+            
+        case JObject(fields) => {
+          val pairs = fields map {
+            case JField(key, value) => (key, wrapSValue(value))
+          }
+          
+          obj(Map(pairs: _*))
+        }
+        
+        case JArray(values) => arr(Vector(values map wrapSValue: _*))
+        
+        case JString(s) => str(s)
+        
+        case JBoolean(b) => bool(b)
+        
+        case JNum(d) => num(d)
+        
+        case JNull => nul
+        
+        case JNothing => sys.error("Hit JNothing")
       }
     }
   }
