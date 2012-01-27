9d1922b9769bf8b2e47f2f0396ebeb6a45ed0078
Nick Matheson <nick@reportgrid.com>
2012-01-26 17:08:43 -0700

    Initial pairing on unproject

--- yggdrasil/src/main/scala/com/reportgrid/yggdrasil/CValue.scala
+++ yggdrasil/src/main/scala/com/reportgrid/yggdrasil/CValue.scala
@@ -34,6 +34,21 @@ trait CValue {
   ): A
 }
 
+case class CInt(value: Int) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = int(value)
+}
+
 case class CLong(value: Long) extends CValue {
   def fold[A](
     str:    String => A,
@@ -50,5 +65,129 @@ case class CLong(value: Long) extends CValue {
 }
 
 
+case class CString(value: String) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = str(value)
+}
 
+
+case class CBool(value: Boolean) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = bool(value)
+}
+
+
+case class CFloat(value: Float) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = float(value)
+}
+
+
+case class CDouble(value: Double) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = double(value)
+}
+
+case class CNum(value: BigDecimal) extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = num(value)
+}
+
+
+case object CEmptyObject extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = emptyObj
+}
+
+
+case object CEmptyArray extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = emptyArr
+}
+
+
+case object CNull extends CValue {
+  def fold[A](
+    str:    String => A,
+    bool:   Boolean => A,
+    int:    Int => A,
+    long:   Long => A,
+    float:  Float => A,
+    double: Double => A,
+    num:    BigDecimal => A,
+    emptyObj: => A,
+    emptyArr: => A,
+    nul:      => A
+  ): A = nul
+}
 // vim: set ts=4 sw=4 et:
