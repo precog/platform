da0a0f948e361d4ccd21cf90ef5600242f60efab
Alissa Pajer <alissa.pajer@gmail.com>
2012-01-27 15:31:01 -0700

    testing more cases of project

--- yggdrasil/src/test/scala/com/reportgrid/storage/leveldb/LevelDBByteProjectionSpec.scala
+++ yggdrasil/src/test/scala/com/reportgrid/storage/leveldb/LevelDBByteProjectionSpec.scala
@@ -39,24 +39,32 @@ import scalaz._
 class LevelDBByteProjectionSpec extends Specification {
   "a byte projection" should {
     "project to the expected key format" in {
-      val testIdentity: Vector[Long] = Vector(2L)
-      val cv1 = CInt(5)
-      val testValues: Seq[CValue] = Seq(cv1)
+      val testIdentity: Vector[Long] = Vector(1L)
+      val cvInt5 = CInt(5)
+      val cvInt6 = CInt(6) 
+      val cvFloat = CFloat(77)
+      val cvBoolean = CBoolean(true)
+      val testValues: Seq[CValue] = Seq(cvInt5, cvInt6)
 
-      val path0: Path = Path("path")
-      val selector0: JPath = JPath("jpath")
-      val valueType0: ColumnType = SInt 
+      val path0: Path = Path("path0")
+      val selector0: JPath = JPath("key0")
+      val selector1: JPath = JPath("key1")
+      val valueTypeInt: ColumnType = SInt
+      val valueTypeFloat: ColumnType = SFloat
 
-      val listmap0: ColumnDescriptor = ColumnDescriptor(path0, selector0, valueType0, Ownership(Set()))
-      //val listmap1: ColumnDescriptor = ColumnDescriptor(path0, selector1, valueType1, Ownership(Set()))
+      val colDesInt1: ColumnDescriptor = ColumnDescriptor(path0, selector0, valueTypeInt, Ownership(Set()))
+      val colDesInt2: ColumnDescriptor = ColumnDescriptor(path0, selector1, valueTypeInt, Ownership(Set()))
+      //val colDesFloat: ColumnDescriptor = ColumnDescriptor(path0, selector0, valueTypeFloat, Ownership(Set()))
 
-      val int0: Int = 0 //must be 0 so that identity indexes are 0-based
-      val int1: Int = 1
+      val index0: Int = 0 //must be 0 so that identity indexes are 0-based
+      val index1: Int = 1
 
-      val columns: ListMap[ColumnDescriptor, Int] = ListMap(listmap0 -> int0)
-      val sorting: Seq[(ColumnDescriptor, SortBy)] = Seq((listmap0, ByValue))
-       
-      val byteProjectionV = ProjectionDescriptor(columns, sorting) map { d => 
+      val columns: ListMap[ColumnDescriptor, Int] = ListMap(colDesInt1 -> index0, colDesInt2 -> index1)
+      val sortingA: Seq[(ColumnDescriptor, SortBy)] = Seq((colDesInt1, ByValue),(colDesInt2, ByValue))
+      //val sortingB: Seq[(ColumnDescriptor, SortBy)] = Seq((colDesInt, ById))
+      //val sortingC: Seq[(ColumnDescriptor, SortBy)] = Seq((colDesInt, ByValueThenId)) 
+
+      val byteProjectionV = ProjectionDescriptor(columns, sortingA) map { d => 
         new LevelDBByteProjection {
           val descriptor: ProjectionDescriptor = d
         }
@@ -66,7 +74,7 @@ class LevelDBByteProjectionSpec extends Specification {
 
       
 
-      val expectedKey: Array[Byte] = Array(0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 2) 
+      val expectedKey: Array[Byte] = Array(0, 0, 0, 5,0,0,0,6,0, 0, 0, 0,0,0,0,1,0,0,0,0,0,0,0,2) 
       val expectedValue: Array[Byte] = Array()
       byteProjection.project(testIdentity, testValues)._1 must_== expectedKey
       byteProjection.project(testIdentity, testValues)._2 must_== expectedValue
