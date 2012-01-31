7f10072541057883a31882898eefe77afdd27c27
Daniel Spiewak <daniel@reportgrid.com>
2012-01-31 12:06:40 -0600

    Fixed issue with comment block termination with two inner dashes

--- quirrel/src/test/scala/com/querio/quirrel/parser/ParserSpecs.scala
+++ quirrel/src/test/scala/com/querio/quirrel/parser/ParserSpecs.scala
@@ -718,6 +718,10 @@ object ParserSpecs extends Specification with ScalaCheck with Parser with StubPh
         case NumLit(_, "1") => ok
       }
     }
+    
+    "greedily terminate comment blocks" in {
+      parse("(-a--)-)42") must throwA[ParseException]
+    }
   }
   
   "composed expression parsing" should {
