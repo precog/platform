4e371e5d8f15ca640ce23670f72c7ae9784ad493
Daniel Spiewak <daniel@reportgrid.com>
2012-02-02 11:32:35 -0600

    Corrected issue in cross/match reordering coming out of a characteristic function

--- daze/src/main/scala/com/precog/daze/Evaluator.scala
+++ daze/src/main/scala/com/precog/daze/Evaluator.scala
@@ -15,7 +15,7 @@ import com.precog.analytics.Path
 import com.precog.yggdrasil._
 import com.precog.util._
 
-trait Evaluator extends DAG with CrossOrdering with OperationsAPI {
+trait Evaluator extends DAG with OperationsAPI {
   import Function._
   
   import instructions._
@@ -140,7 +140,12 @@ trait Evaluator extends DAG with CrossOrdering with OperationsAPI {
         val rightEnum = loop(right, roots)
         
         val (pairs, op, distinct) = instr match {
-          case Map2Match(op) => (leftEnum join rightEnum, op, true)
+          case Map2Match(op) => {
+            val leftSort = ops.sort(leftEnum)
+            val rightSort = ops.sort(rightEnum)
+            (leftSort join rightSort, op, true)
+          }
+          
           case Map2Cross(op) => (leftEnum :* rightEnum, op, false)
           case Map2CrossLeft(op) => (leftEnum :* rightEnum, op, false)
           case Map2CrossRight(op) => (leftEnum *: rightEnum, op, false)
@@ -168,7 +173,12 @@ trait Evaluator extends DAG with CrossOrdering with OperationsAPI {
         val booleanEnum = loop(boolean, roots)
         
         val (pairs, distinct) = cross match {
-          case None => (targetEnum join booleanEnum, true)
+          case None => {
+            val targetSort = ops.sort(targetEnum)
+            val booleanSort = ops.sort(booleanEnum)
+            (targetSort join booleanSort, true)
+          }
+          
           case Some(CrossNeutral) => (targetEnum :* booleanEnum, false)
           case Some(CrossLeft) => (targetEnum :* booleanEnum, false)
           case Some(CrossRight) => (targetEnum *: booleanEnum, false)
@@ -186,6 +196,7 @@ trait Evaluator extends DAG with CrossOrdering with OperationsAPI {
         }
       }
       
+      // TODO this case is buggy (somehow); not using for the moment
       case Sort(parent, indexes) => {
         implicit val order: Order[SEvent] = new Order[SEvent] {
           def order(e1: SEvent, e2: SEvent): Ordering = {
@@ -219,7 +230,7 @@ trait Evaluator extends DAG with CrossOrdering with OperationsAPI {
       }
     }
     
-    loop(orderCrosses(graph), Nil)
+    loop(graph, Nil)
   }
 
   private def unlift[A, B](f: A => Option[B]): PartialFunction[A, B] = new PartialFunction[A, B] {
