c3312f7527485631b618643f163e2947dd784da1
Daniel Spiewak <daniel@reportgrid.com>
2012-02-06 16:42:08 -0600

    Added some positive test cases for sort insertion

--- daze/src/test/scala/com/precog/daze/CrossOrderingSpecs.scala
+++ daze/src/test/scala/com/precog/daze/CrossOrderingSpecs.scala
@@ -56,5 +56,99 @@ object CrossOrderingSpecs extends Specification with CrossOrdering {
       
       orderCrosses(input) mustEqual expected
     }
+    
+    // this will eventually be a re-order cross test case
+    "insert sorts for match on out-of-order operand set" >> {
+      "left" >> {
+        val line = Line(0, "")
+        
+        val left = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val right = Join(line, Map2CrossRight(Add),
+          left,
+          dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het))
+        
+        val input = Join(line, Map2Match(Or), left, right)
+        val expected = Join(line, Map2Match(Or), left, Sort(right, Vector(1)))
+        
+        orderCrosses(input) mustEqual expected
+      }
+      
+      "right" >> {
+        val line = Line(0, "")
+        
+        val right = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val left = Join(line, Map2CrossRight(Add),
+          right,
+          dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het))
+        
+        val input = Join(line, Map2Match(Or), left, right)
+        val expected = Join(line, Map2Match(Or), Sort(left, Vector(1)), right)
+        
+        orderCrosses(input) mustEqual expected
+      }
+      
+      "both" >> {
+        val line = Line(0, "")
+        
+        val foo = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val bar = dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het)
+        val baz = dag.LoadLocal(line, None, Root(line, PushString("/baz")), Het)
+        
+        val left = Join(line, Map2CrossRight(Add), foo, bar)
+        val right = Join(line, Map2CrossRight(Add), foo, baz)
+        
+        val input = Join(line, Map2Match(Or), left, right)
+        val expected = Join(line, Map2Match(Or), Sort(left, Vector(1)), Sort(right, Vector(1)))
+        
+        orderCrosses(input) mustEqual expected
+      }
+    }
+    
+    // this will eventually be a re-order cross test case
+    "insert sorts for filter on out-of-order operand set" >> {
+      "left" >> {
+        val line = Line(0, "")
+        
+        val left = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val right = Join(line, Map2CrossRight(Add),
+          left,
+          dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het))
+        
+        val input = Filter(line, None, None, left, right)
+        val expected = Filter(line, None, None, left, Sort(right, Vector(1)))
+        
+        orderCrosses(input) mustEqual expected
+      }
+      
+      "right" >> {
+        val line = Line(0, "")
+        
+        val right = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val left = Join(line, Map2CrossRight(Add),
+          right,
+          dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het))
+        
+        val input = Filter(line, None, None, left, right)
+        val expected = Filter(line, None, None, Sort(left, Vector(1)), right)
+        
+        orderCrosses(input) mustEqual expected
+      }
+      
+      "both" >> {
+        val line = Line(0, "")
+        
+        val foo = dag.LoadLocal(line, None, Root(line, PushString("/foo")), Het)
+        val bar = dag.LoadLocal(line, None, Root(line, PushString("/bar")), Het)
+        val baz = dag.LoadLocal(line, None, Root(line, PushString("/baz")), Het)
+        
+        val left = Join(line, Map2CrossRight(Add), foo, bar)
+        val right = Join(line, Map2CrossRight(Add), foo, baz)
+        
+        val input = Filter(line, None, None, left, right)
+        val expected = Filter(line, None, None, Sort(left, Vector(1)), Sort(right, Vector(1)))
+        
+        orderCrosses(input) mustEqual expected
+      }
+    }
   }
 }
