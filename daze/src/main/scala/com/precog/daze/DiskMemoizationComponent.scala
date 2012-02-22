5cc411652e2893800c75b0efc6ae0cf14f9a0d71
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2012-02-22 11:29:10 -0700

    Grouping loop function now compiling.

--- daze/src/main/scala/com/precog/daze/DiskMemoizationComponent.scala
+++ daze/src/main/scala/com/precog/daze/DiskMemoizationComponent.scala
@@ -17,7 +17,7 @@ import Iteratee._
 trait DiskMemoizationConfig {
   def memoizationBufferSize: Int
   def memoizationWorkDir: File
-  def memoizationSerialization: FileSerialization[SEvent]
+  def memoizationSerialization: FileSerialization[Vector[SEvent]]
 }
 
 trait DiskMemoizationComponent extends YggConfigComponent with MemoizationComponent { 
