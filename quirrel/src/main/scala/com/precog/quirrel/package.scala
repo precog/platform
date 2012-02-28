64e43799aa48cced07a84ab5452c8962535d18a6
Alissa Pajer <alissa.pajer@gmail.com>
2012-02-28 15:56:50 -0700

    Updated package object quirrel: override def toString in Identifier. (This is the last of Daniel's changes.)

--- quirrel/src/main/scala/com/precog/quirrel/package.scala
+++ quirrel/src/main/scala/com/precog/quirrel/package.scala
@@ -22,5 +22,7 @@ package com.precog
 package object quirrel {
   type TicId = String
 
-  case class Identifier(namespace: Vector[String], id: String)
+  case class Identifier(namespace: Vector[String], id: String) {
+    override def toString = (namespace map { _ + "::" } mkString) + id
+  }
 }
