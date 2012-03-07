a64e6794d8dcfff3eeaa31bebec6efcb89598c20
Kris Nuttycombe <kris@reportgrid.com>
2012-03-07 09:02:43 -0700

    Added test stubs.

---
+++ common/src/test/scala/com/precog/analytics/PermissionsSpec.scala
@@ -0,0 +1,20 @@
+package com.precog.analytics
+
+
+import org.specs2.mutable._
+
+class PermissionsSpec extends Specification {
+  "permissions" should {
+    "restrict access to only the specified set of permissions" in {
+      todo
+    }
+  }
+
+  "permissions serializaiton" should {
+    "deserialized what was serialized" in {
+      todo
+    }
+  }
+}
+
+// vim: set ts=4 sw=4 et:
