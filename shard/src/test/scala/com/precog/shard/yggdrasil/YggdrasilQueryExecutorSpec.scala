80636d85195742b4674c088378057fba125ab9fc
Kris Nuttycombe <kris@reportgrid.com>
2012-03-10 16:14:42 -0700

    Added stub test for YggdrasilQueryExecutor

---
+++ shard/src/test/scala/com/precog/shard/yggdrasil/YggdrasilQueryExecutorSpec.scala
@@ -0,0 +1,15 @@
+package com.precog.shard.yggdrasil
+
+import org.specs2.mutable._
+
+class YggdrasilQueryExecutorSpec extends Specification {
+  "the executor" should {
+    "trap syntax errors in queries" in todo
+    "trap compilaiton errors in queries" in todo
+    "trap runtime errors in queries" in todo
+    "trap timeout errors in queries" in todo
+  }
+}
+
+
+// vim: set ts=4 sw=4 et:
