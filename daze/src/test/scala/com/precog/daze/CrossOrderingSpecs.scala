f63d371c3fdacb7c6b22a472d7d791d3524a068c
Alissa Pajer <alissa.pajer@gmail.com>
2012-03-13 19:16:35 -0600

    java.lang.Math functions now available in quirrel. Left to do: clean up and some refactoring.

--- daze/src/test/scala/com/precog/daze/CrossOrderingSpecs.scala
+++ daze/src/test/scala/com/precog/daze/CrossOrderingSpecs.scala
@@ -21,7 +21,7 @@ package com.precog.daze
 
 import org.specs2.mutable._
 
-object CrossOrderingSpecs extends Specification with CrossOrdering with Timelib {
+object CrossOrderingSpecs extends Specification with CrossOrdering with Timelib with GenLibrary {
   import instructions._
   import dag._
   
