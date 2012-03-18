39de584890b3f68cd9a04f63507168dad0ba37b5
Kris Nuttycombe <kris@reportgrid.com>
2012-03-18 14:01:57 +0000

    Derek's changes.

--- yggdrasil/src/test/scala/com/precog/yggdrasil/RowIteratorSpec.scala
+++ yggdrasil/src/test/scala/com/precog/yggdrasil/RowIteratorSpec.scala
@@ -34,7 +34,8 @@ trait RowIteratorSpec {
 
   def genBufIterator[B <: Buffer, RI <: BufferRowIterator[B]](f: IterBuilder[B, RI])(implicit bufGen: Int => Gen[B]): Gen[RI] = 
     for {
-      len <- choose(1, 1000)
+      //len <- choose(1, 1000)
+      len <- choose(1, 3)
       ids <- listOfN(len, arbitrary[Long])
       buf <- bufGen(len)
       sel <- arbitrary[CPath]
@@ -91,11 +92,16 @@ object JoinRowIteratorSpec extends Specification with ScalaCheck with RowIterato
             case (id2, v2) :: ys =>
               if (id1 < id2) rec(xs, l2, r)
               else if (id1 > id2) rec(l1, ys, r) 
-              else rec(xs, ys, (id1, v1, v2) :: r)
+              else {
+                // Possible cartesian needed, find the full equal sequences on the right and left
+                val (l1Seq, l1Rem) = l1.partition(_._1 == id1)
+                val (l2Seq, l2Rem) = l2.partition(_._1 == id2)
 
-            case Nil => r
+                rec(l1Rem, l2Rem, l1Seq.flatMap { case (id1, v1) => l2Seq.map { case (id2, v2) => (id1, v1, v2) } }.reverse ::: r)
+              }
+            case Nil => r.reverse
           }
-        case Nil => r
+        case Nil => r.reverse
       }
     }
 
@@ -104,14 +110,39 @@ object JoinRowIteratorSpec extends Specification with ScalaCheck with RowIterato
 
   "a JoinRowIterator" should {
     "obtain the same results as a strict join" in {
+//      val k1 = List[Long](0, 1, 4, 4, 6, 7, 7)
+//      val v1 = List[Long](12, 13, 10, 15, 8, 2, 3)
+//      val k2 = List[Long](1, 2, 2, 4, 4, 5, 7)
+//      val v2 = List[Long](10, 22, 26, 1, 30, 5, 4)
+//
+//      println(joinLists(k1 zip v1, k2 zip v2))
+//
+//      val iter1 = LongBufferRowIterator(LongBuffer.allocate(k1.length).put(k1.toArray).clear().asInstanceOf[LongBuffer], LongBuffer.allocate(v1.length).put(v1.toArray).clear().asInstanceOf[LongBuffer], DynCPath(0))
+//      val iter2 = LongBufferRowIterator(LongBuffer.allocate(k2.length).put(k2.toArray).clear().asInstanceOf[LongBuffer], LongBuffer.allocate(v2.length).put(v2.toArray).clear().asInstanceOf[LongBuffer], DynCPath(1))
+//
+//      val jIter = JoinRowIterator(iter1, iter2, 1)
+//
+//      do {
+//        println((jIter.idAt(0), jIter.longAt(0), jIter.longAt(1)))
+//      } while (jIter.advance(1))
+//
+//      12 must_== 12
       check { (i1: LongBufferRowIterator, i2: LongBufferRowIterator) => 
-        val expected = joinLists(i1.keys.array.toList zip i1.values.array.toList, i2.keys.array.toList zip i2.values.array.toList).reverse
+        val expected = joinLists(i1.keys.array.toList zip i1.values.array.toList, i2.keys.array.toList zip i2.values.array.toList)
         val iter = JoinRowIterator(i1, i2, 1)
 
+        println("==============")
+        println("i1 = " + (i1.keys.array.toList zip i1.values.array.toList))
+        println("i2 = " + (i2.keys.array.toList zip i2.values.array.toList))
+        println("expected = " + expected)
+
         var initial = true
+        var doneAdvancing = false
+
         foreach(expected) {
           case (id, lv, rv) => 
-            if (!initial) iter.advance(1)
+            doneAdvancing must_!= true
+            if (!initial) { doneAdvancing = iter.advance(1) }
             initial = false
             println((id, lv, rv) + " :: " + (iter.idAt(0), iter.longAt(0), iter.longAt(1)))
             (iter.idAt(0) must_== id) and (iter.longAt(0) must_== lv) and (iter.longAt(1) must_== rv)
