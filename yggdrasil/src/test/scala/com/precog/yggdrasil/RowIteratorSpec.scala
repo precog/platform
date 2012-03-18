a110ebf1af0fbf6ebd83d08d10fd4cb179f3fd24
Kris Nuttycombe <kris@reportgrid.com>
2012-03-18 00:33:31 +0000

    Added (failing) test for JoinRowIterator

--- yggdrasil/src/test/scala/com/precog/yggdrasil/RowIteratorSpec.scala
+++ yggdrasil/src/test/scala/com/precog/yggdrasil/RowIteratorSpec.scala
@@ -10,7 +10,7 @@ import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen
 import org.scalacheck.Gen._
 
-object BufferRowIteratorSpec extends Specification with ScalaCheck {
+trait RowIteratorSpec {
   type IterBuilder[B <: Buffer, RI <: BufferRowIterator[B]] = (LongBuffer, B, CPath) => RI
   def longBuffer(longs: List[Long]) = LongBuffer.allocate(longs.length).put(longs.toArray).clear().asInstanceOf[LongBuffer]
 
@@ -43,7 +43,9 @@ object BufferRowIteratorSpec extends Specification with ScalaCheck {
   implicit val boolBufferIterBuilder = Arbitrary(genBufIterator[ByteBuffer, BoolBufferRowIterator](BoolBufferRowIterator.apply _))
   implicit val intBufferIterBuilder  = Arbitrary(genBufIterator[IntBuffer, IntBufferRowIterator](IntBufferRowIterator.apply _))
   implicit val longBufferIterBuilder = Arbitrary(genBufIterator[LongBuffer, LongBufferRowIterator](LongBufferRowIterator.apply _))
+}
 
+object BufferRowIteratorSpec extends Specification with ScalaCheck with RowIteratorSpec {
   "a BoolBufferRowIterator" should {
     "properly advance based on a count" in {
       check { (iter: BoolBufferRowIterator) => 
@@ -77,4 +79,46 @@ object BufferRowIteratorSpec extends Specification with ScalaCheck {
     }
   }
 }
+
+object JoinRowIteratorSpec extends Specification with ScalaCheck with RowIteratorSpec {
+  import scala.annotation.tailrec  
+
+  def joinLists(l1: List[(Long, Long)], l2: List[(Long, Long)]): List[(Long, Long, Long)] = {
+    @tailrec def rec(l1: List[(Long, Long)], l2: List[(Long, Long)], r: List[(Long, Long, Long)]): List[(Long, Long, Long)] = {
+      l1 match {
+        case (id1, v1) :: xs =>
+          l2 match {
+            case (id2, v2) :: ys =>
+              if (id1 < id2) rec(xs, l2, r)
+              else if (id1 > id2) rec(l1, ys, r) 
+              else rec(xs, ys, (id1, v1, v2) :: r)
+
+            case Nil => r
+          }
+        case Nil => r
+      }
+    }
+
+    rec(l1, l2, Nil)
+  }
+
+  "a JoinRowIterator" should {
+    "obtain the same results as a strict join" in {
+      check { (i1: LongBufferRowIterator, i2: LongBufferRowIterator) => 
+        val expected = joinLists(i1.keys.array.toList zip i1.values.array.toList, i2.keys.array.toList zip i2.values.array.toList).reverse
+        val iter = JoinRowIterator(i1, i2, 1)
+
+        var initial = true
+        foreach(expected) {
+          case (id, lv, rv) => 
+            if (!initial) iter.advance(1)
+            initial = false
+            println((id, lv, rv) + " :: " + (iter.idAt(0), iter.longAt(0), iter.longAt(1)))
+            (iter.idAt(0) must_== id) and (iter.longAt(0) must_== lv) and (iter.longAt(1) must_== rv)
+        }
+      }
+    }
+  }
+}
+
 // vim: set ts=4 sw=4 et:
