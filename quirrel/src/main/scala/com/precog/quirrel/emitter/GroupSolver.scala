df776c9683b09b38193d2b69f9ec338fadec2db4
Daniel Spiewak <daniel@reportgrid.com>
2012-03-20 18:33:22 -0600

    Fixed new code for AST changes

--- quirrel/src/main/scala/com/precog/quirrel/emitter/GroupSolver.scala
+++ quirrel/src/main/scala/com/precog/quirrel/emitter/GroupSolver.scala
@@ -94,7 +94,16 @@ trait GroupSolver extends AST with GroupFinder with Solver with Solutions {
       actualErrors ++ ourErrors
     }
     
-    case Operation(_, left, _, right) =>
+    case Where(_, left, right) =>
+      inferBuckets(left) ++ inferBuckets(right)
+    
+    case With(_, left, right) =>
+      inferBuckets(left) ++ inferBuckets(right)
+    
+    case Union(_, left, right) =>
+      inferBuckets(left) ++ inferBuckets(right)
+    
+    case Intersect(_, left, right) =>
       inferBuckets(left) ++ inferBuckets(right)
     
     case Add(_, left, right) =>
@@ -264,5 +273,5 @@ trait GroupSolver extends AST with GroupFinder with Solver with Solutions {
   
   case class UnionBucket(left: Bucket, right: Bucket) extends Bucket
   case class IntersectBucket(left: Bucket, right: Bucket) extends Bucket
-  case class Group(origin: Operation, target: Expr, forest: Solution, extras: Set[Expr]) extends Bucket
+  case class Group(origin: Where, target: Expr, forest: Solution, extras: Set[Expr]) extends Bucket
 }
