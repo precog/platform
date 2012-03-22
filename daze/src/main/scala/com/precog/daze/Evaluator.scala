3f0c8d871f3e8e30ea848c42212b4f0ea6e4ec31
Kris Nuttycombe <kris@reportgrid.com>
2012-03-22 16:40:22 -0600

    Cleanup of Genlib.

--- daze/src/main/scala/com/precog/daze/Evaluator.scala
+++ daze/src/main/scala/com/precog/daze/Evaluator.scala
@@ -48,7 +48,7 @@ trait MemoizingEvaluationContext extends EvaluationContext with MemoizationCompo
   }
 }
 
-trait Evaluator extends DAG with CrossOrdering with Memoizer with OperationsAPI with MemoizingEvaluationContext with GenOpcode with ImplLibrary with GenLibrary with Timelib { self =>
+trait Evaluator extends DAG with CrossOrdering with Memoizer with OperationsAPI with MemoizingEvaluationContext with ImplLibrary { self =>
   type X = QueryAPI#X
 
   import Function._
