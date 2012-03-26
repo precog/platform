1a11f71233826dcb2f2689a4299c0771833199d9
Daniel Spiewak <daniel@reportgrid.com>
2012-03-26 14:21:21 -0600

    The memoId used in Split needs to be stable

--- daze/src/main/scala/com/precog/daze/Dataset.scala
+++ daze/src/main/scala/com/precog/daze/Dataset.scala
@@ -32,7 +32,7 @@ trait GroupingOps[Dataset[_], Grouping[_, _]] {
   def zipGroups[A, K: Order](d1: Grouping[K, NEL[Dataset[A]]], d2: Grouping[K, NEL[Dataset[A]]]): Grouping[K, NEL[Dataset[A]]]
 
   // the resulting Dataset[B] needs to be merged such that it is value-unique and has new identities
-  def flattenGroup[A, K, B: Order](g: Grouping[K, NEL[Dataset[A]]], nextId: () => Identity)(f: (K, NEL[Dataset[A]]) => Dataset[B]): Dataset[B]
+  def flattenGroup[A, K, B: Order](g: Grouping[K, NEL[Dataset[A]]], nextId: () => Identity, memoId: Int)(f: (K, NEL[Dataset[A]]) => Dataset[B]): Dataset[B]
 
   def mapGrouping[K, A, B](g: Grouping[K, A])(f: A => B): Grouping[K, B]
 }
