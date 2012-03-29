65aabb412869e40ad60fefc7f0950e710dedc7ff
Kris Nuttycombe <kris@reportgrid.com>
2012-03-29 22:18:27 +0000

    Added typeclass for manual buffer specialization.

--- daze/src/main/scala/com/precog/daze/Dataset.scala
+++ daze/src/main/scala/com/precog/daze/Dataset.scala
@@ -51,7 +51,7 @@ trait GroupingOps[Dataset[_], Grouping[_, _]] {
   def zipGroups[A, K: Order](d1: Grouping[K, NEL[Dataset[A]]], d2: Grouping[K, NEL[Dataset[A]]]): Grouping[K, NEL[Dataset[A]]]
 
   // the resulting Dataset[B] needs to be merged such that it is value-unique and has new identities
-  def flattenGroup[A, K, B](g: Grouping[K, NEL[Dataset[A]]], nextId: () => Identity, memoId: Int, memoCtx: MemoizationContext[Dataset], expiration: Long)(f: (K, NEL[Dataset[A]]) => Dataset[B])(implicit order: Order[B], cm: ClassManifest[B], fs: SortSerialization[B]): Dataset[B]
+  def flattenGroup[A, K, B](g: Grouping[K, NEL[Dataset[A]]], nextId: () => Identity, memoId: Int, memoCtx: MemoizationContext[Dataset], expiration: Long)(f: (K, NEL[Dataset[A]]) => Dataset[B])(implicit buffering: Buffering[B], cm: ClassManifest[B], fs: SortSerialization[B]): Dataset[B]
 
   def mapGrouping[K, A, B](g: Grouping[K, A])(f: A => B): Grouping[K, B]
 }
@@ -99,7 +99,7 @@ trait DatasetExtensions[Dataset[_], Grouping[_, _], A] {
   def count: BigInt
 
   //uniq by value, assign new identities
-  def uniq(nextId: () => Identity, memoId: Int)(implicit order: Order[A], cm: ClassManifest[A], fs: SortSerialization[A]): Dataset[A]
+  def uniq(nextId: () => Identity, memoId: Int)(implicit buffering: Buffering[A], cm: ClassManifest[A], fs: SortSerialization[A]): Dataset[A]
 
   // identify(None) strips all identities
   def identify(nextId: Option[() => Identity]): Dataset[A]
