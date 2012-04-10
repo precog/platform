1faeed69c6261e612ee043ae4bdc57b00b1f5f76
Nick Matheson <nick@reportgrid.com>
2012-04-10 13:19:18 -0600

    Added beta system logging of performance tests

--- performance/src/test/scala/com/precog/performance/RevisedYggdrasilPerformanceSpec.scala
+++ performance/src/test/scala/com/precog/performance/RevisedYggdrasilPerformanceSpec.scala
@@ -64,7 +64,9 @@ trait RevisedYggdrasilPerformanceSpec extends Specification with PerformanceSpec
   val tmpDir = newTempDir() 
   lazy val shard = new TestShard(config, tmpDir)
   lazy val executor = new TestQueryExecutor(config, shard)
-  
+
+  val perfUtil = PerformanceUtil.default
+
   step {    
     Await.result(shard.actorsStart, timeout)
   }
@@ -148,6 +150,7 @@ histogram
       
       println("load test sim")
       val result = Performance().benchmark(test(10), benchParams, benchParams)   
+      perfUtil.uploadResults("load_test_sim", result)
       //val result = Performance().profile(test(10))   
       
       result.report("load test sym", System.out)
@@ -158,6 +161,7 @@ histogram
       val tests = 100000
       val batchSize = 1000
       val result = Performance().benchmark(insert(shard, Path("/test/insert/"), 0, batchSize, tests / batchSize), singleParams, singleParams)   
+      perfUtil.uploadResults("insert_100k", result)
       //val result = Performance().profile(insert(shard, Path("/test/insert/"), 0, batchSize, tests / batchSize))   
 
       println("starting insert test")
@@ -175,6 +179,7 @@ histogram
 
       Thread.sleep(10000)
       val result = Performance().benchmark(testRead(), benchParams, benchParams)   
+      perfUtil.uploadResults("read_100k", result)
       //val result = Performance().profile(testRead())   
       result.report("read 100K", System.out)
       true must_== true
@@ -196,6 +201,7 @@ histogram
       }
       
       val result = Performance().benchmark(test(10), benchParams, benchParams)   
+      perfUtil.uploadResults("read_10k_10x", result)
       //val result = Performance().profile(test(100))   
       
       result.report("read 10K elements x 10 times", System.out)
@@ -228,6 +234,7 @@ histogram
       } 
       
       val result = Performance().benchmark(test(1), benchParams, benchParams)   
+      perfUtil.uploadResults("read_10k_10thread", result)
       //val result = Performance().profile(test(10))   
       
       println("read small thread test")
@@ -257,6 +264,7 @@ count(tests where tests.gender = "male")
       }
       
       val result = Performance().benchmark(test(1), benchParams, benchParams)   
+      perfUtil.uploadResults("hw2_100k", result)
       //val result = Performance().profile(test(100))   
       
       result.report("hw2 test 100K * 1", System.out)
@@ -287,6 +295,7 @@ histogram
       }
       
       val result = Performance().benchmark(test(1), benchParams, benchParams)   
+      perfUtil.uploadResults("hw3_100k", result)
       //val result = Performance().profile(test(100))   
       
       result.report("hw3 test 100K * 1", System.out)
