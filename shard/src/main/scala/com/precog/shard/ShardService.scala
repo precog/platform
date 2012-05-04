ea360a530e108c8ed604369f5a81b57f7dccc0b7
Nick Matheson <nick@reportgrid.com>
2012-05-04 13:17:18 -0600

    Added actor status service (while trying to debug ingest issue)

--- shard/src/main/scala/com/precog/shard/ShardService.scala
+++ shard/src/main/scala/com/precog/shard/ShardService.scala
@@ -91,6 +91,8 @@ trait ShardService extends
                 } ~ 
                 get(new BrowseServiceHandler(state.queryExecutor, state.accessControl))
               }
+            } ~ path("actors/status") {
+              get(new ActorStatusHandler(state.queryExecutor))
             }
           }
         } ->
