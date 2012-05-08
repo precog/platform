bea2aefc36198927176eedf798de284147a653de
Nick Matheson <nick@reportgrid.com>
2012-05-08 16:52:23 -0600

    Fixed shard and ingest test errors

--- ingest/src/main/scala/com/precog/ingest/service/TrackingServiceHandler.scala
+++ ingest/src/main/scala/com/precog/ingest/service/TrackingServiceHandler.scala
@@ -38,25 +38,6 @@ import com.weiglewilczek.slf4s.Logging
 
 import scalaz.{Validation, Success}
 
-//import blueeyes._
-//import blueeyes.json.JsonDSL._
-//import blueeyes.json.xschema.DefaultSerialization._
-//import blueeyes.persistence.cache._
-//import blueeyes.util.Clock
-//
-//
-//import java.util.Properties
-//import java.util.concurrent.TimeUnit
-//import scalaz.Scalaz._
-//import scalaz.Success
-//import scalaz.Failure
-//import scalaz.Semigroup
-//import scalaz.NonEmptyList
-//
-//
-//import com.precog.common._
-//
-
 class TrackingServiceHandler(accessControl: AccessControl, eventStore: EventStore, usageLogging: UsageLogging, insertTimeout: Timeout)(implicit dispatcher: MessageDispatcher)
 extends CustomHttpService[Future[JValue], (Token, Path) => Future[HttpResponse[JValue]]] with Logging {
   val service = (request: HttpRequest[Future[JValue]]) => {
