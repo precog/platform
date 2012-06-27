1fcd11b66e217895e0a6c0380555bcd83a33f4cf
Kris Nuttycombe <kris@reportgrid.com>
2012-06-27 12:34:32 +0000

    Bijections between SampleData and ColumnarTable succeed.

--- yggdrasil/src/test/scala/com/precog/yggdrasil/table/ArbitrarySlice.scala
+++ yggdrasil/src/test/scala/com/precog/yggdrasil/table/ArbitrarySlice.scala
@@ -12,14 +12,19 @@ trait ArbitrarySlice extends ArbitraryProjectionDescriptor {
     containerOfN[List, Boolean](size, arbitrary[Boolean]) map { BitsetColumn.bitset _ }
   }
 
+  private def fullBitSet(size: Int): BitSet = {
+    BitSet((0 until size).toSeq : _*)
+  }
+
   def genColumn(col: ColumnDescriptor, size: Int): Gen[Column] = {
+    val bs = fullBitSet(size)
     col.valueType match {
-      case CStringArbitrary   => containerOfN[Array, String](size, arbitrary[String]) map ArrayStrColumn.apply _
-      case CStringFixed(w)    => containerOfN[Array, String](size, arbitrary[String].filter(_.length < w)) map ArrayStrColumn.apply _
-      case CBoolean           => containerOfN[Array, Boolean](size, arbitrary[Boolean]) map ArrayBoolColumn.apply _
-      case CLong              => containerOfN[Array, Long](size, arbitrary[Long]) map ArrayLongColumn.apply _
-      case CDouble            => containerOfN[Array, Double](size, arbitrary[Double]) map ArrayDoubleColumn.apply _
-      case CDecimalArbitrary  => containerOfN[List, Double](size, arbitrary[Double]) map { arr => ArrayNumColumn(arr.map(v => BigDecimal(v)).toArray) }
+      case CStringArbitrary   => containerOfN[Array, String](size, arbitrary[String]) map { strs => ArrayStrColumn(bs, strs) }
+      case CStringFixed(w)    => containerOfN[Array, String](size, arbitrary[String].filter(_.length < w)) map { strs => ArrayStrColumn(fullBitSet(strs.length), strs) }
+      case CBoolean           => containerOfN[Array, Boolean](size, arbitrary[Boolean]) map { bools => ArrayBoolColumn(bs, bools) }
+      case CLong              => containerOfN[Array, Long](size, arbitrary[Long]) map { longs => ArrayLongColumn(bs, longs) }
+      case CDouble            => containerOfN[Array, Double](size, arbitrary[Double]) map { doubles => ArrayDoubleColumn(bs, doubles) }
+      case CDecimalArbitrary  => containerOfN[List, Double](size, arbitrary[Double]) map { arr => ArrayNumColumn(bs, arr.map(v => BigDecimal(v)).toArray) }
       case CNull              => arbitraryBitSet(size) map { s => new BitsetColumn(s) with NullColumn }
       case CEmptyObject       => arbitraryBitSet(size) map { s => new BitsetColumn(s) with EmptyObjectColumn }
       case CEmptyArray        => arbitraryBitSet(size) map { s => new BitsetColumn(s) with EmptyArrayColumn }
---
+++ yggdrasil/src/test/scala/com/precog/yggdrasil/table/ColumnarTableModuleSpec.scala
@@ -0,0 +1,193 @@
+package com.precog.yggdrasil
+package table
+
+import com.precog.common.Path
+import com.precog.common.VectorCase
+
+import blueeyes.json._
+import blueeyes.json.JsonAST._
+import blueeyes.json.JsonDSL._
+import blueeyes.json.JsonParser
+
+import scala.annotation.tailrec
+import scala.collection.BitSet
+
+import scalaz._
+
+import org.specs2._
+import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+import org.scalacheck._
+import org.scalacheck.Gen
+import org.scalacheck.Gen._
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
+
+class ColumnarTableModuleSpec extends TableModuleSpec with CogroupSpec with ColumnarTableModule with TransformSpec { spec =>
+  override val defaultPrettyParams = Pretty.Params(2)
+
+  val sliceSize = 10
+  val testPath = Path("/tableOpsSpec")
+
+  def debugPrint(dataset: Table): Unit = {
+    println("\n\n")
+    for (slice <- dataset.slices; i <- 0 until slice.size) println(slice.toString(i))
+  }
+
+  def slice(sampleData: SampleData): (Slice, SampleData) = {
+    val (prefix, suffix) = sampleData.data.splitAt(sliceSize)
+
+    @tailrec def buildColArrays(from: Stream[JValue], into: Map[ColumnRef, (BitSet, Array[_])], sliceIndex: Int): (Map[ColumnRef, (BitSet, Object)], Int) = {
+      from match {
+        case jv #:: xs =>
+          val withIdsAndValues = jv.flattenWithPath.foldLeft(into) {
+            case (acc, (jpath, JNothing)) => acc
+            case (acc, (jpath, v)) =>
+              val ctype = CType.forJValue(v) getOrElse { sys.error("Cannot determine ctype for " + v + " at " + jpath + " in " + jv) }
+              val ref = ColumnRef(jpath, ctype)
+
+              val pair: (BitSet, Array[_]) = v match {
+                case JBool(b) => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), new Array[Boolean](sliceSize))).asInstanceOf[(BitSet, Array[Boolean])]
+                  col(sliceIndex) = b
+                  (defined + sliceIndex, col)
+
+                case JInt(ji) => CType.sizedIntCValue(ji) match {
+                  case CLong(v) =>
+                    val (defined, col) = acc.getOrElse(ref, (BitSet(), new Array[Long](sliceSize))).asInstanceOf[(BitSet, Array[Long])]
+                    col(sliceIndex) = v
+                    (defined + sliceIndex, col)
+
+                  case CNum(v) =>
+                    val (defined, col) = acc.getOrElse(ref, (BitSet(), new Array[BigDecimal](sliceSize))).asInstanceOf[(BitSet, Array[BigDecimal])]
+                    col(sliceIndex) = v
+                    (defined + sliceIndex, col)
+                }
+
+                case JDouble(d) => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), new Array[Double](sliceSize))).asInstanceOf[(BitSet, Array[Double])]
+                  col(sliceIndex) = d
+                  (defined + sliceIndex, col)
+
+                case JString(s) => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), new Array[String](sliceSize))).asInstanceOf[(BitSet, Array[String])]
+                  col(sliceIndex) = s
+                  (defined + sliceIndex, col)
+                
+                case JArray(Nil)  => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+
+                case JObject(Nil) => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+
+                case JNull        => 
+                  val (defined, col) = acc.getOrElse(ref, (BitSet(), null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+              }
+
+              acc + (ref -> pair)
+          }
+
+          buildColArrays(xs, withIdsAndValues, sliceIndex + 1)
+
+        case _ => (into, sliceIndex)
+      }
+    }
+
+    val slice = new Slice {
+      val (cols, size) = buildColArrays(prefix, Map.empty[ColumnRef, (BitSet, Array[_])], 0) 
+      val columns = cols map {
+        case (ref @ ColumnRef(_, CBoolean), (defined, values))          => (ref, ArrayBoolColumn(defined, values.asInstanceOf[Array[Boolean]]))
+        case (ref @ ColumnRef(_, CLong), (defined, values))             => (ref, ArrayLongColumn(defined, values.asInstanceOf[Array[Long]]))
+        case (ref @ ColumnRef(_, CDouble), (defined, values))           => (ref, ArrayDoubleColumn(defined, values.asInstanceOf[Array[Double]]))
+        case (ref @ ColumnRef(_, CDecimalArbitrary), (defined, values)) => (ref, ArrayNumColumn(defined, values.asInstanceOf[Array[BigDecimal]]))
+        case (ref @ ColumnRef(_, CStringArbitrary), (defined, values))  => (ref, ArrayStrColumn(defined, values.asInstanceOf[Array[String]]))
+        case (ref @ ColumnRef(_, CEmptyArray), (defined, values))       => (ref, new BitsetColumn(defined) with EmptyArrayColumn)
+        case (ref @ ColumnRef(_, CEmptyObject), (defined, values))      => (ref, new BitsetColumn(defined) with EmptyObjectColumn)
+        case (ref @ ColumnRef(_, CNull), (defined, values))             => (ref, new BitsetColumn(defined) with NullColumn)
+      }
+    }
+
+    (slice, SampleData(suffix))
+  }
+
+  def fromJson(sampleData: SampleData): Table = {
+    val (s, xs) = spec.slice(sampleData)
+
+    new Table(s.columns.keySet, new Iterable[Slice] {
+      def iterator = new Iterator[Slice] {
+        private var _next = s
+        private var _rest = xs
+
+        def hasNext = _next != null
+        def next() = {
+          val tmp = _next
+          _next = if (_rest.data.isEmpty) null else {
+            val (s, xs) = spec.slice(_rest)
+            _rest = xs
+            s
+          }
+          tmp
+        }
+      }
+    })
+  }
+
+  def toJson(dataset: Table): Stream[JValue] = {
+    dataset.toJson.toStream
+  }
+
+  def cogroup(ds1: Table, ds2: Table): Table = sys.error("todo")
+  def liftF1(f: CValue => CValue): CF1 = sys.error("todo")
+
+  "a table dataset" should {
+    "verify bijection from static JSON" in {
+      val sample: List[JValue] = List(
+        JObject(
+          JField("key", JArray(JInt(-1L) :: JInt(0L) :: Nil)) ::
+          JField("value", JNull) :: Nil
+        ), 
+        JObject(
+          JField("key", JArray(JInt(-3090012080927607325l) :: JInt(2875286661755661474l) :: Nil)) ::
+          JField("value", JObject(List(
+            JField("q8b", JArray(List(
+              JDouble(6.615224799778253E307d), 
+              JArray(List(JBool(false), JNull, JDouble(-8.988465674311579E307d))), JDouble(-3.536399224770604E307d)))), 
+            JField("lwu",JDouble(-5.121099465699862E307d))))
+          ) :: Nil
+        ), 
+        JObject(
+          JField("key", JArray(JInt(-3918416808128018609l) :: JInt(-1L) :: Nil)) ::
+          JField("value", JDouble(-1.0)) :: Nil
+        )
+      )
+
+      val dataset = fromJson(SampleData(sample.toStream))
+      //dataset.slices.foreach(println)
+      val results = dataset.toJson.toList
+      results must containAllOf(sample).only
+    }
+
+    "verify bijection from JSON" in checkMappings
+
+    /*
+    "in cogroup" >> {
+      "survive scalacheck" in { 
+        check { cogroupData: (SampleData, SampleData) => testCogroup(cogroupData._1, cogroupData._2) } 
+      }
+
+      "cogroup across slice boundaries" in testCogroupSliceBoundaries
+      "survive pathology 2" in testCogroupPathology2
+    }
+    */
+
+    "in transform" >> {
+      "perform the identity transform" in checkTransformLeaf
+    }
+  }
+}
+
+
+// vim: set ts=4 sw=4 et:
