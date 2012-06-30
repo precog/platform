47664836d6fb0e67452d3cae1e9f0d0c02fa911b
Alissa Pajer <alissa.pajer@gmail.com>
2012-06-30 13:37:53 -0600

    still working - haven't tried to compile yet.

--- bytecode/src/main/scala/com/precog/bytecode/Library.scala
+++ bytecode/src/main/scala/com/precog/bytecode/Library.scala
@@ -47,13 +47,24 @@ trait Op2Like {
   override def toString = "[0x%06x]".format(opcode) + fqn
 }
 
+trait ReductionLike {
+  val namespace: Vector[String]
+  val name: String
+  val opcode: Int
+
+  def fqn: String
+  override def toString = "[0x%06x]".format(opcode) + fqn
+}
+
 trait Library {
   type Morphism <: MorphismLike
   type Op1 <: Op1Like with Morphism
   type Op2 <: Op2Like with Morphism
+  type Reduction <: ReductionLike with Morphism
 
   def libMorphism: Set[Morphism]
   def lib1: Set[Op1] 
   def lib2: Set[Op2]
+  def libReduction: Set[Reduction]
 }
 
