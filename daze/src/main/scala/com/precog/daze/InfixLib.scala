d53495bbf1da4c93d5ee3b3242235f8a5333e60e
Daniel Spiewak <daniel@reportgrid.com>
2012-07-03 10:56:40 -0600

    All numeric functions need to be defined a zillion times.  MACROS!!!!!

--- daze/src/main/scala/com/precog/daze/InfixLib.scala
+++ daze/src/main/scala/com/precog/daze/InfixLib.scala
@@ -12,40 +12,7 @@ trait InfixLib extends ImplLibrary with GenOpcode {
   type F1 = CF1P
   type F2 = CF2P
   
-  def PrimitiveEqualsF2 = new CF2P({
-    case (c1: LongColumn, c2: LongColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = c1(row) == c2(row)
-    }
-    case (c1: DoubleColumn, c2: DoubleColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = c1(row) == c2(row)
-    }
-    case (c1: NumColumn, c2: NumColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = c1(row) == c2(row)
-    }
-    case (c1: StringColumn, c2: StringColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = c1(row) == c2(row)
-    }
-    case (c1: BoolColumn, c2: BoolColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = !(c1(row) ^ c2(row))      // when equals just isn't fast enough!
-    }
-    case (c1: NullColumn, c2: NullColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = true
-    }
-    case (c1: EmptyObjectColumn, c2: EmptyObjectColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = true
-    }
-    case (c1: EmptyArrayColumn, c2: EmptyArrayColumn) => new BoolColumn {
-      def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
-      def apply(row: Int) = true
-    }
-  })
+  def PrimitiveEqualsF2 = table.cf.std.Eq
   
   object Infix {
     val InfixNamespace = Vector("std", "infix")
@@ -56,7 +23,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) + c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) + c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) + c2(row)
+        }
+        case (c1: DoubleColumn, c2: Long) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) + c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new DoubleColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) + c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) + c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) + c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new NumColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) + c2(row)
         }
@@ -73,7 +64,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) - c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) - c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) - c2(row)
+        }
+        case (c1: DoubleColumn, c2: Long) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) - c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new DoubleColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) - c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) - c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) - c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new NumColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) - c2(row)
         }
@@ -90,7 +105,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) * c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) * c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) * c2(row)
+        }
+        case (c1: DoubleColumn, c2: Long) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) * c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new DoubleColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) * c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) * c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) * c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new NumColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) * c2(row)
         }
@@ -103,16 +142,40 @@ trait InfixLib extends ImplLibrary with GenOpcode {
 
     object Div extends Op2(InfixNamespace, "divide") {
       def f2: F2 = new CF2P({
-        case (c1: LongColumn, c2: LongColumn) => new LongColumn {
-          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row) && c2(row) != 0
+        case (c1: LongColumn, c2: LongColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) / c2(row)
+        }
+        case (c1: LongColumn, c2: DoubleColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) / c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) / c2(row)
         }
+        case (c1: DoubleColumn, c2: Long) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = (c1(row): BigDecimal) / c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new DoubleColumn {
-          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row) && c2(row) != 0
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) / c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) / c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) / c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new NumColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) / c2(row)
         }
         case (c1: NumColumn, c2: NumColumn) => new NumColumn {
-          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row) && c2(row) != 0
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) / c2(row)
         }
       })
@@ -124,7 +187,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) < c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
+        case (c1: DoubleColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) < c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new BoolColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) < c2(row)
         }
@@ -141,7 +228,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) <= c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
+        case (c1: DoubleColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) <= c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new BoolColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) <= c2(row)
         }
@@ -158,7 +269,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) > c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
+        case (c1: DoubleColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) > c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new BoolColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) > c2(row)
         }
@@ -175,7 +310,31 @@ trait InfixLib extends ImplLibrary with GenOpcode {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) >= c2(row)
         }
+        case (c1: LongColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
+        case (c1: LongColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
+        case (c1: DoubleColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
         case (c1: DoubleColumn, c2: DoubleColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
+        case (c1: DoubleColumn, c2: NumColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
+        case (c1: NumColumn, c2: LongColumn) => new BoolColumn {
+          def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
+          def apply(row: Int) = c1(row) >= c2(row)
+        }
+        case (c1: NumColumn, c2: DoubleColumn) => new BoolColumn {
           def isDefinedAt(row: Int) = c1.isDefinedAt(row) && c2.isDefinedAt(row)
           def apply(row: Int) = c1(row) >= c2(row)
         }
