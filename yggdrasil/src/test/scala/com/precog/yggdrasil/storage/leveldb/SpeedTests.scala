4c97b6707a88d26349b37bf4df427f58b8ea9891
Derek Chen-Becker <derek@reportgrid.com>
2012-07-04 07:42:55 -0600

    Add unit test to ensure proper readahead behavior
    [Fixes #32224071]

--- yggdrasil/src/test/scala/com/precog/yggdrasil/storage/leveldb/SpeedTests.scala
+++ yggdrasil/src/test/scala/com/precog/yggdrasil/storage/leveldb/SpeedTests.scala
@@ -20,16 +20,22 @@
 package com.precog.storage
 package leveldb
 
-import com.precog.common.VectorCase
+import com.precog.common.{Path,VectorCase}
 import com.precog.util.Bijection
+import com.precog.yggdrasil._
 import com.precog.yggdrasil.leveldb.LevelDBProjection
 
+import org.apache.commons.io.FileUtils
+
 import org.scalacheck.Arbitrary
+
+import scalaz.effect._
 
 import java.io.File
 import java.math.BigDecimal
 import java.nio.ByteBuffer
-import scalaz.effect._
+
+import scala.collection.immutable.ListMap
 
 object SpeedTests {
   def main (argv : Array[String]) {
@@ -41,53 +47,68 @@ object SpeedTests {
       }
     }
 
-    val column = LevelDBProjection.forDescriptor(new File("/tmp/speed"), sys.error("todo") /*ProjectionComparator.BigDecimal*/) ||| {
-      errors => throw errors
-    }
+    val dataDir = File.createTempFile("ColumnSpec", ".db")
+    println("Using %s for dbtest".format(dataDir))
+    dataDir.delete() // Ugly, but it works
 
-    val biGen = Arbitrary.arbitrary[BigInt]
-    val intGen = Arbitrary.arbitrary[Int]
-
-    def time[T](name : String)(f : => T) = {
-      println("Beginning " + name)
-      val start = System.currentTimeMillis
-      val result = f
-      val duration = System.currentTimeMillis - start
-      println("%s took %d ms".format(name, duration))
-      (result,duration)
-    }
-
-    // Stuff a whole bunch of crap into the datastore
-    val results = (0 until chunks).map { offset =>
-      val toInsert = (1 to chunksize).map{ id =>
-        for {v     <- biGen.sample
-             scale <- intGen.sample} 
-          yield (id + offset * 100, new java.math.BigDecimal(v.underlying, scale))
+    val colDesc = ColumnDescriptor(Path("/"), ".foo", CInt, Authorities(Set()))
+    
+    ProjectionDescriptor(ListMap(colDesc -> 0), Seq(colDesc -> ById)) foreach { descriptor =>
+      val writeColumn = LevelDBProjection.forDescriptor(dataDir, descriptor) ||| {
+        errors => throw errors
+      }
+  
+      val intGen = Arbitrary.arbitrary[Int]
+  
+      def time[T](name : String)(f : => T) = {
+        println("Beginning " + name)
+        val start = System.currentTimeMillis
+        val result = f
+        val duration = System.currentTimeMillis - start
+        println("%s took %d ms".format(name, duration))
+        (result,duration)
+      }
+  
+      // Stuff a whole bunch of crap into the datastore
+      val results = (0 until chunks).map { offset =>
+        val toInsert = (0 until chunksize).map{ id =>
+          val indexValue = id + offset * chunksize
+          (indexValue, Seq(CInt(indexValue)))
+        }
+  
+        time("writes") {
+          toInsert foreach { case (id, value) => writeColumn.insert(VectorCase(id), value).unsafePerformIO }
+        }
+      }
+  
+      writeColumn.close.unsafePerformIO
+  
+      val count = chunks * chunksize
+      val totalduration = results.map(_._2).sum
+      println("Total duration for %d writes = %d ms (%f/s)".format(count, totalduration, count / (totalduration / 1000.0)))
+  
+      val readColumn = LevelDBProjection.forDescriptor(dataDir, descriptor) ||| {
+        errors => throw errors
       }
 
-      time("writes") {
-        toInsert.foldLeft(IO(())) { case (io, Some((id, value))) => io.flatMap(_ => column.insert(VectorCase(id), sys.error("todo") /*value.as[Array[Byte]].as[ByteBuffer]*/)) } unsafePerformIO
+      // Pull it all back out
+      val readCount = 50
+      val readResults = (1 to readCount) map { 
+        _=> time("reads") {
+          val dataset = readColumn.traverseIndex(System.currentTimeMillis + 3600000l) // One hour should be plenty
+          val iterator = dataset.iterable.iterator
+          iterator foreach { case (id,v) => /* NOOP */ }
+        }
       }
+  
+      FileUtils.deleteDirectory(dataDir)
+      
+      val totalReadDuration = readResults.map(_._2).sorted.drop(5).dropRight(5).sum
+      val avgReadDuration = totalReadDuration / (readCount - 10)
+      println("Read avg = %d ms (%f/s)".format(avgReadDuration, count * 1000.0 / avgReadDuration))
+
+      sys.exit(0)
     }
-
-    column.close.unsafePerformIO
-
-    val count = chunks * chunksize
-    val totalduration = results.map(_._2).sum
-    println("Total duration for %d writes = %d ms (%f/s)".format(count, totalduration, count / (totalduration / 1000.0)))
-
-    //val d = new Column("speed", "/tmp")
-
-    // Pull it all back out
-    //time("reads on value index") {
-    //  toInsert.foreach{ case Some((id,value)) => 
-    //    d.getIds(value)
-    //    //if (!d.getIds(value).contains(id)) {
-    //    //  println("Data for %s is %s".format(value, d.getIds(value)))
-    //    //  println("Missing data for " + id)
-    //    //}
-    //  }
-    //}
   }
 }
 
