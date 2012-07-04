517cd863ceca475f15943316b272e8e8ade7d017
Alissa Pajer <alissa.pajer@gmail.com>
2012-07-04 15:12:22 -0600

    TimeLib ready to try to compile.

--- daze/src/main/scala/com/precog/daze/TimeLib.scala
+++ daze/src/main/scala/com/precog/daze/TimeLib.scala
@@ -93,9 +93,36 @@ trait TimeLib extends GenOpcode with ImplLibrary {
     }
   }
 
+  DateTimeZone.setDefault(DateTimeZone.UTC)
+
+  object ParseDateTime extends Op2(TimeNamespace, "parse") {
+    def f2: F2 = new CF2P({
+      case (c1: StrColumn, c2: StrColumn) => new Map2Column(c1, c2) with DateColumn {
+        def apply(row: Int) = {
+          val time = c1(row)
+          val fmt = c2(row)
+
+          if (isValidFormat(time, fmt)) {
+            val format = DateTimeFormat.forPattern(fmt).withOffsetParsed()
+            val ISO = format.parseDateTime(time)
+
+            ISO.toString()
+          } else sys.error("todo")
+        }
+    
+
+    //val operandType = (Some(SString), Some(SString))
+    //val operation: PartialFunction[(SValue, SValue), SValue] = {
+    //  case (SString(time), SString(fmt)) if (isValidFormat(time, fmt)) =>
+    //    val format = DateTimeFormat.forPattern(fmt).withOffsetParsed()
+    //    val ISO = format.parseDateTime(time)
+    //    SString(ISO.toString())
+    //}
+  }
+
   object ChangeTimeZone extends Op2(TimeNamespace, "changeTimeZone") {
     def f2: F2 = new CF2P({
-      case (c1: StrColumn, c2: StrColumn) => new Map2Column(c1, c2) with StrColumn {
+      case (c1: DateColumn, c2: StrColumn) => new Map2Column(c1, c2) with DateColumn {
         def apply(row: Int) = {
           val time = c1(row)
           val tz = c2(row)
@@ -121,9 +148,69 @@ trait TimeLib extends GenOpcode with ImplLibrary {
     } */
   }
 
+  trait TimePlus extends Op2 {
+    def f2: F2 = new CF2P({
+      case (c1: DateColumn, c2: LongColumn) => new Map2Column(c1, c2) with DateColumn {
+        def apply(row: Int) = {
+          val time = c1(row)
+          val incr = c2(row)
+
+          if (isValidISO(time) && isValidInt(incr)) {   //TODO test for isValidInt case
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time1)
+
+            plus(newTime, incr.toInt)
+          } else sys.error("todo'")
+        }
+      }
+    })
+
+    //val operandType = (Some(SString), Some(SDecimal)) 
+
+    //val operation: PartialFunction[(SValue, SValue), SValue] = {
+    //  case (SString(time), SDecimal(incr)) if isValidISO(time) => 
+    //    val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+    //    SString(plus(newTime, incr.toInt))
+    //}
+
+    def plus(d: DateTime, i: Int): String
+  }
+
+  object YearsPlus extends Op2(TimeNamespace, "yearsPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.years(i)).toString()
+  }
+
+  object MonthsPlus extends Op2(TimeNamespace, "monthsPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.months(i)).toString()
+  }
+
+  object WeeksPlus extends Op2(TimeNamespace, "weeksPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.weeks(i)).toString()
+  }
+
+  object DaysPlus extends Op2(TimeNamespace, "daysPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.days(i)).toString()
+  }
+
+  object HoursPlus extends Op2(TimeNamespace, "hoursPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.hours(i)).toString()
+  }
+
+  object MinutesPlus extends Op2(TimeNamespace, "minutesPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.minutes(i)).toString()
+  }
+
+  object SecondsPlus extends Op2(TimeNamespace, "secondsPlus") with TimePlus{ 
+    def plus(d: DateTime, i: Int) = d.plus(Period.seconds(i)).toString()
+  }
+
+  object MillisPlus extends Op2(TimeNamespace, "millisPlus") with TimePlus{
+    def plus(d: DateTime, i: Int) = d.plus(Period.millis(i)).toString()
+  }
+  
+
   trait TimeBetween extends Op2 {
     def f2: F2 = new CF2P({
-      case (c1: StrColumn, c2: StrColumn) => new Map2Column(c1, c2) with LongColumn {
+      case (c1: DateColumn, c2: DateColumn) => new Map2Column(c1, c2) with LongColumn {
         def apply(row: Int) = {
           val time1 = c1(row)
           val time2 = c2(row)
@@ -185,8 +272,18 @@ trait TimeLib extends GenOpcode with ImplLibrary {
 
   object MillisToISO extends Op2(TimeNamespace, "millisToISO") {
     def f2: F2 = new CF2P({
-      case (c1: StrColumn, c2: StrColumn) => new Map2Column(c1, c2) with StrColumn {
+      case (c1: LongColumn, c2: StrColumn) => new Map2Column(c1, c2) with DateColumn {
         def apply(row: Int) = 
+          val time = c1(row)
+          val tz = c2(row)
+
+          if (time >= Long.MinValue && time <= Long.MaxValue && isValidTimeZone(tz)){
+            val timeZone = DateTimeZone.forID(tz)
+            val dateTime = new DateTime(time.toLong, timeZone)
+
+            dateTime.toString()
+          } else sys.error("todo")
+        }
       }
     })
     
@@ -202,8 +299,15 @@ trait TimeLib extends GenOpcode with ImplLibrary {
 
   object GetMillis extends Op1(TimeNamespace, "getMillis") {
     def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
+      case c: DateColumn => new Map1Column(c) with LongColumn {
+        def apply(row: Int) = {
+          val time = c(row)
+
+          if (isValidISO(time)) {
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+            newTime.getMillis()
+          } else sys.error("todo")
+        }
       }
     })
     
@@ -217,8 +321,16 @@ trait TimeLib extends GenOpcode with ImplLibrary {
 
   object TimeZone extends Op1(TimeNamespace, "timeZone") {
     def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
+      case c: DateColumn => new Map1Column(c) with StrColumn {
         def apply(row: Int) = 
+          val time = c1(row)
+
+          if (isValidISO(time)) {
+            val format = DateTimeFormat.forPattern("ZZ")
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+            format.print(newTime)
+          } else sys.error("todo")
+        }
       }
     })
     
@@ -233,8 +345,20 @@ trait TimeLib extends GenOpcode with ImplLibrary {
 
   object Season extends Op1(TimeNamespace, "season") {
     def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
+      case c: DateColumn => new Map1Column(c) with StrColumn {
+        def apply(row: Int) = {
+          val time = c(row)
+
+          if (isValidISO(time)) {
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+            val day = newTime.dayOfYear.get
+            
+            if (day >= 79 & day < 171) "spring"
+            else if (day >= 171 & day < 265) "summer"
+            else if (day >= 265 & day < 355) "fall"
+            else "winter"
+          } else sys.error("todo")  //todo TEST THIS!!!!!!!!!!!!!!!!!!!!
+        }
       }
     })
     
@@ -253,6 +377,20 @@ trait TimeLib extends GenOpcode with ImplLibrary {
   } 
 
   trait TimeFraction extends Op1 {
+    def f1: F1 = new CF1P({
+      case c: DateColumn => new Map1Column(c) with LongColumn {
+        def apply(row: Int) = {
+          val time = c(row)
+
+          if (isValidISO(time)) => {
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+            fraction(newTime)
+          }
+        }
+
+        def fraction(d: DateTime): Int
+      }
+    })
     /* val operandType = Some(SString)
     val operation: PartialFunction[SValue, SValue] = {
       case SString(time) if isValidISO(time) => 
@@ -264,132 +402,75 @@ trait TimeLib extends GenOpcode with ImplLibrary {
   }
 
   object Year extends Op1(TimeNamespace, "year") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.year.get
+    def fraction(d: DateTime) = d.year.get
   }
 
   object QuarterOfYear extends Op1(TimeNamespace, "quarter") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = ((d.monthOfYear.get - 1) / 3) + 1
+    def fraction(d: DateTime) = ((d.monthOfYear.get - 1) / 3) + 1
   }
 
   object MonthOfYear extends Op1(TimeNamespace, "monthOfYear") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.monthOfYear.get
+    def fraction(d: DateTime) = d.monthOfYear.get
   }
 
   object WeekOfYear extends Op1(TimeNamespace, "weekOfYear") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.weekOfWeekyear.get
+    def fraction(d: DateTime) = d.weekOfWeekyear.get
   } 
 
   object WeekOfMonth extends Op1(TimeNamespace, "weekOfMonth") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    /* def fraction(newTime: DateTime) = {
+    def fraction(newTime: DateTime) = {
       val dayOfMonth = newTime.dayOfMonth().get
       val firstDate = newTime.withDayOfMonth(1)
       val firstDayOfWeek = firstDate.dayOfWeek().get
       val offset = firstDayOfWeek - 1
       ((dayOfMonth + offset) / 7) + 1
-    } */
+    } 
   }
  
   object DayOfYear extends Op1(TimeNamespace, "dayOfYear") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.dayOfYear.get
+    def fraction(d: DateTime) = d.dayOfYear.get
   }
 
   object DayOfMonth extends Op1(TimeNamespace, "dayOfMonth") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.dayOfMonth.get
+    def fraction(d: DateTime) = d.dayOfMonth.get
   }
 
   object DayOfWeek extends Op1(TimeNamespace, "dayOfWeek") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.dayOfWeek.get
+    def fraction(d: DateTime) = d.dayOfWeek.get
   }
 
   object HourOfDay extends Op1(TimeNamespace, "hourOfDay") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.hourOfDay.get
+    def fraction(d: DateTime) = d.hourOfDay.get
   }
 
   object MinuteOfHour extends Op1(TimeNamespace, "minuteOfHour") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.minuteOfHour.get
+    def fraction(d: DateTime) = d.minuteOfHour.get
   }
 
   object SecondOfMinute extends Op1(TimeNamespace, "secondOfMinute") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.secondOfMinute.get
+    def fraction(d: DateTime) = d.secondOfMinute.get
   }
     
   object MillisOfSecond extends Op1(TimeNamespace, "millisOfSecond") with TimeFraction {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // def fraction(d: DateTime) = d.millisOfSecond.get
+    def fraction(d: DateTime) = d.millisOfSecond.get
   }
 
   trait TimeTruncation extends Op1 {
+    def fmt: DateTimeFormatter
+
+    def f1: F1 = new CF1P({
+      case c: DateColumn => new Map1Column(c) with DateColumn {
+        def apply(row: Int) = {
+          val time = c(row)
+
+          if (isValidISO(time)) {
+            val newTime = ISODateTimeFormat.dateTimeParser().withOffsetParsed.parseDateTime(time)
+            fmt.print(newTime)
+          } else sys.error("todo")
+        }
+      }
+    })
+
     /* val operandType = Some(SString)
     val operation: PartialFunction[SValue, SValue] = {
       case SString(time) if isValidISO(time) => 
@@ -401,122 +482,50 @@ trait TimeLib extends GenOpcode with ImplLibrary {
   }
 
   object Date extends Op1(TimeNamespace, "date") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.date()
+    val fmt = ISODateTimeFormat.date()
   }
 
   object YearMonth extends Op1(TimeNamespace, "yearMonth") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.yearMonth()
+    val fmt = ISODateTimeFormat.yearMonth()
   }
 
   object YearDayOfYear extends Op1(TimeNamespace, "yearDayOfYear") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.ordinalDate()
+    val fmt = ISODateTimeFormat.ordinalDate()
   }
 
   object MonthDay extends Op1(TimeNamespace, "monthDay") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = DateTimeFormat.forPattern("MM-dd")
+    val fmt = DateTimeFormat.forPattern("MM-dd")
   }
 
   object DateHour extends Op1(TimeNamespace, "dateHour") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.dateHour()
+    val fmt = ISODateTimeFormat.dateHour()
   }
 
   object DateHourMinute extends Op1(TimeNamespace, "dateHourMin") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.dateHourMinute()
+    val fmt = ISODateTimeFormat.dateHourMinute()
   }
 
   object DateHourMinuteSecond extends Op1(TimeNamespace, "dateHourMinSec") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.dateHourMinuteSecond()
+    val fmt = ISODateTimeFormat.dateHourMinuteSecond()
   }
 
   object DateHourMinuteSecondMillis extends Op1(TimeNamespace, "dateHourMinSecMilli") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.dateHourMinuteSecondMillis()
+    val fmt = ISODateTimeFormat.dateHourMinuteSecondMillis()
   }
 
   object TimeWithZone extends Op1(TimeNamespace, "timeWithZone") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.time()
+    val fmt = ISODateTimeFormat.time()
   }
 
   object TimeWithoutZone extends Op1(TimeNamespace, "timeWithoutZone") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.hourMinuteSecondMillis()
+    val fmt = ISODateTimeFormat.hourMinuteSecondMillis()
   }
 
   object HourMinute extends Op1(TimeNamespace, "hourMin") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.hourMinute()
+    val fmt = ISODateTimeFormat.hourMinute()
   }
 
   object HourMinuteSecond extends Op1(TimeNamespace, "hourMinSec") with TimeTruncation {
-    def f1: F1 = new CF1P({
-      case c: StrColumn => new Map1Column(c) with StrColumn {
-        def apply(row: Int) = 
-      }
-    })
-    
-    // val fmt = ISODateTimeFormat.hourMinuteSecond()
+    val fmt = ISODateTimeFormat.hourMinuteSecond()
   }
 }
