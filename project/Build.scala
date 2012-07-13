3caf80c0c6366d43ca3be1c2f4fa64922dd21c1e
Derek Chen-Becker <derek@reportgrid.com>
2012-07-13 13:56:32 -0600

    Bump version for 1.1.2 release

--- project/Build.scala
+++ project/Build.scala
@@ -45,7 +45,7 @@ object PlatformBuild extends Build {
   
   val commonSettings = Seq(
     organization := "com.precog",
-    version := "1.1.2-SNAPSHOT",
+    version := "1.1.2",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-g:none"),
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
