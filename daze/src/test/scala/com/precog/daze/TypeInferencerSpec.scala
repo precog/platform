faedf661019b76206b0f387485de2822bf1d7d66
Miles Sabin <miles@milessabin.com>
2012-07-18 20:22:24 +0100

    Split structure/type inference.

--- daze/src/test/scala/com/precog/daze/TypeInferencerSpec.scala
+++ daze/src/test/scala/com/precog/daze/TypeInferencerSpec.scala
@@ -26,7 +26,7 @@ class TypeInferencerSpec extends Specification
     BuiltInFunction2Op,
     Add, Neg,
     DerefArray, DerefObject,
-    ArraySwap, WrapObject,
+    ArraySwap, WrapObject, JoinObject,
     Map2Cross, Map2CrossLeft, Map2CrossRight, Map2Match,
     PushString, PushNum
   }
@@ -50,9 +50,34 @@ class TypeInferencerSpec extends Specification
     flattenAux(jtpe).groupBy(_._1).mapValues(_.flatMap(_._2))
   }
 
-  def extractLoads(graph : DepGraph) : Map[String, Map[JPath, Set[CType]]] = {
+  def extractLoads(graph : DepGraph): Map[String, Map[JPath, Set[CType]]] = {
+    
+    def merge(left: Map[String, Map[JPath, Set[CType]]], right: Map[String, Map[JPath, Set[CType]]]): Map[String, Map[JPath, Set[CType]]] = {
+      def mergeAux(left: Map[JPath, Set[CType]], right: Map[JPath, Set[CType]]): Map[JPath, Set[CType]] = {
+        left ++ right.map { case (path, ctpes) => path -> (ctpes ++ left.getOrElse(path, Set())) }
+      }
+      left ++ right.map { case (file, jtpes) => file -> mergeAux(jtpes, left.getOrElse(file, Map())) }
+    }
+
+    def extractSpecLoads(spec: BucketSpec):  Map[String, Map[JPath, Set[CType]]] = spec match {
+      case UnionBucketSpec(left, right) =>
+        merge(extractSpecLoads(left), extractSpecLoads(right)) 
+      
+      case IntersectBucketSpec(left, right) =>
+        merge(extractSpecLoads(left), extractSpecLoads(right)) 
+      
+      case Group(id, target, child) =>
+        merge(extractLoads(target), extractSpecLoads(child)) 
+      
+      case UnfixedSolution(id, target) =>
+        extractLoads(target)
+      
+      case Extra(target) =>
+        extractLoads(target)
+    }
+    
     graph match {
-      case r : Root => Map.empty
+      case _ : Root => Map()
 
       case New(loc, parent) => extractLoads(parent)
 
@@ -64,11 +89,11 @@ class TypeInferencerSpec extends Specification
 
       case Morph1(loc, m, parent) => extractLoads(parent)
 
-      case Morph2(loc, m, left, right) => extractLoads(left) ++ extractLoads(right)
+      case Morph2(loc, m, left, right) => merge(extractLoads(left), extractLoads(right))
 
-      case Join(loc, instr, left, right) => extractLoads(left) ++ extractLoads(right)
+      case Join(loc, instr, left, right) => merge(extractLoads(left), extractLoads(right))
 
-      case Filter(loc, cross, target, boolean) => extractLoads(target) ++ extractLoads(boolean)
+      case Filter(loc, cross, target, boolean) => merge(extractLoads(target), extractLoads(boolean))
 
       case Sort(parent, indices) => extractLoads(parent)
 
@@ -76,15 +101,30 @@ class TypeInferencerSpec extends Specification
 
       case Distinct(loc, parent) => extractLoads(parent)
 
-      case Split(loc, spec, child) => extractLoads(child)
-
-      case s @ SplitGroup(loc, id, provenance) => extractLoads(s.parent)
-
-      case s @ SplitParam(loc, id) => extractLoads(s.parent)
+      case Split(loc, spec, child) => merge(extractSpecLoads(spec), extractLoads(child))
+      
+      case SplitGroup(_, _, _) | SplitParam(_, _) => Map() 
     }
   }
 
   "type inference" should {
+    "propagate structure/type information through a trivial Join/DerefObject node" in {
+      val line = Line(0, "")
+
+      val input =
+        Join(line, Map2Cross(DerefObject),
+          LoadLocal(line, Root(line, PushString("/file"))),
+          Root(line, PushString("column")))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+      
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull))
+      )
+
+      result must_== expected
+    }
+
     "propagate structure/type information through New nodes" in {
       val line = Line(0, "")
 
@@ -92,7 +132,7 @@ class TypeInferencerSpec extends Specification
         Operate(line, Neg,
           New(line,
             Join(line, Map2Cross(DerefObject), 
-              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              LoadLocal(line, Root(line, PushString("/file"))),
               Root(line, PushString("column")))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -110,7 +150,7 @@ class TypeInferencerSpec extends Specification
       val input =
         Operate(line, Neg,
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            LoadLocal(line, Root(line, PushString("/file"))),
             Root(line, PushString("column"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -128,7 +168,7 @@ class TypeInferencerSpec extends Specification
       val input =
         Reduce(line, Mean,
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            LoadLocal(line, Root(line, PushString("/file"))),
             Root(line, PushString("column"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -146,7 +186,7 @@ class TypeInferencerSpec extends Specification
       val input =
         Morph1(line, Median,
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            LoadLocal(line, Root(line, PushString("/file"))),
             Root(line, PushString("column"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -164,10 +204,10 @@ class TypeInferencerSpec extends Specification
       val input =
         Morph2(line, Covariance,
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column0"))),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            LoadLocal(line, Root(line, PushString("/file1"))),
             Root(line, PushString("column1"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -187,7 +227,7 @@ class TypeInferencerSpec extends Specification
         Operate(line, Neg,
           New(line,
             Join(line, Map2Cross(DerefArray), 
-              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              LoadLocal(line, Root(line, PushString("/file"))),
               Root(line, PushNum("0")))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -205,10 +245,10 @@ class TypeInferencerSpec extends Specification
       val input =
         Join(line, Map2Cross(ArraySwap),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column0"))),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            LoadLocal(line, Root(line, PushString("/file1"))),
             Root(line, PushString("column1"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -227,10 +267,10 @@ class TypeInferencerSpec extends Specification
       val input =
         Join(line, Map2Cross(WrapObject),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column0"))),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            LoadLocal(line, Root(line, PushString("/file1"))),
             Root(line, PushString("column1"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -249,17 +289,19 @@ class TypeInferencerSpec extends Specification
       val input =
         Join(line, Map2Match(BuiltInFunction2Op(min)),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column0"))),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column1"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
 
       val expected = Map(
-        "/file0" -> Map(JPath("column0") -> Set(CLong, CDouble, CDecimalArbitrary)),
-        "/file1" -> Map(JPath("column1") -> Set(CLong, CDouble, CDecimalArbitrary))
+        "/file0" -> Map(
+          JPath("column0") -> Set(CLong, CDouble, CDecimalArbitrary),
+          JPath("column1") -> Set(CLong, CDouble, CDecimalArbitrary)
+        )
       )
 
       result must_== expected
@@ -271,10 +313,10 @@ class TypeInferencerSpec extends Specification
       val input =
         Filter(line, None,
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            LoadLocal(line, Root(line, PushString("/file0"))),
             Root(line, PushString("column0"))),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            LoadLocal(line, Root(line, PushString("/file1"))),
             Root(line, PushString("column1"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -294,7 +336,7 @@ class TypeInferencerSpec extends Specification
         Operate(line, Neg,
           Sort(
             Join(line, Map2Cross(DerefObject), 
-              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              LoadLocal(line, Root(line, PushString("/file"))),
               Root(line, PushString("column"))),
             Vector()
           )
@@ -316,7 +358,7 @@ class TypeInferencerSpec extends Specification
         Operate(line, Neg,
           Memoize(
             Join(line, Map2Cross(DerefObject), 
-              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              LoadLocal(line, Root(line, PushString("/file"))),
               Root(line, PushString("column"))),
             23
           )
@@ -338,7 +380,7 @@ class TypeInferencerSpec extends Specification
         Operate(line, Neg,
           Distinct(line,
             Join(line, Map2Cross(DerefObject), 
-              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              LoadLocal(line, Root(line, PushString("/file"))),
               Root(line, PushString("column")))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
@@ -349,17 +391,51 @@ class TypeInferencerSpec extends Specification
 
       result must_== expected
     }
+    
+    "propagate structure/type information through Split nodes" in {
+      val line = Line(0, "")
 
+      def clicks = LoadLocal(line, Root(line, PushString("/file")))
+
+      lazy val input: Split =
+        Split(line,
+          Group(
+            1,
+            clicks,
+            UnfixedSolution(0, 
+              Join(line, Map2Cross(DerefObject),
+                clicks,
+                Root(line, PushString("column0"))))),
+          Join(line, Map2Match(Add),
+            Join(line, Map2Cross(DerefObject),
+              SplitParam(line, 0)(input),
+              Root(line, PushString("column1"))),
+            Join(line, Map2Cross(DerefObject),
+              SplitGroup(line, 1, clicks.provenance)(input),
+              Root(line, PushString("column2")))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(
+          JPath.Identity -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull),
+          JPath("column0") -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull)
+        )
+      )
+
+      result must_== expected
+    }
+    
     "rewrite loads for a trivial but complete DAG such that they will restrict the columns loaded" in {
       val line = Line(0, "")
 
       val input =
         Join(line, Map2Match(Add),
           Join(line, Map2Cross(DerefObject), 
-            dag.LoadLocal(line, Root(line, PushString("/clicks"))),
+            LoadLocal(line, Root(line, PushString("/clicks"))),
             Root(line, PushString("time"))),
           Join(line, Map2Cross(DerefObject),
-            dag.LoadLocal(line, Root(line, PushString("/hom/heightWeight"))),
+            LoadLocal(line, Root(line, PushString("/hom/heightWeight"))),
             Root(line, PushString("height"))))
 
       val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
