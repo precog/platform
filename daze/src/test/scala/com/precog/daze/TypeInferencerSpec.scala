3edc44fcabb092748fee9ee4704e84aa0714b8b4
Miles Sabin <miles@milessabin.com>
2012-07-18 13:54:40 +0100

    Filling out tests.

--- daze/src/test/scala/com/precog/daze/TypeInferencerSpec.scala
+++ daze/src/test/scala/com/precog/daze/TypeInferencerSpec.scala
@@ -13,14 +13,22 @@ import org.specs2.mutable._
 import scalaz._
 
 class TypeInferencerSpec extends Specification
-  with DAG
-  with RandomLibrary
-  with TypeInferencer {
+  with TestConfigComponent 
+  with ReductionLib 
+  with StatsLib
+  with MathLib
+  with InfixLib
+  with MemoryDatasetConsumer {
 
   import dag._
   import instructions.{
-    Add, DerefArray, DerefObject, Line, Map2Cross,
-    Map2CrossLeft, Map2CrossRight, Map2Match, PushString
+    Line,
+    BuiltInFunction2Op,
+    Add, Neg,
+    DerefArray, DerefObject,
+    ArraySwap, WrapObject,
+    Map2Cross, Map2CrossLeft, Map2CrossRight, Map2Match,
+    PushString, PushNum
   }
   import bytecode._
 
@@ -77,7 +85,272 @@ class TypeInferencerSpec extends Specification
   }
 
   "type inference" should {
-    "rewrite loads such that they will restrict the columns loaded" in {
+    "propagate structure/type information through New nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          New(line,
+            Join(line, Map2Cross(DerefObject), 
+              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              Root(line, PushString("column")))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Operate nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            Root(line, PushString("column"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Reduce nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Reduce(line, Mean,
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            Root(line, PushString("column"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Morph1 nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Morph1(line, Median,
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file"))),
+            Root(line, PushString("column"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Morph2 nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Morph2(line, Covariance,
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            Root(line, PushString("column0"))),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            Root(line, PushString("column1"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file0" -> Map(JPath("column0") -> Set(CLong, CDouble, CDecimalArbitrary)),
+        "/file1" -> Map(JPath("column1") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through DerefArray Join nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          New(line,
+            Join(line, Map2Cross(DerefArray), 
+              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              Root(line, PushNum("0")))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath(0) -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through ArraySwap Join nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Join(line, Map2Cross(ArraySwap),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            Root(line, PushString("column0"))),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            Root(line, PushString("column1"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file0" -> Map(JPath("column0") -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull)),
+        "/file1" -> Map(JPath("column1") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through WrapObject Join nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Join(line, Map2Cross(WrapObject),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            Root(line, PushString("column0"))),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            Root(line, PushString("column1"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file0" -> Map(JPath("column0") -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull)),
+        "/file1" -> Map(JPath("column1") -> Set(CStringArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Op2 Join nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Join(line, Map2Match(BuiltInFunction2Op(min)),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            Root(line, PushString("column0"))),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            Root(line, PushString("column1"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file0" -> Map(JPath("column0") -> Set(CLong, CDouble, CDecimalArbitrary)),
+        "/file1" -> Map(JPath("column1") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Filter nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Filter(line, None,
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file0"))),
+            Root(line, PushString("column0"))),
+          Join(line, Map2Cross(DerefObject), 
+            dag.LoadLocal(line, Root(line, PushString("/file1"))),
+            Root(line, PushString("column1"))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file0" -> Map(JPath("column0") -> Set(CBoolean, CLong, CDouble, CDecimalArbitrary, CStringArbitrary, CNull)),
+        "/file1" -> Map(JPath("column1") -> Set(CBoolean))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Sort nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          Sort(
+            Join(line, Map2Cross(DerefObject), 
+              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              Root(line, PushString("column"))),
+            Vector()
+          )
+        )
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Memoize nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          Memoize(
+            Join(line, Map2Cross(DerefObject), 
+              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              Root(line, PushString("column"))),
+            23
+          )
+        )
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "propagate structure/type information through Distinct nodes" in {
+      val line = Line(0, "")
+
+      val input =
+        Operate(line, Neg,
+          Distinct(line,
+            Join(line, Map2Cross(DerefObject), 
+              dag.LoadLocal(line, Root(line, PushString("/file"))),
+              Root(line, PushString("column")))))
+
+      val result = extractLoads(inferTypes(JType.JPrimitiveUnfixedT)(input))
+
+      val expected = Map(
+        "/file" -> Map(JPath("column") -> Set(CLong, CDouble, CDecimalArbitrary))
+      )
+
+      result must_== expected
+    }
+
+    "rewrite loads for a trivial but complete DAG such that they will restrict the columns loaded" in {
       val line = Line(0, "")
 
       val input =
