10e5f6cc363464e4400412b5d13a684ffe6e34e8
Tom Switzer <thomas.switzer@gmail.com>
2012-07-26 09:53:42 -0600

    Initial sketch of performance test runner.

---
+++ ragnarock/src/main/scala/com/precog/ragnarock/Timer.scala
@@ -0,0 +1,64 @@
+package com.precog.ragnarock
+
+import scalaz._
+
+import org.joda.time.{ Instant, Interval }
+
+
+trait Timer {
+
+  /** A single instance in time. */
+  type Time
+
+  /** A span between 2 instances in time. */
+  type TimeSpan
+
+  /** Returns the current time. */
+  def now(): Time
+
+  /** Returns the time span between 2 times. */
+  def duration(start: Time, end: Time): TimeSpan
+
+  /**
+   * The product of 2 `TimeSpan`s should be the extents of the union of both.
+   * Since there is no identity in this case, `TimeSpan` is a `Semigroup`.
+   * Wrap it in an `Option` to make it a `Monoid`.
+   */
+  implicit def TimeSpanSemigroup: Semigroup[TimeSpan]
+}
+
+
+trait SimpleTimer extends Timer {
+  type Time = Long
+  type TimeSpan = (Time, Time)
+
+  def now(): Time = System.nanoTime()
+
+  def duration(start: Time, end: Time): TimeSpan = (start, end)
+
+  val TimeSpanSemigroup = new Semigroup[TimeSpan] {
+    def append(a: TimeSpan, b: => TimeSpan): TimeSpan =
+      (a._1 min b._1, a._2 max b._2)
+  }
+}
+
+
+
+trait JodaTimer extends Timer {
+  type Time = Instant
+  type TimeSpan = Interval
+
+  def now(): Time = new Instant()
+  def duration(start: Time, end: Time) = new Interval(start, end)
+
+  val TimeSpanSemigroup = new Semigroup[TimeSpan] {
+    def append(a: TimeSpan, b: => TimeSpan) = {
+      val start = if (b.getStart isBefore a.getStart) b.getStart else a.getStart
+      val end = if (a.getEnd isBefore b.getEnd) b.getEnd else a.getEnd
+      new Interval(start, end)
+    }
+  }
+}
+
+
+
