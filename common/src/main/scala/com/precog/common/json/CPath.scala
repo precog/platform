ea2e28412e489b074f0bd708b5f2301139863b10
Tom Switzer <thomas.switzer@gmail.com>
2012-08-22 21:20:48 -0600

    Add CPathArray as type of CPathNode.

--- common/src/main/scala/com/precog/common/json/CPath.scala
+++ common/src/main/scala/com/precog/common/json/CPath.scala
@@ -143,6 +143,10 @@ sealed case class CPathIndex(index: Int) extends CPathNode {
   override def toString = "[" + index + "]"
 }
 
+case object CPathArray extends CPathNode {
+  override def toString = "[*]"
+}
+
 trait CPathSerialization {
   implicit val CPathDecomposer : Decomposer[CPath] = new Decomposer[CPath] {
     def decompose(cpath: CPath) : JValue = JString(cpath.toString)
