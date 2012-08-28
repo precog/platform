22967db79477f2f9c6c3209d4b146757f02c5764
Derek Chen-Becker <derek@precog.com>
2012-08-28 11:01:14 -0600

    Tweaks to the data generation script in prep for automating data gen

--- regen-jdbm-data.sh
+++ regen-jdbm-data.sh
@@ -19,8 +19,28 @@
 ## 
 #!/bin/bash
 
-DATADIR=`dirname $0`/pandora/dist/data-jdbm/data
+if [[ $# != 1 ]]; then
+    echo "Usage: `basename $0` <target data directory>"
+    echo "  For now target is normally pandora/dist/data-jdbm/data/"
+    exit
+fi
+
+SRCDIR=muspelheim/src/test/resources/test_data
+DATADIR=$1
+
+cd `dirname $0`
+
+pushd $SRCDIR > /dev/null
+SOURCES=""
+for source in `find . -name '*.json'`; do
+    DPATH=`echo $source | sed -e 's/^\.\(.*\)\.json/\1\//'`
+    DATAFILE=${source/\.\//}
+    SOURCES="$SOURCES $DPATH=$SRCDIR/$DATAFILE"
+done
+popd > /dev/null
+
+[ -f yggdrasil/target/yggdrasil-assembly-2.0.0-SNAPSHOT.jar ] || sbt yggdrasil/assembly
 
 rm -rf $DATADIR/*
 
-java -cp yggdrasil/target/yggdrasil-assembly-2.0.0-SNAPSHOT.jar com.precog.yggdrasil.util.YggUtils import -t C18ED787-BF07-4097-B819-0415C759C8D5 -s $DATADIR '/views=muspelheim/src/test/resources/test_data/views.json' '/clicks/=muspelheim/src/test/resources/test_data/clicks.json' '/campaigns/=muspelheim/src/test/resources/test_data/campaigns.json' '/obnoxious/=muspelheim/src/test/resources/test_data/obnoxious.json' '/organizations=muspelheim/src/test/resources/test_data/organizations.json' '/richie1/test=muspelheim/src/test/resources/test_data/richie1/test.json' '/test/empty_object=muspelheim/src/test/resources/test_data/test/empty_object.json' '/test/null=muspelheim/src/test/resources/test_data/test/null.json' '/test/empty_array=muspelheim/src/test/resources/test_data/test/empty_array.json' '/fastspring_nulls/=muspelheim/src/test/resources/test_data/fastspring_nulls.json' '/fastspring_mixed_type/=muspelheim/src/test/resources/test_data/fastspring_mixed_type.json'
+java -cp yggdrasil/target/yggdrasil-assembly-2.0.0-SNAPSHOT.jar com.precog.yggdrasil.util.YggUtils import -t C18ED787-BF07-4097-B819-0415C759C8D5 -s $DATADIR $SOURCES
