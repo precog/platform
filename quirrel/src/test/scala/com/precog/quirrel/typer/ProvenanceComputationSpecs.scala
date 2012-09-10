2af0a15a455ac17d634f760e1d01606343f20df0
Alissa Pajer <alissa.pajer@gmail.com>
2012-09-10 16:22:36 -0600

    fixed dispatches

--- quirrel/src/test/scala/com/precog/quirrel/typer/ProvenanceComputationSpecs.scala
+++ quirrel/src/test/scala/com/precog/quirrel/typer/ProvenanceComputationSpecs.scala
@@ -22,7 +22,7 @@ object ProvenanceComputationSpecs extends Specification
       forall(lib1) { f =>
         val tree = parse("""
           clicks := //clicks
-          foo('a) := %s('a) 
+          foo(a) := %s(a) 
           foo(clicks)""".format(f.fqn))
 
         tree.provenance mustEqual StaticProvenance("/clicks")
@@ -34,7 +34,7 @@ object ProvenanceComputationSpecs extends Specification
       forall(lib2) { f =>
         val tree = parse("""
           clicks := //clicks
-          foo('a, 'b) := %s('a, 'b) 
+          foo(a, b) := %s(a, b) 
           foo(clicks.a, clicks.b)""".format(f.fqn))
 
         tree.provenance mustEqual StaticProvenance("/clicks")
@@ -46,7 +46,7 @@ object ProvenanceComputationSpecs extends Specification
       forall(libReduction) { f =>
         val tree = parse("""
           clicks := //clicks
-          foo('a) := %s('a) 
+          foo(a) := %s(a) 
           foo(clicks.a)""".format(f.fqn))
 
         tree.provenance mustEqual ValueProvenance
@@ -423,7 +423,7 @@ object ProvenanceComputationSpecs extends Specification
     "identify op2 dispatch according to its children given set related by ~" in {
       forall(lib2) { f =>
         val tree = compile("""//foo ~ //bar %s(//foo, //bar)""".format(f.fqn))
-        tree.provenance must beLike { case DynamicProvenance(_) => ok }
+        tree.provenance mustEqual UnionProvenance(StaticProvenance("/foo"), StaticProvenance("/bar"))
         tree.errors must beEmpty
       }
     }
