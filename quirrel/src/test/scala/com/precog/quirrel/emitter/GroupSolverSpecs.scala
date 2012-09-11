2b29ef1daf6cc35f0380960bf8b0341c55fed28d
Daniel Spiewak <daniel@reportgrid.com>
2012-09-11 16:25:33 -0600

    GroupSolverSpecs now passing

--- quirrel/src/test/scala/com/precog/quirrel/emitter/GroupSolverSpecs.scala
+++ quirrel/src/test/scala/com/precog/quirrel/emitter/GroupSolverSpecs.scala
@@ -35,10 +35,8 @@ object GroupSolverSpecs extends Specification
           origin @ Where(_, target, pred @ Eq(_, solution, _)))) = compile(input)
           
       val expected = Group(origin, target, UnfixedSolution("'day", solution))
-      println(ExprUtils.buildChains(Map())(target))
-      println(ExprUtils.buildChains(Map())(pred))
       tree.errors must beEmpty
-      // tree.buckets must beSome(expected)
+      tree.buckets must beSome(expected)
     }
     
     "identify composite bucket for trivial cf example with conjunction" in {
@@ -54,7 +52,7 @@ object GroupSolverSpecs extends Specification
           UnfixedSolution("'day", rightSol)))
       
       tree.errors must beEmpty
-      // tree.buckets must beSome(expected)
+      tree.buckets must beSome(expected)
     }
     
     "identify separate buckets for independent tic variables on same set" in {
@@ -80,7 +78,7 @@ object GroupSolverSpecs extends Specification
           UnfixedSolution("'b", solB)))
       
       tree.errors must beEmpty
-      // tree.buckets must beSome(expected)
+      tree.buckets must beSome(expected)
     }
     
     "identify separate buckets for independent tic variables on different sets" in {
@@ -109,7 +107,7 @@ object GroupSolverSpecs extends Specification
           UnfixedSolution("'b", solB)))
       
       tree.errors must beEmpty
-      // tree.buckets must beSome(expected)
+      tree.buckets must beSome(expected)
     }
     
     "produce an error when a single tic-variable lacks a defining set" in {
