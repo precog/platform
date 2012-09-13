e661d0a9b763810b7652fba92495f01e1800c649
Miles Sabin <miles@milessabin.com>
2012-09-13 17:05:23 +0100

    Fixed bug in trans.EqualLiteral which unbreaks test for And/Or in Filter.

--- daze/src/test/scala/com/precog/daze/EvaluatorSpecs.scala
+++ daze/src/test/scala/com/precog/daze/EvaluatorSpecs.scala
@@ -2120,8 +2120,8 @@ trait EvaluatorSpecs[M[+_]] extends Specification
           result2 must contain(42, 12, 1, true, false, "daniel",
             Map("test" -> SString("fubar")), Vector())
         }
-      }.pendingUntilFixed
-      
+      }
+
       "or" >> {
         val line = Line(0, "")
         val numbers = dag.LoadLocal(line, Root(line, PushString("/het/numbers")))
@@ -2145,8 +2145,8 @@ trait EvaluatorSpecs[M[+_]] extends Specification
           
           result2 must contain(77, 13)
         }
-      }.pendingUntilFixed
-      
+      }
+
       "complement of equality" >> {
         val line = Line(0, "")
         val numbers = dag.LoadLocal(line, Root(line, PushString("/het/numbers")))
