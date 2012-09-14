edf3ef6ab16648ae0458a9f776f4b3e16b3771c2
Tom Switzer <thomas.switzer@gmail.com>
2012-09-14 10:00:47 -0600

    Add offset, limit, sortOn and sortOrder as API/Query option.

--- daze/src/main/scala/com/precog/daze/QueryExecutor.scala
+++ daze/src/main/scala/com/precog/daze/QueryExecutor.scala
@@ -1,8 +1,10 @@
 package com.precog
 package daze
 
+import com.precog.yggdrasil.TableModule
 import com.precog.common._
 
+import blueeyes.json.JPath
 import blueeyes.json.JsonAST._
 
 import akka.actor.ActorSystem
@@ -23,8 +25,13 @@ object EvaluationError {
   val timeoutError: EvaluationError = TimeoutError
 }
 
+case class QueryOptions(
+  page: Option[(Int, Int)] = None,
+  sortOn: List[JPath] = Nil,
+  sortOrder: TableModule.DesiredSortOrder = TableModule.SortAscending)
+
 trait QueryExecutor[M[+_]] {
-  def execute(userUID: String, query: String, prefix: Path): Validation[EvaluationError, StreamT[M, List[JValue]]]
+  def execute(userUID: String, query: String, prefix: Path, opts: QueryOptions): Validation[EvaluationError, StreamT[M, List[JValue]]]
   def browse(userUID: String, path: Path): M[Validation[String, JArray]]
   def structure(userUID: String, path: Path): M[Validation[String, JObject]]
   def status(): M[Validation[String, JValue]]
@@ -36,7 +43,7 @@ trait NullQueryExecutor extends QueryExecutor[Id.Id] {
   def actorSystem: ActorSystem
   implicit def executionContext: ExecutionContext
 
-  def execute(userUID: String, query: String, prefix: Path) = {
+  def execute(userUID: String, query: String, prefix: Path, opts: QueryOptions) = {
     failure(SystemError(new UnsupportedOperationException("Query service not avaialble")))
   }
   
