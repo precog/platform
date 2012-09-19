b728a21d18cf3beb270c1d9011c969b363ba93c0
Daniel Spiewak <daniel@reportgrid.com>
2012-09-19 15:10:30 -0600

    MegaReduce is now a ForcingPoint

--- daze/src/main/scala/com/precog/daze/DAG.scala
+++ daze/src/main/scala/com/precog/daze/DAG.scala
@@ -556,7 +556,7 @@ trait DAG extends Instructions {
   }
   
   object dag {
-    trait ForcingPoint extends DepGraph
+    sealed trait ForcingPoint extends DepGraph
     
     object ConstString {
       def unapply(graph : DepGraph) : Option[String] = graph.value match {
@@ -698,7 +698,7 @@ trait DAG extends Instructions {
       lazy val containsSplitArg = parent.containsSplitArg
     }
     
-    case class MegaReduce(loc: Line, reds: NEL[dag.Reduce], parent: DepGraph) extends DepGraph {
+    case class MegaReduce(loc: Line, reds: NEL[Reduction], parent: DepGraph) extends DepGraph with ForcingPoint {
       lazy val identities = Vector()
       
       val sorting = IdentitySort
