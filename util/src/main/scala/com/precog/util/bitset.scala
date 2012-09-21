380a6954d896b7394fc8abf82f6d5a8362a62aac
Erik Osheim <d_m@plastic-idolatry.com>
2012-09-21 12:03:03 -0600

    Fix masking error caused by new com.precog.util.BitSet implementation.

--- util/src/main/scala/com/precog/util/bitset.scala
+++ util/src/main/scala/com/precog/util/bitset.scala
@@ -19,10 +19,11 @@
  */
 package com.precog.util
 
-import scala.collection._
+import scala.collection.mutable
+import scala.collection.{BitSet => ScalaBitSet}
 
 package object bitset {
-  def makeMutable(bitSet: BitSet): mutable.BitSet = (mutable.BitSet.canBuildFrom() ++= bitSet.iterator).result()
+  def makeMutable(bitSet: ScalaBitSet): mutable.BitSet = (mutable.BitSet.canBuildFrom() ++= bitSet.iterator).result()
 }
 
 // vim: set ts=4 sw=4 et:
