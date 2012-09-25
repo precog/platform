564644389e74e898553457d3c55faa029f875900
Miles Sabin <miles@milessabin.com>
2012-09-25 11:23:55 +0100

    Fixed grant add URI in accounts service, reinstated owner check for read/reduce permissions in AccessControl and un'pended test.

--- accounts/src/main/scala/com/precog/accounts/AccountService.scala
+++ accounts/src/main/scala/com/precog/accounts/AccountService.scala
@@ -96,7 +96,7 @@ trait AccountService extends BlueEyesServiceBuilder with AkkaDefaults with Accou
                 path("'accountId") {
                   get(new GetAccountDetailsHandler(state.accountManagement)) ~ 
                   delete(new DeleteAccountHandler(state.accountManagement)) ~
-                  path("/grants") {
+                  path("/grants/") {
                     post(new CreateAccountGrantHandler(state.accountManagement, state.securityService))
                   } ~
                   path("/plan") {
