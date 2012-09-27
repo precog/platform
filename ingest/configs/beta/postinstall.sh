81d1ff83d8b5ff78172d87de0815e40ca39a0a34
Derek Chen-Becker <derek@precog.com>
2012-09-27 12:22:40 -0600

    Added ingest configs for deployment

---
+++ ingest/configs/beta/postinstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+# Reload the upstart config
+initctl reload-configuration
+
+# Keep monit from interrupting us
+if ! monit unmonitor -g ingest-v1-beta; then
+    echo "Monit unhappy on unmonitor of ingest-v1-beta"
+fi
+
+
+# Stop and start the service
+if status ingest-v1-beta | grep running; then
+    stop ingest-v1-beta
+fi
+
+sleep 5
+
+if ! RESULT=`start ingest-v1-beta 2>&1` > /dev/null ; then
+    if echo "$RESULT" | grep -v "already running" > /dev/null ; then
+	echo "Failure: $RESULT"
+	exit 1
+    fi
+    fi
+
+# Restart monit to pick up changes
+if ! monit reload; then
+    echo "Monit unhappy on reload"
+fi
+
+sleep 5
+
+if ! monit monitor -g ingest-v1-beta; then
+    echo "Monit unhappy on remonitor of ingest-v1-beta"
+fi
+
+# Wait 30 seconds for startup, then test the health URLs
+sleep 30
+
+echo "Running health checks"
+curl -v -f -G "http://localhost:30060/blueeyes/services/ingest/v1/health"
+echo "Completed health checks"
+
+exit 0
