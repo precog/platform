9b6f11a01e87fdc912522552ef372962c73a7aa7
Kris Nuttycombe <kris@precog.com>
2012-10-10 01:42:28 -0400

    Version bump after 2.0.1 release.

--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ object PlatformBuild extends Build {
 
   val commonSettings = Seq(
     organization := "com.precog",
-    version := "2.0.1",
+    version := "2.0.2-SNAPSHOT",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-g:none"),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
     scalaVersion := "2.9.2",
