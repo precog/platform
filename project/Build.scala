27526f784458106985c4e0492a2b1d3f0a13a561
Kris Nuttycombe <kris@precog.com>
2012-10-10 12:12:55 -0400

    Revert "Version bump after 2.0.1 release."
    This reverts commit b82a1cd65c8e3b51f850d2adde950e97b997e113.

--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ object PlatformBuild extends Build {
 
   val commonSettings = Seq(
     organization := "com.precog",
-    version := "2.0.2-SNAPSHOT",
+    version := "2.0.1",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-g:none"),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
     scalaVersion := "2.9.2",
