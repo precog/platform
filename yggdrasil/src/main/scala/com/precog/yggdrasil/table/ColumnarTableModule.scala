3fe3b1b9349aadc3fee227b101d65aee6a034f59
Tom Switzer <thomas.switzer@gmail.com>
2012-10-12 12:45:23 -0600

    Sort completely in memory.
    Conflicts:
    
    	yggdrasil/src/main/scala/com/precog/yggdrasil/table/BlockStoreColumnarTableModule.scala

--- yggdrasil/src/main/scala/com/precog/yggdrasil/table/ColumnarTableModule.scala
+++ yggdrasil/src/main/scala/com/precog/yggdrasil/table/ColumnarTableModule.scala
@@ -1070,7 +1070,7 @@ trait ColumnarTableModule[M[+_]] extends TableModule[M] with ColumnarTableTypes 
         
         for {
           sorts <- sortPairs
-          //_ = println("sorts: " + System.currentTimeMillis) 
+          // _ = println("sorts: " + System.currentTimeMillis) 
           groupedSubsets <- {
             val edgeAlignments = spanningGraph.edges flatMap {
               case MergeEdge(a, b) =>
@@ -1087,7 +1087,7 @@ trait ColumnarTableModule[M[+_]] extends TableModule[M] with ColumnarTableTypes 
                   case (aSorted, bSorted) => 
                     for {
                       aligned <- Table.align(aSorted.table, aSorted.sortedOn, bSorted.table, bSorted.sortedOn)
-                      //_ = println("aligned: " + System.currentTimeMillis)
+                      // _ = println("aligned: " + System.currentTimeMillis)
                     } yield {
                       List(
                         aSorted.copy(table = aligned._1),
@@ -1754,7 +1754,7 @@ trait ColumnarTableModule[M[+_]] extends TableModule[M] with ColumnarTableTypes 
 
       for {
         omniverse <- borgedUniverses.map(s => unionAll(s.toSet))
-        //json <- omniverse.table.toJson
+        json <- omniverse.table.toJson
         //_ = println("omniverse: \n" + json.mkString("\n"))
         sorted <- omniverse.table.compact(groupKeySpec(Source)).sort(groupKeySpec(Source))
         result <- sorted.partitionMerge(DerefObjectStatic(Leaf(Source), CPathField("groupKeys"))) { partition =>
@@ -1779,7 +1779,7 @@ trait ColumnarTableModule[M[+_]] extends TableModule[M] with ColumnarTableTypes 
                            // transform to get just the information related to the particular groupId,
                            for {
                              partitionSorted <- partition.transform(recordTrans).sort(sortByTrans, unique = true)
-                             //json <- partitionSorted.toJson
+                             json <- partitionSorted.toJson
                              //_ = println("group " + groupId + " partition: " + json.mkString("\n"))
                            } yield {
                              groupId -> partitionSorted.transform(DerefObjectStatic(Leaf(Source), CPathField("1")))
