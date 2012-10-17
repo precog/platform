0771e3036d23aa4320edde85161a9e1e0a637588
Derek Chen-Becker <derek@precog.com>
2012-10-17 13:55:10 -0600

    Fix TODO reporting script to use ack-term (to workaround no PTY for ack)
    See https://github.com/petdance/ack/issues/90 for details. Ack is broken for
    non-PTY (e.g. jenkins build) usage, so we work around with a custom python
    script that sets up a PTY

--- runTodoFixmeReport.sh
+++ runTodoFixmeReport.sh
@@ -20,7 +20,7 @@
 #!/bin/bash
 
 (echo -e "<h1>FIXMEs</h1>\n\n<pre>";
- ack -i --heading --break --scala 'FIXME';
+ ack-term --nocolor -i --heading --break --scala 'FIXME';
  echo -e "\n</pre><h1>TODOs</h1>\n\n<pre>";
- ack -i --heading --break --scala 'TODO(?!uble)';
+ ack-term --nocolor -i --heading --break --scala 'TODO(?!uble)';
  echo "</pre>") | mutt -e "set content_type=text/html" -s "FIXME/TODO report `date '+%F'`" engineering@precog.com
