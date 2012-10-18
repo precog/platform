c69fbacb05bdb94c6a3391e4759cf4e721241642
Derek Chen-Becker <derek@precog.com>
2012-10-18 14:28:46 -0600

    Updated table size to be represented by an ADT
    This allows us to distinguish between estimated sizes and fully-known
    sizes (e.g. resulting from sort, partitionMerge, etc)

--- yggdrasil/src/main/scala/com/precog/yggdrasil/table/BlockStoreColumnarTableModule.scala
+++ yggdrasil/src/main/scala/com/precog/yggdrasil/table/BlockStoreColumnarTableModule.scala
@@ -248,7 +248,7 @@ trait BlockStoreColumnarTableModule[M[+_]] extends
       )
     }
 
-    def apply(slices: StreamT[M, Slice], size: Option[Long] = None) = new Table(slices, size)
+    def apply(slices: StreamT[M, Slice], size: TableSize) = new Table(slices, size)
 
     def align(sourceLeft: Table, alignOnL: TransSpec1, sourceRight: Table, alignOnR: TransSpec1): M[(Table, Table)] = {
       sealed trait AlignState
@@ -692,7 +692,7 @@ trait BlockStoreColumnarTableModule[M[+_]] extends
         )
       )
       
-      Table(StreamT(M.point(head)), Some(totalCount)).transform(TransSpec1.DerefArray1)
+      Table(StreamT(M.point(head)), ExactSize(totalCount)).transform(TransSpec1.DerefArray1)
     }
   }
   
@@ -734,7 +734,14 @@ trait BlockStoreColumnarTableModule[M[+_]] extends
       paths          <- pathsM
       projectionData <- (paths map { path => loadable(metadataView, path, JPath.Identity, tpe) }).sequence map { _.flatten }
       val (coveringProjections, colMetadata) = projectionData.unzip
-      val maxSize = colMetadata.toList.flatMap { _.values.flatMap { _.values.collect { case stats: MetadataStats => stats.count } } }.sorted.lastOption
+      val projectionSizes = colMetadata.toList.flatMap { _.values.flatMap { _.values.collect { case stats: MetadataStats => stats.count } } }.sorted
+      val tableSize: TableSize = projectionSizes.headOption.flatMap { minSize => projectionSizes.lastOption.map { maxSize => {
+        if (coveringProjections.size == 1) {
+          ExactSize(minSize)
+        } else {
+          EstimateSize(minSize, maxSize)
+        }
+      }}}.getOrElse(UnknownSize)
     } yield {
       val head = StreamT.Skip(
         StreamT.wrapEffect(
@@ -749,11 +756,11 @@ trait BlockStoreColumnarTableModule[M[+_]] extends
         )
       )
   
-      Table(StreamT(M.point(head)), maxSize)
+      Table(StreamT(M.point(head)), tableSize)
     }
   }
 
-  class Table(slices: StreamT[M, Slice], size: Option[Long]) extends ColumnarTable(slices, size) {
+  class Table(slices: StreamT[M, Slice], size: TableSize) extends ColumnarTable(slices, size) {
     import Table._
     import SliceTransform._
     import trans._
@@ -767,7 +774,7 @@ trait BlockStoreColumnarTableModule[M[+_]] extends
      * @see com.precog.yggdrasil.TableModule#sort(TransSpec1, DesiredSortOrder, Boolean)
      */
     def sort(sortKey: TransSpec1, sortOrder: DesiredSortOrder, unique: Boolean = false): M[Table] = groupByN(Seq(sortKey), Leaf(Source), sortOrder, unique).map {
-      _.headOption getOrElse Table(this.slices, Some(0)) // If we start with an empty table, we always end with an empty table (but then we know that we have zero size)
+      _.headOption getOrElse Table(StreamT.empty[M, Slice], ExactSize(0)) // If we start with an empty table, we always end with an empty table (but then we know that we have zero size)
     }
 
     /**
