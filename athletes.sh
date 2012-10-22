e04a05ffdec854838d1ef1c4d132ffe56c1ee782
Miles Sabin <miles@milessabin.com>
2012-10-22 14:55:41 +0100

    Fixed assembly jar path.

--- athletes.sh
+++ athletes.sh
@@ -20,6 +20,6 @@
 #!/bin/bash
 
 java \
-  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -cp ragnarok/target/ragnarok-assembly-2.0.1-SNAPSHOT.jar \
+  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -cp ragnarok/target/ragnarok-assembly-2.0.1-*.jar \
   com.precog.ragnarok.test.AthletesTestSuite \
   --root-dir /home/miles/projects/eclipse/precog-2.9.2/workspace/platform/jprofiler/jprofiler.db --runs 1 --dry-runs 0 --timeout 28800 --json
