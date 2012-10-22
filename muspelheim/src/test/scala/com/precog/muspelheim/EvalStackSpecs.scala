0331d51b016ecd17bb38135690c8a094fccca1b8
Alissa Pajer <alissa.pajer@gmail.com>
2012-10-22 12:28:42 -0600

    Fixed non-terminating decimal expansion bug, exhibited when two BigDecimals were divided.
    Fixes[#38198069, #37945591]

--- muspelheim/src/test/scala/com/precog/muspelheim/EvalStackSpecs.scala
+++ muspelheim/src/test/scala/com/precog/muspelheim/EvalStackSpecs.scala
@@ -1418,6 +1418,46 @@ trait EvalStackSpecs extends Specification {
       }
     }
 
+    "accept division inside an object" in {
+      val input = """
+        | data := //conversions
+        | 
+        | x := solve 'productID
+        |   data' := data where data.product.ID = 'productID
+        |   { count: count(data' where data'.customer.isCasualGamer = false),
+        |     sum: sum(data'.marketing.uniqueVisitors  where data'.customer.isCasualGamer = false) }
+        | 
+        | { max: max(x.sum/x.count), min: min(x.sum/x.count) }
+      """.stripMargin
+
+      val results = evalE(input)
+
+      results must haveSize(1)
+
+      forall(results) {
+        case (ids, SObject(obj)) =>
+          ids must haveSize(0)
+
+          obj.keys mustEqual(Set("min", "max"))
+          (obj("min") match { case SDecimal(num) => num.toDouble ~= 862.7464285714286 }) mustEqual true
+          (obj("max") match { case SDecimal(num) => num.toDouble ~= 941.0645161290323 }) mustEqual true
+      }
+    }
+
+    "accept division of two BigDecimals" in {
+      val input = "92233720368547758073 / 12223372036854775807"
+
+      val result = evalE(input)
+
+      result must haveSize(1)
+
+      forall(result) {
+        case (ids, SDecimal(num)) =>
+          ids must haveSize(0)
+          (num.toDouble ~= 7.54568543692) mustEqual true
+      }
+    }
+
     "evaluate a solve constrained by inclusion" in {
       val input = """
         | clicks := //clicks
@@ -1454,7 +1494,7 @@ trait EvalStackSpecs extends Specification {
       }
     }
 
-    "evaluate a quantified characteristic function of two parameters" in {
+    "evaluate a function of two parameters" in {
       val input = """
         | fun(a, b) := 
         |   //campaigns where //campaigns.ageRange = a & //campaigns.gender = b
