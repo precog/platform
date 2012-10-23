c4766b1e88de38db6d76d18b1244b2e2fffc74a4
Miles Sabin <miles@milessabin.com>
2012-10-23 14:57:05 +0100

    Removed debugging artefacts.

--- athletes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-java \
-  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -cp ragnarok/target/ragnarok-assembly-2.0.1-*.jar \
-  com.precog.ragnarok.test.AthletesTestSuite \
-  --root-dir /home/miles/projects/eclipse/precog-2.9.2/workspace/platform/jprofiler/jprofiler.db --runs 1 --dry-runs 0 --timeout 28800 --json
