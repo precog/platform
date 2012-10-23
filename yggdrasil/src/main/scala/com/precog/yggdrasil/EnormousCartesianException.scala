4178906289c50beaaf33a5adc712c6a5db72c1df
Daniel Spiewak <daniel@reportgrid.com>
2012-10-23 10:17:49 -0600

    Implemented a limit on cartesian size (by default 2.4 billion)
    [Finishes #38203617]

--- yggdrasil/src/main/scala/com/precog/yggdrasil/EnormousCartesianException.scala
+++ yggdrasil/src/main/scala/com/precog/yggdrasil/EnormousCartesianException.scala
@@ -19,11 +19,7 @@
  */
 package com.precog.yggdrasil
 
-case class EnormousCartesianException(left: Option[Long], right: Option[Long]) extends RuntimeException {
-  override def getMessage = {
-    val msgM = for (ls <- left; rs <- right)
-      yield "cannot evaluate cartesian of sets with size %d and %d".format(ls, rs)
-    
-    msgM getOrElse "cannot evaluate large cartesian"
-  }
+case class EnormousCartesianException(left: TableSize, right: TableSize) extends RuntimeException {
+  override def getMessage =
+    "cannot evaluate cartesian of sets with size %s and %s".format(left, right)
 }
