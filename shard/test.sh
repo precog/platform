7714ea32116726ac31405c33ecbc032895aa5cd1
Daniel Spiewak <daniel@reportgrid.com>
2012-10-24 14:11:37 -0600

    Factored out the Jenkins build into a committed script; attempting to avoid issues with conflicting shard/test instances

--- shard/test.sh
+++ shard/test.sh
@@ -19,4 +19,4 @@
 ## 
 #!/bin/sh
 cd $(dirname $0)/..
-exec ./run.sh -q shard/src/test/resources/queries/ muspelheim/src/test/resources/test_data/*.json
+exec ./run.sh $* -q shard/src/test/resources/queries/ muspelheim/src/test/resources/test_data/*.json
