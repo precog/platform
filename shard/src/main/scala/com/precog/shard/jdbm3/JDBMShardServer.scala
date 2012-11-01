578cace038d4fdbc938f512554a75d3b9d214695
Kris Nuttycombe <kris@precog.com>
2012-11-01 14:55:41 -0600

    Restored shard main class.

--- shard/src/main/scala/com/precog/shard/jdbm3/JDBMShardServer.scala
+++ shard/src/main/scala/com/precog/shard/jdbm3/JDBMShardServer.scala
@@ -17,22 +17,22 @@
  * program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-name := "shard"
+package com.precog.shard
+package jdbm3
 
-libraryDependencies ++= Seq(
-      "org.apache"                %% "kafka-core"         % "0.7.5",
-      "joda-time"                 % "joda-time"           % "1.6.2",
-      "ch.qos.logback"            % "logback-classic"     % "1.0.0"
-)
+import com.precog.common.security._
+import com.precog.ingest.service.NullUsageLogging
 
-ivyXML :=
-  <dependencies>
-    <dependency org="org.apache" name="kafka-core_2.9.2" rev="0.7.5">
-      <exclude org="com.sun.jdmk"/>
-      <exclude org="com.sun.jmx"/>
-      <exclude org="javax.jms"/>
-      <exclude org="jline"/>
-    </dependency>
-  </dependencies>
+import blueeyes.BlueEyesServer
+import blueeyes.util.Clock
 
-mainClass := Some("com.precog.shard.jdbm3.JDBMShardServer")
+import org.streum.configrity.Configuration
+
+object JDBMShardServer extends BlueEyesServer with ShardService with JDBMQueryExecutorComponent with MongoAPIKeyManagerComponent {
+  
+  val clock = Clock.System
+
+  def usageLoggingFactory(config: Configuration) = new NullUsageLogging("")
+
+  val asyncContext = defaultFutureDispatch
+}
