25d2e38e7eb2eaf3aa84cdfda764d02ed61b7df0
Kris Nuttycombe <kris@precog.com>
2012-11-01 17:05:09 -0600

    Shlepped test slice building code in to build slices from Mongo.

--- mongo/build.sbt
+++ mongo/build.sbt
@@ -21,5 +21,6 @@
 name := "mongo"
 
 libraryDependencies ++= Seq(
+  "com.reportgrid"          %% "blueeyes-mongo"       % "1.0.0-SNAPSHOT" changing(),
   "org.mongodb" % "mongo-java-driver"   % "2.9.3"
 )
--- mongo/src/main/scala/com/precog/yggdrasil/mongo/MongoColumnarTableModule.scala
+++ mongo/src/main/scala/com/precog/yggdrasil/mongo/MongoColumnarTableModule.scala
@@ -35,6 +35,10 @@ import com.precog.util.BitSetUtil.Implicits._
 
 import akka.dispatch.Future
 
+import blueeyes.json.JsonAST._
+import blueeyes.persistence.mongo.json._
+import BijectionsMongoJson._
+
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 import com.mongodb.WriteConcern;
@@ -82,8 +86,122 @@ trait MongoColumnarTableModule extends BlockStoreColumnarTableModule[Future] {
       for {
         paths <- pathsM(table)
       } yield {
-        sys.error("todo")
+        Table(
+          StreamT.unfoldM[Future, Slice, (List[Path], Option[DBCursor])]((paths.toList, None)) { 
+            case (paths, Some(cursor)) => 
+              M.point {
+                val (slice, remainder) = makeSlice(cursor)
+                Some(slice, (paths, remainder))
+              }
+
+            case (path :: xs, None) => 
+              path.elements.toList match {
+                case dbName :: collectionName :: Nil =>
+                  M.point {
+                    val coll = mongo.getDB(dbName).getCollection(collectionName)
+                    val cursor = coll.find()
+                    val (slice, remainder) = makeSlice(cursor)
+                    Some((slice, (xs, remainder)))
+                  }
+
+                case err => 
+                  sys.error("MongoDB path " + path.path + " does not have the form /dbName/collectionName; rollups not yet supported.")
+              }
+
+            case (Nil, None) => 
+              M.point(None)
+          },
+          UnknownSize
+        )
       }
+    }
+
+    def makeSlice(cursor: DBCursor): (Slice, Option[DBCursor]) = {
+      @tailrec def buildColArrays(from: DBCursor, into: Map[ColumnRef, (BitSet, Array[_])], sliceIndex: Int): (Map[ColumnRef, (BitSet, Object)], Int) = {
+        if (from.hasNext) {
+          // horribly inefficient, but a place to start
+          // FIXME: get objectId in the right place
+          val Success(jv) = MongoToJson(from.next())
+          val withIdsAndValues = jv.flattenWithPath.foldLeft(into) {
+            case (acc, (jpath, JNothing)) => acc
+            case (acc, (jpath, v)) =>
+              val ctype = CType.forJValue(v) getOrElse { sys.error("Cannot determine ctype for " + v + " at " + jpath + " in " + jv) }
+              val ref = ColumnRef(CPath(jpath), ctype)
+
+              val pair: (BitSet, Array[_]) = v match {
+                case JBool(b) => 
+                  val (defined, col) = acc.getOrElse(ref, (new BitSet, new Array[Boolean](yggConfig.maxSliceSize))).asInstanceOf[(BitSet, Array[Boolean])]
+                  col(sliceIndex) = b
+                  (defined + sliceIndex, col)
+                  
+                case JNum(d) => {
+                  val isLong = ctype == CLong
+                  val isDouble = ctype == CDouble
+                  
+                  val (defined, col) = if (isLong) {
+                    val (defined, col) = acc.getOrElse(ref, (new BitSet, new Array[Long](yggConfig.maxSliceSize))).asInstanceOf[(BitSet, Array[Long])]
+                    col(sliceIndex) = d.toLong
+                    (defined, col)
+                  } else if (isDouble) {
+                    val (defined, col) = acc.getOrElse(ref, (new BitSet, new Array[Double](yggConfig.maxSliceSize))).asInstanceOf[(BitSet, Array[Double])]
+                    col(sliceIndex) = d.toDouble
+                    (defined, col)
+                  } else {
+                    val (defined, col) = acc.getOrElse(ref, (new BitSet, new Array[BigDecimal](yggConfig.maxSliceSize))).asInstanceOf[(BitSet, Array[BigDecimal])]
+                    col(sliceIndex) = d
+                    (defined, col)
+                  }
+                  
+                  (defined + sliceIndex, col)
+                }
+
+                case JString(s) => 
+                  val (defined, col) = acc.getOrElse(ref, (new BitSet, new Array[String](yggConfig.maxSliceSize))).asInstanceOf[(BitSet, Array[String])]
+                  col(sliceIndex) = s
+                  (defined + sliceIndex, col)
+                
+                case JArray(Nil)  => 
+                  val (defined, col) = acc.getOrElse(ref, (new BitSet, null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+
+                case JObject(Nil) => 
+                  val (defined, col) = acc.getOrElse(ref, (new BitSet, null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+
+                case JNull        => 
+                  val (defined, col) = acc.getOrElse(ref, (new BitSet, null)).asInstanceOf[(BitSet, Array[Boolean])]
+                  (defined + sliceIndex, col)
+              }
+
+              acc + (ref -> pair)
+          }
+
+          //println("Computed " + withIdsAndValues)
+
+          buildColArrays(from, withIdsAndValues, sliceIndex + 1)
+        } else {
+          (into, sliceIndex)
+        }
+      }
+  
+      // FIXME: If cursor is empty the generated
+      // columns won't satisfy sampleData.schema. This will cause the subsumption test in
+      // Slice#typed to fail unless it allows for vacuous success
+      val slice = new Slice {
+        val (cols, size) = buildColArrays(cursor, Map.empty[ColumnRef, (BitSet, Array[_])], 0) 
+        val columns = cols map {
+          case (ref @ ColumnRef(_, CBoolean), (defined, values))     => (ref, ArrayBoolColumn(defined, values.asInstanceOf[Array[Boolean]]))
+          case (ref @ ColumnRef(_, CLong), (defined, values))        => (ref, ArrayLongColumn(defined, values.asInstanceOf[Array[Long]]))
+          case (ref @ ColumnRef(_, CDouble), (defined, values))      => (ref, ArrayDoubleColumn(defined, values.asInstanceOf[Array[Double]]))
+          case (ref @ ColumnRef(_, CNum), (defined, values))         => (ref, ArrayNumColumn(defined, values.asInstanceOf[Array[BigDecimal]]))
+          case (ref @ ColumnRef(_, CString), (defined, values))      => (ref, ArrayStrColumn(defined, values.asInstanceOf[Array[String]]))
+          case (ref @ ColumnRef(_, CEmptyArray), (defined, values))  => (ref, new BitsetColumn(defined) with EmptyArrayColumn)
+          case (ref @ ColumnRef(_, CEmptyObject), (defined, values)) => (ref, new BitsetColumn(defined) with EmptyObjectColumn)
+          case (ref @ ColumnRef(_, CNull), (defined, values))        => (ref, new BitsetColumn(defined) with NullColumn)
+        }
+      }
+  
+      (slice, if (cursor.hasNext) Some(cursor) else { cursor.close(); None })
     }
   }
 }
