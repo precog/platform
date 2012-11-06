dbc9d212547cff56b2990500695a9970794395b0
Derek Chen-Becker <derek@precog.com>
2012-11-06 06:35:08 -0700

    Initial setup of embedded Jetty server

--- mongo/build.sbt
+++ mongo/build.sbt
@@ -20,8 +20,14 @@
 name := "mongo"
 
 libraryDependencies ++= Seq(
-  "com.reportgrid"          %% "blueeyes-mongo"       % "1.0.0-SNAPSHOT" changing(),
-  "org.mongodb" % "mongo-java-driver"   % "2.9.3"
+  "com.reportgrid"    %% "blueeyes-mongo"   % "1.0.0-SNAPSHOT" changing(),
+  "org.mongodb"       % "mongo-java-driver" % "2.9.3",
+  "org.eclipse.jetty" % "jetty-server"      % "8.1.7.v20120910"
 )
+
+ivyXML := 
+<dependency org="org.eclipse.jetty.orbit" name="javax.servlet" rev="3.0.0.v201112011016">
+<artifact name="javax.servlet" type="orbit" ext="jar"/>
+</dependency>
 
 mainClass := Some("com.precog.shard.mongo.MongoShardServer")
--- mongo/src/main/scala/com/precog/yggdrasil/mongo/MongoShardServer.scala
+++ mongo/src/main/scala/com/precog/yggdrasil/mongo/MongoShardServer.scala
@@ -20,11 +20,20 @@
 package com.precog.shard
 package mongo
 
+import akka.dispatch.Future
+
 import com.precog.common.security._
 import com.precog.ingest.service.NullUsageLogging
 
 import blueeyes.BlueEyesServer
+import blueeyes.core.data.ByteChunk
+import blueeyes.core.http._
+import blueeyes.json.JsonAST.JValue
 import blueeyes.util.Clock
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import org.eclipse.jetty.server.{Handler, Request, Server}
+import org.eclipse.jetty.server.handler.{AbstractHandler, DefaultHandler, HandlerList, ResourceHandler}
 
 import org.streum.configrity.Configuration
 
@@ -35,4 +44,48 @@ object MongoShardServer extends BlueEyesServer with ShardService with MongoQuery
   def usageLoggingFactory(config: Configuration) = new NullUsageLogging("")
 
   val asyncContext = defaultFutureDispatch
+
+  val jettyService = this.service("labcoat", "1.0") { context =>
+    startup {
+      val config = rootConfig.detach("services.quirrel.v1")
+      val serverPort = config[Int]("labcoat.port", 8000)
+      val rootKey = config[String]("security.masterAccount.apiKey")
+
+      val server = new Server(serverPort)
+      val resourceHandler = new ResourceHandler
+      resourceHandler.setDirectoriesListed(false)
+      resourceHandler.setWelcomeFiles(new Array[String](0))
+      resourceHandler.setResourceBase(this.getClass.getClassLoader.getResource("web").toString)
+
+      val rootHandler = new AbstractHandler {
+        def handle(target: String,
+                   baseRequest: Request,
+                   request: HttpServletRequest,
+                   response: HttpServletResponse): Unit = {
+          if (target == "/") {
+            response.sendRedirect("http://localhost:%d/index.html?apiKey=%s&analyticsService=http://localhost:%d/".format(serverPort, rootKey, port))
+          }
+        }
+      }
+
+      val handlers = new HandlerList
+
+      handlers.setHandlers(Array[Handler](rootHandler, resourceHandler, new DefaultHandler))
+      server.setHandler(handlers)
+      server.start()
+
+      Future(server)
+    } -> 
+    request { (server: Server) =>
+      // Nothing to do here, really
+      path("/") {
+        get {
+          (req: HttpRequest[ByteChunk]) => Future { HttpResponse[ByteChunk]() }
+        }
+      }
+    } ->
+    shutdown { (server: Server) =>
+      Future(server.stop())
+    }
+  }
 }
