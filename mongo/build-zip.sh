7d5d6cc7a87006319a30c02c5788b52390134243
Derek Chen-Becker <derek@precog.com>
2012-11-08 12:59:41 -0700

    Use submodule for quirrelide (labcoat) instead of bundling directly for mongo embedded labcoat
    [Fixes #39217711]

--- mongo/build-zip.sh
+++ mongo/build-zip.sh
@@ -33,8 +33,10 @@ rm -rf $TMPDIR/*
 
 java -jar tools/proguard.jar @proguard.conf -injars target/mongo-assembly*.jar -outjars $TMPDIR/precog.jar | tee proguard.log 
 mkdir web
-zip -u $TMPDIR/precog.jar web
-rmdir web
+cp -R quirrelide/build/* web/
+rm -rf web/index.html web/php web/*.php
+zip -ru $TMPDIR/precog.jar web
+rm -rf web
 cp precog.sh precog.bat config.cfg README.md $TMPDIR/
 zip -r precog.zip $TMPDIR
 
