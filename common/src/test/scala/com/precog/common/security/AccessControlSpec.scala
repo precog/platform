de2ba41f68747dc0bb308d7c53bd56fe265a338b
Miles Sabin <miles@milessabin.com>
2012-11-08 01:40:35 +0000

    Removed unused definitions.

--- common/src/test/scala/com/precog/common/security/AccessControlSpec.scala
+++ common/src/test/scala/com/precog/common/security/AccessControlSpec.scala
@@ -169,8 +169,6 @@ class AccessControlSpec extends Specification {
     }
 
     "prevent access via a revoked grant" in {
-      val invalidGrantId = "not-there"
-      
       val userAccountId = "user"
       val userAPIKeyRecord = apiKeyManager.newStandardAPIKeyRecord(userAccountId)
       val userAPIKey = userAPIKeyRecord.apiKey
@@ -202,8 +200,6 @@ class AccessControlSpec extends Specification {
     }
 
     "prevent access via an expired grant" in {
-      val invalidGrantId = "not-there"
-      
       val userAccountId = "user"
       val userAPIKeyRecord = apiKeyManager.newStandardAPIKeyRecord(userAccountId)
       val userAPIKey = userAPIKeyRecord.apiKey
@@ -228,8 +224,6 @@ class AccessControlSpec extends Specification {
     }
 
     "prevent access via a grant with a revoked parent" in {
-      val invalidGrantId = "not-there"
-      
       val user1AccountId = "user1"
       val user1APIKeyRecord = apiKeyManager.newStandardAPIKeyRecord(user1AccountId)
       val user1APIKey = user1APIKeyRecord.apiKey
