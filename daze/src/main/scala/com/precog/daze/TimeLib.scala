79c17104e530ff4413070031ea9d57879511c382
Derek Chen-Becker <derek@precog.com>
2012-11-09 07:46:52 -0700

    Fix isValidISO check in TimeLib to be flexible on format
    [Fixes #39228977]

--- daze/src/main/scala/com/precog/daze/TimeLib.scala
+++ daze/src/main/scala/com/precog/daze/TimeLib.scala
@@ -38,7 +38,6 @@ trait TimeLib[M[+_]] extends GenOpcode[M] {
   private val basicParser = ISODateTimeFormat.basicDateTime
 
   def parseDateTime(value: String, withOffset: Boolean): DateTime = {
-    println("Parsing: " + value)
     val parser = if (value.contains("-") || value.contains(":")) {
       fullParser
     } else {
@@ -108,7 +107,7 @@ trait TimeLib[M[+_]] extends GenOpcode[M] {
   )
 
   private def isValidISO(str: String): Boolean = {
-    try { new DateTime(str); true
+    try { parseDateTime(str, true); true
     } catch {
       case e:IllegalArgumentException => { false }
     }
@@ -557,7 +556,7 @@ trait TimeLib[M[+_]] extends GenOpcode[M] {
 
         def apply(row: Int) = {
           val time = c(row)
-
+          
           val newTime = parseDateTime(time, true)
           fmt.print(newTime)
         }
