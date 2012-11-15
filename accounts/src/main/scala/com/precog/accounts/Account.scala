620fe4dab74c8c683e3a5c0fe7992d8f4efb646e
Kris Nuttycombe <kris@precog.com>
2012-11-14 17:11:31 -0700

    Added hierarchical account administration.

--- accounts/src/main/scala/com/precog/accounts/Account.scala
+++ accounts/src/main/scala/com/precog/accounts/Account.scala
@@ -65,16 +65,17 @@ case class Account(accountId: String,
 
 trait AccountSerialization extends AccountPlanSerialization {
   val UnsafeAccountDecomposer: Decomposer[Account] = new Decomposer[Account] {
-    override def decompose(t: Account): JValue = JObject(List(
-      JField("accountId", t.accountId),
-      JField("email", t.email),
-      JField("passwordHash", t.passwordHash),
-      JField("passwordSalt", t.passwordSalt),
-      JField("accountCreationDate", t.accountCreationDate.serialize),
-      JField("apiKey", t.apiKey),
-      JField("rootPath", t.rootPath),
-      JField("plan", t.plan.serialize)
-      )) 
+    override def decompose(t: Account): JValue = JObject(
+      JField("accountId", t.accountId) ::
+      JField("email", t.email) ::
+      JField("passwordHash", t.passwordHash) ::
+      JField("passwordSalt", t.passwordSalt) ::
+      JField("accountCreationDate", t.accountCreationDate.serialize) ::
+      JField("apiKey", t.apiKey) ::
+      JField("rootPath", t.rootPath) ::
+      JField("plan", t.plan.serialize) ::
+      t.parentId.map(i => JField("parentId", i) :: Nil).getOrElse(Nil)
+    ) 
   }
 
   implicit val AccountDecomposer: Decomposer[Account] = new Decomposer[Account] {
