b833d07a576e53450405992ad853666e0aba237b
Derek Chen-Becker <derek@precog.com>
2012-11-28 13:21:19 -0700

    Capitalization changes for (Account|Grant)ID => \1Id

--- ingest/src/test/scala/com/precog/ingest/service/TestEventService.scala
+++ ingest/src/test/scala/com/precog/ingest/service/TestEventService.scala
@@ -115,7 +115,7 @@ trait TestEventService extends
     contentType: MimeType,
     apiKey: Option[APIKey],
     path: Path,
-    ownerAccountId: Option[AccountID],
+    ownerAccountId: Option[AccountId],
     sync: Boolean = true
   )(data: A)(implicit
     bi: A => Future[JValue],
