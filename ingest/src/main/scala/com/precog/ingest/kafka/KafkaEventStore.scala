eabbb1a382374a488ace02b9033a1668b563b63e
Derek Chen-Becker <derek@precog.com>
2012-11-28 16:32:43 -0700

    PrecogUnitify ingest classes to help avoid accidental swallowing of Futures
    Kris spotted a Future() that was being returned as Unit, thus failing
    to sequence properly

--- ingest/src/main/scala/com/precog/ingest/kafka/KafkaEventStore.scala
+++ ingest/src/main/scala/com/precog/ingest/kafka/KafkaEventStore.scala
@@ -7,6 +7,8 @@ import akka.util.Timeout
 import common._
 import util._
 import ingest.util._
+import com.precog.util.PrecogUnit
+
 
 import java.util.Properties
 import java.util.concurrent.atomic.AtomicInteger
@@ -29,14 +31,14 @@ class KafkaEventStore(router: EventRouter, producerId: Int, firstEventId: Int = 
   def save(action: Action, timeout: Timeout) = {
     val actionId = nextEventId.incrementAndGet
     action match {
-      case event : Event => router.route(EventMessage(producerId, actionId, event)) map { _ => () }
-      case archive : Archive => router.route(ArchiveMessage(producerId, actionId, archive)) map { _ => () }
+      case event : Event => router.route(EventMessage(producerId, actionId, event)) map { _ => PrecogUnit }
+      case archive : Archive => router.route(ArchiveMessage(producerId, actionId, archive)) map { _ => PrecogUnit }
     }
   }
 
-  def start(): Future[Unit] = Promise.successful(())
+  def start(): Future[PrecogUnit] = Promise.successful(PrecogUnit)
 
-  def stop(): Future[Unit] = router.close.map(_ => ())
+  def stop(): Future[PrecogUnit] = router.close.map(_ => PrecogUnit)
 }
 
 class LocalKafkaEventStore(config: Configuration)(implicit dispatcher: MessageDispatcher) extends EventStore with Logging {
@@ -51,7 +53,7 @@ class LocalKafkaEventStore(config: Configuration)(implicit dispatcher: MessageDi
 
   private val producer = new Producer[String, IngestMessage](new ProducerConfig(localProperties))
 
-  def start(): Future[Unit] = Promise.successful(())
+  def start(): Future[PrecogUnit] = Promise.successful(PrecogUnit)
 
   def save(action: Action, timeout: Timeout) = Future {
     val msg = action match {
@@ -60,7 +62,8 @@ class LocalKafkaEventStore(config: Configuration)(implicit dispatcher: MessageDi
     }
     val data = new ProducerData[String, IngestMessage](localTopic, msg)
     producer.send(data)
+    PrecogUnit
   }
 
-  def stop(): Future[Unit] = Future { producer.close } 
+  def stop(): Future[PrecogUnit] = Future { producer.close; PrecogUnit } 
 }
