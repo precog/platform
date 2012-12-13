6042613cf2b97ebdbebca7becdfd5399fa9bd8b7
Daniel Spiewak <daniel@reportgrid.com>
2012-12-13 12:38:55 -0700

    Implemented a special binding for expandGlob
    [Finishes #40850457]

--- daze/src/main/scala/com/precog/daze/Evaluator.scala
+++ daze/src/main/scala/com/precog/daze/Evaluator.scala
@@ -51,6 +51,7 @@ trait Evaluator[M[+_]] extends DAG
     with TableModule[M]        // TODO specific implementation
     with InfixLib[M]
     with UnaryLib[M]
+    with FSLib[M]
     with BigDecimalOperations
     with YggConfigComponent { self =>
 
--- quirrel/src/main/scala/com/precog/quirrel/typer/Binder.scala
+++ quirrel/src/main/scala/com/precog/quirrel/typer/Binder.scala
@@ -10,7 +10,7 @@ trait Binder extends parser.AST with Library {
   type Formal = (Identifier, Let)
   
   protected override lazy val LoadId = Identifier(Vector(), "load")
-  protected override lazy val ExpandPathId = Identifier(Vector("std", "fs"), "expandPath")
+  protected override lazy val ExpandGlobId = Identifier(Vector("std", "fs"), "expandGlob")
   protected override lazy val DistinctId = Identifier(Vector(), "distinct")
 
   override def bindNames(tree: Expr) = {
@@ -160,6 +160,7 @@ trait Binder extends parser.AST with Library {
             case ReductionBinding(_) => 1
             case LoadBinding => 1
             case DistinctBinding => 1
+            case ExpandGlobBinding(_) => 1
             case Morphism1Binding(_) => 1
             case Morphism2Binding(_) => 2
             case Op1Binding(_) => 1
@@ -264,7 +265,7 @@ trait Binder extends parser.AST with Library {
       libReduction.map(ReductionBinding) ++
       libMorphism1.map(Morphism1Binding) ++
       libMorphism2.map(Morphism2Binding) ++
-      Set(LoadBinding, DistinctBinding)
+      Set(LoadBinding, DistinctBinding, ExpandGlobBinding(expandGlob))
       
     val env = Env(Map(), builtIns.map({ b => b.name -> b })(collection.breakOut))
 
@@ -343,6 +344,11 @@ trait Binder extends parser.AST with Library {
   case object LoadBinding extends BuiltInBinding {
     val name = LoadId
     override val toString = "<native: load(1)>"
+  }
+  
+  case class ExpandGlobBinding(mor: Morphism1) extends BuiltInBinding {
+    val name = ExpandGlobId
+    override val toString = "<native: expandGlob(1)>"
   }
 
   case class Morphism1Binding(mor: Morphism1) extends BuiltInBinding {
