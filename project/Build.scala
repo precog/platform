618ad8cf454fd9cc94f1c3fa272e13010ab07195
Derek Chen-Becker <derek@precog.com>
2012-12-12 17:26:46 -0700

    Revert bump so that BlueEyes is using 1.0.0-M6.1

--- project/Build.scala
+++ project/Build.scala
@@ -55,7 +55,7 @@ object PlatformBuild extends Build {
     }
   )
 
-  val blueeyesVersion = "1.0.0-M8"
+  val blueeyesVersion = "1.0.0-M6.1"
 
   val commonSettings = Seq(
     organization := "com.precog",
