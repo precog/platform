0940dad4b07d3622fbadabfb8684bff128f9c19d
Derek Chen-Becker <derek@precog.com>
2013-01-03 10:22:25 -0700

    Reorg specs so that backend-agnostic traits are in muspelheim
    [Fixes #41864221]

--- pandora/src/test/scala/com/precog/pandora/RenderStackSpecs.scala
+++ pandora/src/test/scala/com/precog/pandora/RenderStackSpecs.scala
@@ -20,50 +20,7 @@
 package com.precog
 package pandora
 
-import akka.dispatch.Future
-
-import com.precog.common._
-import com.precog.common.json._
-import com.precog.daze.EvaluationContext
-import com.precog.muspelheim.{EvalStackSpecs, ParseEvalStack}
-import com.precog.yggdrasil.table.ColumnarTableModule
-
-import com.weiglewilczek.slf4s.Logging
-
-import scalaz._
-import scalaz.syntax.copointed._
-
-trait RenderStackSpecs extends EvalStackSpecs 
-    with ParseEvalStack[Future]
-    with ColumnarTableModule[Future]
-    with Logging {
-
-  implicit val M: Monad[Future] with Copointed[Future]
-
-  "full stack rendering" should {
-    def evalTable(str: String, debug: Boolean = false): Table = {
-      import trans._
-      
-      logger.debug("Beginning evaluation of query: " + str)
-      
-      val forest = compile(str) filter { _.errors.isEmpty }
-      forest must haveSize(1)
-      
-      val tree = forest.head
-      tree.errors must beEmpty
-      val Right(dag) = decorate(emit(tree))
-      val tableM = eval(dag, EvaluationContext("dummyAPIKey", Path.Root, new org.joda.time.DateTime()), true)
-      tableM map { _ transform DerefObjectStatic(Leaf(Source), CPathField("value")) } copoint
-    }
-    
-    "render a set of numbers interleaved by delimiters" in {
-      val stream = evalTable("(//tutorial/transactions).quantity") renderJson ','
-      val strings = stream map { _.toString }
-      val str = strings.foldLeft("") { _ + _ } copoint
-      
-      str must contain(",")
-    }
-  }
-}
+import com.precog.muspelheim.RenderStackSpecs
 
 class JDBMRenderStackSpecs extends RenderStackSpecs with JDBMPlatformSpecs
+
--- pandora/src/test/scala/com/precog/pandora/UndefinedLiteralSpecs.scala
+++ pandora/src/test/scala/com/precog/pandora/UndefinedLiteralSpecs.scala
@@ -20,71 +20,7 @@
 package com.precog
 package pandora
 
-import com.precog.muspelheim.EvalStackSpecs
-
-trait UndefinedLiteralSpecs extends EvalStackSpecs {
-  "undefined literals be handled properly in" >> {
-    "binary operation on load with undefined" >> {
-      val input = """
-          medals := //summer_games/london_medals
-          medals.Total + undefined
-        """
-
-      val results = evalE(input)
-
-      results must beEmpty
-    }
-
-    "multiple binary operations on loads with undefined" >> {
-      val input = """
-          medals := //summer_games/london_medals
-          campaigns := //campaigns
-          medals ~ campaigns
-            medals.Total + campaigns.cmp + undefined
-        """
-
-      val results = evalE(input)
-
-      results must beEmpty
-    }
-
-    "intersect load with undefined" >> {
-      val input = """
-          clicks  := //clicks
-          clicks intersect undefined
-        """
-
-      val results = evalE(input)
-
-      results must beEmpty
-    }
-
-    "union load with undefined" >> {
-      val input = """
-          clicks  := //clicks
-          clicks union undefined
-        """
-
-      val results = evalE(input)
-
-      results must not(beEmpty)
-    }
-
-    "multiple union on loads with undefined" >> {
-      val input = """
-          clicks  := //clicks
-          views   := //views
-          clickViews := clicks union views
-
-          clickViews union undefined
-        """
-
-      val results = evalE(input)
-
-      results must not(beEmpty)
-    }
-  }
-}
+import com.precog.muspelheim.UndefinedLiteralSpecs
 
 class JDBMUndefinedLiteralSpecs extends UndefinedLiteralSpecs with JDBMPlatformSpecs
 
