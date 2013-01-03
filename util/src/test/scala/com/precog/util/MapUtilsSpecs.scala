ce214e9febd26bf0eb5d215887eaa70ccbdd97a1
Daniel Spiewak <daniel@precog.com>
2013-01-03 12:34:34 -0700

    Implemented a generic cogroup function which operates on arbitrary maps of traversables

---
+++ util/src/test/scala/com/precog/util/MapUtilsSpecs.scala
@@ -0,0 +1,37 @@
+package com.precog.util
+
+import org.specs2.ScalaCheck
+import org.specs2.mutable._
+import org.scalacheck._
+
+import scalaz.Either3
+
+object MapUtilsSpecs extends Specification with ScalaCheck with MapUtils {
+  import Prop._
+  
+  "cogroup" should {
+    "produce left, right and middle cases" in check { (left: Map[Int, List[Int]], right: Map[Int, List[Int]]) =>
+      val result = left cogroup right
+      
+      val leftKeys = left.keySet -- right.keySet
+      val rightKeys = right.keySet -- left.keySet
+      val middleKeys = left.keySet & right.keySet
+      
+      val leftContrib = leftKeys.toSeq flatMap { key =>
+        left(key) map { Either3.left3[Int, (List[Int], List[Int]), Int](_) }
+      }
+      
+      val rightContrib = rightKeys.toSeq flatMap { key =>
+        right(key) map { Either3.right3[Int, (List[Int], List[Int]), Int](_) }
+      }
+      
+      val middleContrib = middleKeys.toSeq map { key =>
+        Either3.middle3[Int, (List[Int], List[Int]), Int]((left(key), right(key)))
+      }
+      
+      result must containAllOf(leftContrib)
+      result must containAllOf(rightContrib)
+      result must containAllOf(middleContrib)
+    }
+  }
+}
