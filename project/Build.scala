8809e65e13ba753debfe5f11fab4c2c2c68d3209
Derek Chen-Becker <derek@precog.com>
2013-01-12 09:49:56 -0700

    Fixes to RealMongo spec engine

--- project/Build.scala
+++ project/Build.scala
@@ -36,7 +36,7 @@ object PlatformBuild extends Build {
     }
   )
 
-  val blueeyesVersion = "1.0.0-M6.6"
+  val blueeyesVersion = "1.0.0-M6.7"
   val scalazVersion = "7.0-precog-M1"
 
   val commonSettings = Seq(
