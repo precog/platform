5b3a501af9455b3c6e1728746095cd663db3cccc
Kris Nuttycombe <kris@precog.com>
2013-01-15 15:33:18 -0500

    A very temporary, very hacky workaround for being able to retrieve account IDs for api keys that are not directly associated with accounts.
    Revert this commit before nuttycom/wip/batch_ingest is merged.

--- shard/src/main/scala/com/precog/shard/PerAccountThreadPoolModule.scala
+++ shard/src/main/scala/com/precog/shard/PerAccountThreadPoolModule.scala
@@ -19,6 +19,7 @@ import scalaz._
 trait PerAccountThreadPoolModule { self =>
 
   def accountManager: BasicAccountManager[Future]
+  def apiKeyManager: APIKeyManager[Future]
 
   implicit def defaultAsyncContext: ExecutionContext
 
@@ -34,12 +35,15 @@ trait PerAccountThreadPoolModule { self =>
   }
 
   def getAccountExecutionContext(apiKey: APIKey): EitherT[Future, String, ExecutionContext] = {
-    EitherT.eitherT(accountManager.listAccountIds(apiKey) map { accounts =>
-      if (accounts.size < 1) {
-        \/.left("Could not locate accountId for apiKey " + apiKey)
-      } else {
-        \/.right(asyncContextFor(accounts.head)) // FIXME: Which account should we use if there's more than one?
+    EitherT.eitherT(
+      apiKeyManager.rootPath(apiKey) flatMap { keyPath =>
+        accountManager.findControllingAccount(keyPath) map { 
+          case Some(accountId) =>
+            \/.right(asyncContextFor(accountId))
+          case None =>
+            \/.left("Could not locate accountId for apiKey " + apiKey)
+        }
       }
-    })
+    )
   }
 }
