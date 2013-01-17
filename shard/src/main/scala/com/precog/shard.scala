5245cf17b500b14d809ff51a96a804700d2dd175
Kris Nuttycombe <kris@precog.com>
2013-01-17 11:13:46 -0700

    All modules compile again.

--- shard/src/main/scala/com/precog/shard.scala
+++ shard/src/main/scala/com/precog/shard.scala
@@ -12,5 +12,7 @@ package object shard {
   type QueryResult = Either[JValue, StreamT[Future, CharBuffer]]
 
   type JobQueryT[M[+_], +A] = QueryT[JobQueryState, M, A]
+
+  type ShardQuery[+A] = JobQueryT[Future, A]
 }
 
