804215dd7e5ad5d0579ef71ed2559c0868c55ae2
Daniel Spiewak <daniel@precog.com>
2013-01-17 17:03:16 -0700

    Reenabled line information in the DAG
    PLATFORM-674 #finish

--- daze/src/main/scala/com/precog/daze/DAG.scala
+++ daze/src/main/scala/com/precog/daze/DAG.scala
@@ -274,8 +274,7 @@ trait DAG extends Instructions with TransSpecModule {
           }
         }
         
-        // TODO reenable lines
-        case _: Line => loop(loc, roots, splits, stream.tail)
+        case loc: Line => loop(loc, roots, splits, stream.tail)
         
         case instr @ instructions.LoadLocal => {
           continue {
