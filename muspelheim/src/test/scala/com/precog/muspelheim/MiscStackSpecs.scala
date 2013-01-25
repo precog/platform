4cf7478a7adb2312425ffffd0932fb0cf6525ea9
Kris Nuttycombe <kris@precog.com>
2013-01-25 01:32:47 -0500

    Removed stray println

--- muspelheim/src/test/scala/com/precog/muspelheim/MiscStackSpecs.scala
+++ muspelheim/src/test/scala/com/precog/muspelheim/MiscStackSpecs.scala
@@ -1595,7 +1595,7 @@ trait MiscStackSpecs extends EvalStackSpecs {
 
       val resultsE = evalE(input)
 
-      println(resultsE)
+      resultsE must not beEmpty
     }
 
     "determine click times around each click" in {
