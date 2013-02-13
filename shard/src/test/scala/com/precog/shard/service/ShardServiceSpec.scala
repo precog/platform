7218f5943f7b7ec58f7c286c4122e1067ea1e760
Kris Nuttycombe <kris@precog.com>
2013-02-13 15:36:39 -0500

    Changed compiler errors and warnings such that they are represented in a common fashion, reusing the QueryLogger for all error reporting instead of having multiple channels.
    Cleaned up representations substantially in the process.

--- shard/src/test/scala/com/precog/shard/service/ShardServiceSpec.scala
+++ shard/src/test/scala/com/precog/shard/service/ShardServiceSpec.scala
@@ -415,7 +415,7 @@ trait TestPlatform extends ManagedPlatform { self =>
         if(allowed)
           shardQueryMonad.point(success(wrap(JArray(List(JNum(2))))))
         else
-          shardQueryMonad.point(failure(AccessDenied("No data accessable at the specified path")))
+          shardQueryMonad.point(failure(InvalidStateError("No data accessable at the specified path")))
       }
     }
   }
