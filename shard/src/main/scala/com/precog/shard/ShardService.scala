9df23c06f1fe898e3faea374444eaf3a2a20c6e6
Kris Nuttycombe <kris@precog.com>
2013-02-14 10:07:38 -0700

    Removed/reworked a couple of shard service specs that were not testing what they purported to test.

--- shard/src/main/scala/com/precog/shard/ShardService.scala
+++ shard/src/main/scala/com/precog/shard/ShardService.scala
@@ -168,7 +168,7 @@ trait ShardService extends
           } ~
           options {
             (request: HttpRequest[ByteChunk]) => (a: APIKey, p: Path) => optionsResponse 
-          }//): HttpService[ByteChunk, (APIKey, Path) => Future[HttpResponse[ByteChunk]]]
+          }
         }
       }
     }
