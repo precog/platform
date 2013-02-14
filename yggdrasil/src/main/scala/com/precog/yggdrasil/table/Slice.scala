224f6d51d98d92538610aa6afcc21ca921be70af
Erik Osheim <erik@precog.com>
2013-02-13 19:26:00 -0700

    wip: support Slice via SegmentsWrapper

--- yggdrasil/src/main/scala/com/precog/yggdrasil/table/Slice.scala
+++ yggdrasil/src/main/scala/com/precog/yggdrasil/table/Slice.scala
@@ -1551,13 +1551,14 @@ import com.precog.niflheim._
 
 class SegmentsWrapper(segments: Segments) extends Slice {
 
-  private val cols: Map[ColumnRef, Column] = segments.a.map {
-    case segment: ArraySegment[a] => // (_, cpath, ctype, defined, values) =>
-      val cpath: CPath = segment.cpath
+  private def buildColumnRef(seg: Segment) = ColumnRef(seg.cpath, seg.ctype)
+
+  private def buildColumn(seg: Segment): Column = seg match {
+    case segment: ArraySegment[a] =>
       val ctype: CValueType[a] = segment.ctype
       val defined: BitSet = segment.defined
       val values: Array[a] = segment.values
-      val col = ctype match {
+      ctype match {
         case CString => new ArrayStrColumn(defined, values)
         case CDate => new ArrayDateColumn(defined, values)
         case CNum => new ArrayNumColumn(defined, values)
@@ -1566,22 +1567,26 @@ class SegmentsWrapper(segments: Segments) extends Slice {
         case cat: CArrayType[_] => new ArrayHomogeneousArrayColumn(defined, values)(cat)
         case CBoolean => sys.error("impossible")
       }
-      (ColumnRef(cpath, ctype), col)
 
-    case BooleanSegment(_, cpath, defined, values, length) =>
-      (ColumnRef(cpath, CBoolean), new ArrayBoolColumn(defined, values))
+    case BooleanSegment(_, _, defined, values, _) =>
+      new ArrayBoolColumn(defined, values)
 
-    case NullSegment(_, cpath, ctype, defined, length) => ctype match {
+    case NullSegment(_, _, ctype, defined, _) => ctype match {
       case CNull =>
-        (ColumnRef(cpath, ctype), NullColumn(defined))
+        NullColumn(defined)
       case CEmptyObject =>
-        (ColumnRef(cpath, ctype), new MutableEmptyObjectColumn(defined))
+        new MutableEmptyObjectColumn(defined)
       case CEmptyArray =>
-        (ColumnRef(cpath, ctype), new MutableEmptyArrayColumn(defined))
+        new MutableEmptyArrayColumn(defined)
       case CUndefined =>
         sys.error("also impossible")
     }
-  }.toMap
+  }
+
+  private def buildMap(segments: Segments): Map[ColumnRef, Column] =
+    segments.a.map(seg => (buildColumnRef(seg), buildColumn(seg))).toMap
+
+  private val cols: Map[ColumnRef, Column] = buildMap(segments)
 
   def size: Int = segments.length
   def columns: Map[ColumnRef, Column] = cols
