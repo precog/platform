4aa3336a7599f0dcd3ef3c57841892f387733f57
Tom Switzer <switzer@precog.com>
2013-02-20 15:03:15 -0700

    Add ability to perform per-CPath counts.

--- niflheim/src/main/scala/com/precog/niflheim/Segment.scala
+++ niflheim/src/main/scala/com/precog/niflheim/Segment.scala
@@ -24,6 +24,8 @@ import com.precog.common.json._
 import com.precog.util._
 import com.precog.util.BitSetUtil.Implicits._
 
+import scala.{ specialized => spec }
+
 import blueeyes.json._
 
 case class SegmentId(blockid: Long, cpath: CPath, ctype: CType)
@@ -39,7 +41,25 @@ sealed trait Segment {
   def extend(amount: Int): Segment
 }
 
-case class ArraySegment[A](blockid: Long, cpath: CPath, ctype: CValueType[A], defined: BitSet, values: Array[A]) extends Segment {
+sealed trait ValueSegment[@spec(Boolean,Long,Double) A] extends Segment {
+  def ctype: CValueType[A]
+  def map[@spec(Boolean,Long,Double) B: CValueType: Manifest](f: A => B): ValueSegment[B]
+
+  def normalize: ValueSegment[A] = this match {
+    case seg: ArraySegment[_] if seg.ctype == CBoolean =>
+      val values0 = seg.values.asInstanceOf[Array[Boolean]]
+      val values = BitSetUtil.create()
+      defined.foreach { row =>
+        values(row) = values0(row)
+      }
+      BooleanSegment(blockid, cpath, defined, values, values.length).asInstanceOf[ValueSegment[A]]
+
+    case _ =>
+      this
+  }
+}
+
+case class ArraySegment[@spec(Boolean,Long,Double) A](blockid: Long, cpath: CPath, ctype: CValueType[A], defined: BitSet, values: Array[A]) extends ValueSegment[A] {
   private implicit val m = ctype.manifest
 
   override def equals(that: Any): Boolean = that match {
@@ -72,9 +92,17 @@ case class ArraySegment[A](blockid: Long, cpath: CPath, ctype: CValueType[A], de
     while (i < len) { arr(i) = values(i); i += 1 }
     ArraySegment(blockid, cpath, ctype, defined.copy, arr)
   }
+
+  def map[@spec(Boolean,Long,Double) B: CValueType: Manifest](f: A => B): ValueSegment[B] = {
+    val values0 = new Array[B](values.length)
+    defined.foreach { row =>
+      values0(row) = f(values(row))
+    }
+    ArraySegment[B](blockid, cpath, CValueType[B], defined, values0).normalize
+  }
 }
 
-case class BooleanSegment(blockid: Long, cpath: CPath, defined: BitSet, values: BitSet, length: Int) extends Segment {
+case class BooleanSegment(blockid: Long, cpath: CPath, defined: BitSet, values: BitSet, length: Int) extends ValueSegment[Boolean] {
   val ctype = CBoolean
 
   override def equals(that: Any) = that match {
@@ -90,6 +118,14 @@ case class BooleanSegment(blockid: Long, cpath: CPath, defined: BitSet, values: 
   }
 
   def extend(amount: Int) = BooleanSegment(blockid, cpath, defined.copy, values.copy, length + amount)
+
+  def map[@spec(Boolean,Long,Double) B: CValueType: Manifest](f: Boolean => B): ValueSegment[B] = {
+    val values0 = new Array[B](values.length)
+    defined.foreach { row =>
+      values0(row) = f(values(row))
+    }
+    ArraySegment[B](blockid, cpath, CValueType[B], defined, values0).normalize
+  }
 }
 
 case class NullSegment(blockid: Long, cpath: CPath, ctype: CNullType, defined: BitSet, length: Int) extends Segment {
--- niflheim/src/main/scala/com/precog/niflheim/StorageReader.scala
+++ niflheim/src/main/scala/com/precog/niflheim/StorageReader.scala
@@ -21,11 +21,16 @@ package com.precog.niflheim
 
 import com.precog.common.CType
 import com.precog.common.json.CPath
+import com.precog.util._
 
 trait StorageReader {
   def snapshot(pathConstraints: Option[Set[CPath]]): Seq[Segment]
   def structure: Iterable[(CPath, CType)]
 
   def id: Long
+
+  /**
+   * Returns the total length of the block.
+   */
   def length: Int
 }
