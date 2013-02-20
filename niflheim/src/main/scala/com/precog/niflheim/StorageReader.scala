0fba652d0ee6da4dfd0627b81e7aa0fe313074c2
Tom Switzer <switzer@precog.com>
2013-02-20 15:03:15 -0700

    Add ability to perform per-CPath counts.

--- niflheim/src/main/scala/com/precog/niflheim/StorageReader.scala
+++ niflheim/src/main/scala/com/precog/niflheim/StorageReader.scala
@@ -2,11 +2,16 @@ package com.precog.niflheim
 
 import com.precog.common.CType
 import com.precog.common.json.CPath
+import com.precog.util._
 
 trait StorageReader {
   def snapshot(pathConstraints: Option[Set[CPath]]): Seq[Segment]
   def structure: Iterable[(CPath, CType)]
 
   def id: Long
+
+  /**
+   * Returns the total length of the block.
+   */
   def length: Int
 }
