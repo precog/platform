f06f26b00bf6a51446e876978135d453f0edaf48
Erik Osheim <d_m@plastic-idolatry.com>
2013-02-26 17:23:08 -0500

    Add runtime sort info & make sorts memorable.
    This adds runtime sort information to the evaluator, which can be used
    to determine cases where we may have Cross{Left,Right}Sort with a
    reduced table and others, and so our sorted order would still be valid.
    
    Additionally, we also add a new optimization that pushes sorts down
    the dag if their inner node is a Cross{Left,Right}Sort join with a
    constant. This prevents repeated sorts from occuring, when the only
    difference between them is a deref or a wrap.

--- daze/src/main/scala/com/precog/daze/DAG.scala
+++ daze/src/main/scala/com/precog/daze/DAG.scala
@@ -1101,7 +1101,7 @@ trait DAG extends Instructions {
       
       lazy val sorting = joinSort match {
         case tbl: TableSort => tbl
-        case CrossLeftSort => left.sorting
+        case CrossLeftSort => left.sorting // This isn't correct.
         case CrossRightSort => right.sorting
       }
       
@@ -1219,7 +1219,7 @@ trait DAG extends Instructions {
     
     case object IdentitySort extends TableSort
     case class ValueSort(id: Int) extends TableSort
-    case object NullSort extends TableSort
+    // case object NullSort extends TableSort -- Not USED!
     
     case object CrossLeftSort extends JoinSort
     case object CrossRightSort extends JoinSort
