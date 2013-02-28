df912857bb704b2dccf6fa4c380fc7725a2751ed
Erik Osheim <erik@precog.com>
2013-02-27 20:08:41 -0500

    Improve Timing interface.
    This removes the (sketchy) checkpoint timing, and adds an overload
    which allows you to use the thunk result in the log message.

--- util/src/main/scala/com/precog/util/Timing.scala
+++ util/src/main/scala/com/precog/util/Timing.scala
@@ -49,6 +49,14 @@ object Timing {
     result
   }
 
+  def time[A](f: A => String)(thunk: => A): A = {
+    val t0 = System.nanoTime()
+    val result = thunk
+    val t = System.nanoTime() - t0
+    System.err.println("%s took %.2fms" format (f(result), t / m))
+    result
+  }
+
   def timeM[M[+_]: Monad, A](s: String)(ma: => M[A]): M[A] = {
     val t0 = System.nanoTime()
     ma map { a =>
@@ -58,41 +66,12 @@ object Timing {
     }
   }
 
-  /**
-   * Intended to be used like so:
-   * 
-   *   def mycode() {
-   *     Timing.initialize("starting timer")
-   *     val a = doSomething()
-   *     Timing.checkpoint("do something")
-   *     val b = somethingElseWithA(a)
-   *     Timing.checkpoint("something else with a")
-   *     val result = blah(a, b)
-   *     Timing.checkpoint("final result")
-   *     result
-   *   }
-   * 
-   * This would print:
-   * 
-   *   starting timer
-   *   do something took 0.82ms
-   *   something else with a took 13.22ms
-   *   final result took 12.95ms
-   */
-
-  @volatile
-  private var t0: Long = 0L
-
-  def initialize(s: String) {
-    t0 = System.nanoTime()
-    System.err.println(s)
+  def timeM[M[+_]: Monad, A](f: A => String)(ma: => M[A]): M[A] = {
+    val t0 = System.nanoTime()
+    ma map { a =>
+      val t = System.nanoTime() - t0
+      System.err.println("%s took %.2fms" format (f(a), t / m))
+      a
+    }
   }
-  
-  def checkpoint(s: String) {
-    val t1: Long = System.nanoTime()
-    val t: Long = t1 - t0
-    t0 = t1
-    System.err.println("%s took %.2fms" format (s, t / m))
-  }
-
 }
