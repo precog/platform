b4debf4b43b937093da3392461f67f1c956f60cc
Daniel Spiewak <daniel@precog.com>
2013-03-06 16:04:34 -0700

    Implemented Cond rewrite
    PLATFORM-1043 #finish

---
+++ daze/src/main/scala/com/precog/daze/CondRewriter.scala
@@ -0,0 +1,14 @@
+package com.precog
+package daze
+
+trait CondRewriter extends DAG {
+  import instructions._
+  import dag._
+
+  def rewriteConditionals(node: DepGraph): DepGraph = {
+    node mapDown { recurse => {
+      case peer @ IUI(true, Filter(leftJoin, left, pred1), Filter(rightJoin, right, Operate(Comp, pred2))) if pred1 == pred2 => 
+        Cond(recurse(pred1), recurse(left), leftJoin, recurse(right), rightJoin)(peer.loc)
+    }}
+  }
+}
