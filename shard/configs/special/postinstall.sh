61ec406e301c93c3fd1403ba383aaeb2b7abc475
Kris Nuttycombe <kris@precog.com>
2013-03-07 16:30:36 -0500

    Updated config files for V2

--- shard/configs/special/postinstall.sh
+++ shard/configs/special/postinstall.sh
@@ -6,19 +6,19 @@ set -e
 initctl reload-configuration
 
 # Keep monit from interrupting us
-if ! monit unmonitor -g shard-v1; then
-    echo "Monit unhappy on unmonitor of shard-v1"
+if ! monit unmonitor -g shard-v2; then
+    echo "Monit unhappy on unmonitor of shard-v2"
 fi
 
 
 # Stop and start the service
-if status shard-v1 | grep running; then
-    stop shard-v1
+if status shard-v2 | grep running; then
+    stop shard-v2
 fi
 
 sleep 5
 
-if ! RESULT=`start shard-v1 2>&1` > /dev/null ; then
+if ! RESULT=`start shard-v2 2>&1` > /dev/null ; then
     if echo "$RESULT" | grep -v "already running" > /dev/null ; then
 	echo "Failure: $RESULT"
 	exit 1
@@ -32,15 +32,15 @@ fi
 
 sleep 5
 
-if ! monit monitor -g shard-v1; then
-    echo "Monit unhappy on remonitor of shard-v1"
+if ! monit monitor -g shard-v2; then
+    echo "Monit unhappy on remonitor of shard-v2"
 fi
 
 # Wait 30 seconds for startup, then test the health URLs
 sleep 30
 
 echo "Running health checks"
-curl -v -f -G "http://localhost:30070/blueeyes/services/quirrel/v1/health"
+curl -v -f -G "http://localhost:30070/blueeyes/services/analytics/v2/health"
 echo "Completed health checks"
 
 exit 0
