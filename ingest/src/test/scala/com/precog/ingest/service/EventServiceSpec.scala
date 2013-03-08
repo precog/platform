35978079636c366088e6214b339ec1b1407b2e3f
Tom Switzer <switzer@precog.com>
2013-03-08 14:31:44 -0700

    CSV parsing changes: flat objects with duplicate headers.
    This makes 2 changes to how we parse CSV. First, CSVs are considered
    flat objects. Headers are wrapped in JPathFields now, rather than
    treating them as arbitrary JPaths. Second, duplicate headers indicate
    "arrays". So, `a,a,a\n1,2,3` will be parsed into `{ a: [1,2,3] }`.
    
    PLATFORM-1046 #finish

--- ingest/src/test/scala/com/precog/ingest/service/EventServiceSpec.scala
+++ ingest/src/test/scala/com/precog/ingest/service/EventServiceSpec.scala
@@ -135,6 +135,30 @@ class EventServiceSpec extends TestEventService with AkkaConversions with com.pr
             JParser.parseUnsafe("""{ "a": 6, "b": 7, "c": "8" }"""))
       }
     }
+
+    "handle CSVs with duplicate headers" in {
+      val data = """URL,Title,Status,HubScore,Comments,24 Hours,7 Days,30 Days,Total,24 Hours,Total,Published Date,Edited Date,Featured
+                   |http://alexk2009.hubpages.com/hub/Big-Birds-that-carry-off-children,Eagles carrying off children and babies,Published,91,21,11,98,2352,10856,0,252,11/05/11,12/19/12,yes
+                   |http://alexk2009.hubpages.com/hub/Creating-Spirits,Creating Spirits and magical astral and physical thought forms,Published,88,0,1,15,58,1076,0,0,05/15/09,01/12/13,yes
+                   |http://alexk2009.hubpages.com/hub/The-Illusion-of-Money-part-one,The illusion of money,Published,88,6,0,5,32,708,0,0,04/02/10,01/13/13,yes""".stripMargin
+
+      val result = track(CSV, Some(testAccount.apiKey), testAccount.rootPath, Some(testAccount.accountId), sync = true, batch = true) {
+        chunk(data)
+      }
+
+      result.copoint must beLike {
+        case (HttpResponse(HttpStatus(OK, _), _, Some(_), _), events) =>
+          events flatMap { _.data.map(v => JParser.parseUnsafe(v.renderCompact)) } must contain(
+            JParser.parseUnsafe("""{
+              "URL": "http://alexk2009.hubpages.com/hub/Big-Birds-that-carry-off-children",
+              "Title": "Eagles carrying off children and babies", "Status": "Published",
+              "24 Hours": [ 11, 0 ], "Total": [ 10856, 252 ],
+              "HubScore": 91, "Comments": 21, "7 Days": 98, "30 Days": 2352,
+              "Published Date": "11/05/11", "Edited Date": "12/19/12", "Featured": "yes"
+            }""")
+          )
+      }
+    }
     
     "reject track request when API key not found" in {
       val result = track(JSON, Some("not gonna find it"), testAccount.rootPath, Some(testAccount.accountId))(testValue) 
