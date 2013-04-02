06c73ed17bf67edd588262927779de30af04f8df
Kris Nuttycombe <kris@precog.com>
2013-04-02 14:18:08 -0400

    Changed blueeyes version to 1.0.0-M8.4 in hopes that the health check failure on Jenkins is bad-blueeyes-related.

--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ object PlatformBuild extends Build {
     }
   )
 
-  val blueeyesVersion = "1.0.0-SNAPSHOT"
+  val blueeyesVersion = "1.0.0-M8.4"
   val scalazVersion = "7.0.0-M8"
 
   val commonSettings = Seq(
