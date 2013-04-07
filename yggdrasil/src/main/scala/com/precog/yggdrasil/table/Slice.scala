4f1041831eb12d7967bb5a2662ab36c99431803f
Derek Chen-Becker <derek@precog.com>
2013-04-07 11:01:33 -0600

    Fixes a race  condition in RawHandler leading to erroneous results
    PLATFORM-1156 #finish
    
    Also changed synchronization from a volatile var to a dedicated lock
    object and added more logging throughout. Updated the quirrelide
    submodule to point at the new GitHub repo (precog vs reportgrid)

--- yggdrasil/src/main/scala/com/precog/yggdrasil/table/Slice.scala
+++ yggdrasil/src/main/scala/com/precog/yggdrasil/table/Slice.scala
@@ -21,6 +21,8 @@ import org.joda.time.{DateTime, Period}
 import com.precog.util.{BitSet, BitSetUtil, Loop}
 import com.precog.util.BitSetUtil.Implicits._
 
+import com.weiglewilczek.slf4s.Logging
+
 import scala.annotation.{switch, tailrec}
 
 import scala.collection.{breakOut, mutable}
@@ -1664,7 +1666,7 @@ object Slice {
 
 import com.precog.niflheim._
 
-case class SegmentsWrapper(segments: Seq[Segment], projectionId: Int, blockId: Long) extends Slice {
+case class SegmentsWrapper(segments: Seq[Segment], projectionId: Int, blockId: Long) extends Slice with Logging {
   import TransSpecModule.paths
 
   // FIXME: This should use an identity of Array[Long](projectionId,
@@ -1721,6 +1723,13 @@ case class SegmentsWrapper(segments: Seq[Segment], projectionId: Int, blockId: L
 
   private val cols: Map[ColumnRef, Column] = buildMap(segments) + buildKeyColumn(segments.headOption map (_.length) getOrElse 0)
 
-  val size: Int = segments.foldLeft(0)(_ max _.length)
+  val size: Int = {
+    val sz = segments.foldLeft(0)(_ max _.length)
+    if (logger.isTraceEnabled) {
+      logger.trace("Computed size %d from:\n  %s".format(sz, segments.mkString("\n  ")))
+    }
+    sz
+  }
+
   def columns: Map[ColumnRef, Column] = cols
 }
