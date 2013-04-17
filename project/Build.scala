290595eb5b084caf16ab27a4753b45f15b1070a0
Kris Nuttycombe <kris@precog.com>
2013-04-17 18:49:53 -0400

    Updates blueeyes to M8.6

--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ object PlatformBuild extends Build {
     }
   )
 
-  val blueeyesVersion = "1.0.0-M8.5"
+  val blueeyesVersion = "1.0.0-M8.6"
   val scalazVersion = "7.0.0-RC1"
 
   val commonSettings = Seq(
