be0a3ee8a346cffdc215ef5ff38b8ff04f593b9e
Alissa Pajer <alissa@precog.com>
2013-04-24 19:34:28 -0400

    Generalizes join optimization.
    PLATFORM-1230 #finish
    PLATFORM-1210 #finish

--- daze/src/test/scala/com/precog/daze/EvaluatorSpecs.scala
+++ daze/src/test/scala/com/precog/daze/EvaluatorSpecs.scala
@@ -3618,7 +3618,6 @@ trait EvaluatorSpecs[M[+_]] extends Specification
         left,
         Const(CLong(first))(line))(line),
       Const(CLong(second))(line))(line)
-
 }
 
 object EvaluatorSpecs extends EvaluatorSpecs[YId] with test.YIdInstances 
