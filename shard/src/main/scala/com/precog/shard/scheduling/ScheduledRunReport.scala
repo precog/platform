f456ea1fbd968d2984cc9900f54222832b78e987
Derek Chen-Becker <derek@precog.com>
2013-05-07 18:16:28 -0400

    Added Mongo storage for scheduled tasks

---
+++ shard/src/main/scala/com/precog/shard/scheduling/ScheduledRunReport.scala
@@ -0,0 +1,28 @@
+package com.precog.shard
+package scheduling
+
+import blueeyes.json._
+import blueeyes.json.serialization._
+import blueeyes.json.serialization.Versioned._
+import blueeyes.json.serialization.DefaultSerialization._
+
+import java.util.UUID
+
+import org.joda.time.DateTime
+
+import scalaz._
+
+import shapeless._
+
+object ScheduledRunReport {
+  import com.precog.common.ingest.JavaSerialization._
+
+  implicit val iso = Iso.hlist(ScheduledRunReport.apply _, ScheduledRunReport.unapply _)
+
+  val schemaV1 = "id" :: "startedAt" :: "endedAt" :: "records" :: "messages" :: HNil
+
+  implicit val decomposer = decomposerV[ScheduledRunReport](schemaV1, Some("1.0".v))
+  implicit val extractor  = extractorV[ScheduledRunReport](schemaV1, Some("1.0".v))
+}
+
+case class ScheduledRunReport(id: UUID, startedAt: DateTime, endedAt: DateTime, records: Long, messages: List[String] = Nil)
