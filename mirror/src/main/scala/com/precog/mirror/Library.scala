8887712f3399dec35b5d773ad86141d6c056f4ba
Daniel Spiewak <daniel@precog.com>
2013-05-14 11:11:09 +0200

    Initial (mostly stubbed) implementation of an in-memory evaluator

---
+++ mirror/src/main/scala/com/precog/mirror/Library.scala
@@ -0,0 +1,31 @@
+package com.precog
+package mirror
+
+import bytecode._
+import quirrel._
+import quirrel.typer.Binder
+
+trait LibraryModule extends Binder {
+  
+  // TODO do something interesting here
+  class Lib extends Library {
+    type Morphism1 = Morphism1Like
+    type Morphism2 = Morphism2Like
+    type Op1 = Op1Like with Morphism1
+    type Op2 = Op2Like
+    type Reduction = ReductionLike with Morphism1
+
+    def libMorphism1 = Set()
+    def libMorphism2 = Set()
+    def lib1 = Set()
+    def lib2 = Set()
+    def libReduction = Set()
+    
+    lazy val expandGlob = new Morphism1Like {
+      val namespace = Vector("std", "fs")
+      val name = "expandGlob"
+      val opcode = 0x0001
+      val tpe = UnaryOperationType(JType.JUniverseT, JType.JUniverseT)
+    }
+  }
+}
