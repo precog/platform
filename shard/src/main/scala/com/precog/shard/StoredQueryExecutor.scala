5d7c170998575bd7e688915516aaaad67f832d75
Derek Chen-Becker <derek@precog.com>
2013-05-16 20:08:08 -0400

    Properly deref values from cached NIHDBProjections
    Not sure if it would be better to do this elsewhere, or with a
    different approach

--- shard/src/main/scala/com/precog/shard/StoredQueryExecutor.scala
+++ shard/src/main/scala/com/precog/shard/StoredQueryExecutor.scala
@@ -27,6 +27,7 @@ import com.precog.common.security._
 import com.precog.common.jobs._
 import com.precog.muspelheim._
 import com.precog.yggdrasil.table.Slice
+import com.precog.yggdrasil.TransSpecModule
 import com.precog.yggdrasil.vfs._
 import com.precog.util.InstantOrdering
 
@@ -46,6 +47,8 @@ import com.weiglewilczek.slf4s.Logging
 import java.nio.CharBuffer
 import java.io.{ StringWriter, PrintWriter }
 
+import org.joda.time.Instant
+
 import scala.math.Ordered._
 import scalaz._
 import scalaz.NonEmptyList.nels
@@ -60,27 +63,30 @@ import scalaz.syntax.traverse._
 import scalaz.syntax.std.boolean._
 import scalaz.syntax.std.option._
 
+case class StoredQueryResult[M[+_]](data: StreamT[M, Slice], cachedAt: Option[Instant])
+
 trait StoredQueries[M[+_]] {
-  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StreamT[M, Slice]]]
+  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StoredQueryResult[M]]]
 }
 
 object VFSStoredQueries {
   def cachePath(path: Path) = path / Path(".cached")
 }
 
-class VFSStoredQueries[M[+_]: Monad](platform: Platform[M, StreamT[M, Slice]], vfs: VFS[M], scheduler: Scheduler[M], jobManager: JobManager[M], permissionsFinder: PermissionsFinder[M], clock: Clock) extends StoredQueries[M] {
+class VFSStoredQueries[M[+_]: Monad](platform: Platform[M, StreamT[M, Slice]], vfs: VFS[M], scheduler: Scheduler[M], jobManager: JobManager[M], permissionsFinder: PermissionsFinder[M], clock: Clock) extends StoredQueries[M] with Logging {
   import VFSStoredQueries._
 
-  private def executeUncached(apiKey: APIKey, path: Path, queryOptions: QueryOptions, cacheAt: Option[Path]): M[Validation[EvaluationError, StreamT[M, Slice]]] = {
-    for { 
+  private def executeUncached(apiKey: APIKey, path: Path, queryOptions: QueryOptions, cacheAt: Option[Path]): M[Validation[EvaluationError, StoredQueryResult[M]]] = {
+    logger.debug("Executing query for %s and caching to %s".format(path, cacheAt))
+    for {
       executorV <- platform.executorFor(apiKey)
       // TODO: Check read permissions
       queryOpt <- vfs.readQuery(path, Version.Current)
       queryV = queryOpt.toSuccess(nels("Query not found at %s for %s".format(path.path, apiKey)))
       basePathV = path.prefix.toSuccess(nels("Path %s cannot be relativized.".format(path.path)))
-      resultV <- (queryV tuple basePathV tuple executorV.toValidationNel) traverse { 
-        case ((query, basePath), executor) => executor.execute(apiKey, query, basePath, queryOptions) 
-      } 
+      resultV <- (queryV tuple basePathV tuple executorV.toValidationNel) traverse {
+        case ((query, basePath), executor) => executor.execute(apiKey, query, basePath, queryOptions)
+      }
       resultV0 = resultV.leftMap(e => EvaluationError.acc(e map { InvalidStateError(_)})).flatMap(a => a)
       cachingV <- cacheAt map { cachedPath =>
         for {
@@ -93,20 +99,33 @@ class VFSStoredQueries[M[+_]: Monad](platform: Platform[M, StreamT[M, Slice]], v
           for {
             writeAs <- writeAsV.leftMap(InvalidStateError(_))
             stream <- resultV0
-          } yield { 
-            vfs.persistingStream(apiKey, cachedPath, writeAs, perms.toSet[Permission], Some(job.id), stream) 
+          } yield {
+            vfs.persistingStream(apiKey, cachedPath, writeAs, perms.toSet[Permission], Some(job.id), stream)
           }
         }
       } getOrElse {
         resultV0.point[M]
       }
-    } yield cachingV
+    } yield cachingV.map(StoredQueryResult(_, None))
   }
 
-  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StreamT[M, Slice]]] = {
+  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StoredQueryResult[M]]] = {
     val cachedPath = cachePath(path)
+
+    def fallBack = if (onlyIfCached) {
+      // Return a 202 and schedule a caching run of the query
+      sys.error("todo")
+    } else {
+      // if current cached version is older than the max age or cached
+      // version does not exist, then run synchronously and cache (if cacheable)
+      executeUncached(apiKey, path, queryOptions, cacheable.option(cachedPath))
+    }
+
+    logger.debug("Checking on cached result for %s with maxAge = %s and recacheAfter = %s and cacheable = %s".format(path, maxAge, recacheAfter, cacheable))
+
     platform.metadataClient.currentVersion(apiKey, cachedPath) flatMap {
       case Some(VersionEntry(id, _, timestamp)) if maxAge.forall(ms => timestamp.plus(ms) >= clock.instant()) =>
+        logger.debug("Found fresh cache entry (%s) for query on %s".format(timestamp, path))
         if (recacheAfter.exists(ms => timestamp.plus(ms) < clock.instant())) {
           // if recacheAfter has expired since the head version was cached,
           // then return the cached version and refresh the cache
@@ -114,36 +133,42 @@ class VFSStoredQueries[M[+_]: Monad](platform: Platform[M, StreamT[M, Slice]], v
             writeAsOpt <- platform.metadataClient.currentAuthorities(apiKey, path)
             writeAsV = writeAsOpt.toSuccess(nels("Unable to determine write authorities for caching of %s".format(path.path)))
             basePathV = path.prefix.toSuccess(nels("Path %s cannot be relativized.".format(path.path)))
-            taskIdV <- (writeAsV tuple basePathV) traverse { 
+            taskIdV <- (writeAsV tuple basePathV) traverse {
               case (writeAs, basePath) =>
                 scheduler.addTask(None, apiKey, writeAs, basePath, path, cachedPath, None).map(_.toValidationNel)
-            } 
+            }
             currentV <- vfs.readCache(cachedPath, Version.Current) map {
               _.toSuccess(nels("Could not find cached data for path %s.".format(cachedPath)))
             }
           } yield {
-            (taskIdV.flatMap(a => a) |@| currentV) { (_, stream) => stream } leftMap { errors =>
+            // FIXME: what should we be doing with the task ID here? Return in a header?
+            (taskIdV.flatMap(a => a) |@| currentV) { (_, stream) =>
+              // FIXME: because we're getting data back from
+              // NIHDBProjection, it contains the identities. Is there
+              // a better way/place to do this deref?
+              StoredQueryResult(stream.map(_.deref(TransSpecModule.paths.Value)), Some(timestamp))
+            } leftMap { errors =>
               EvaluationError.systemError {
-                new RuntimeException("Errors occurrd running previously cached query: " + errors.shows)
+                new RuntimeException("Errors occurred running previously cached query: " + errors.shows)
               }
             }
           }
         } else {
           // simply return the cached results
           vfs.readCache(cachedPath, Version.Current) map {
-            _.toSuccess(InvalidStateError("Could not find cached data for path %s.".format(cachedPath)))
+            _.map { stream =>
+              StoredQueryResult(stream.map(_.deref(TransSpecModule.paths.Value)), Some(timestamp))
+            }.toSuccess(InvalidStateError("Could not find cached data for path %s.".format(cachedPath)))
           }
         }
-         
-      case _ => 
-        if (onlyIfCached) {
-          // Return a 202 and schedule a caching run of the query
-          sys.error("todo")
-        } else {
-          // if current cached version is older than the max age or cached
-          // version does not exist, then run synchronously and cache (if cacheable)
-          executeUncached(apiKey, path, queryOptions, cacheable.option(cachedPath))
-        }
+
+      case Some(VersionEntry(_, _, timestamp)) =>
+        logger.debug("Cached entry (%s) found for %s, but is not applicable to max-age %s".format(timestamp, path, maxAge))
+        fallBack
+
+      case _ =>
+        logger.debug("No cached entry found for " + path)
+        fallBack
     }
   }
 }
@@ -154,5 +179,5 @@ object NoopStoredQueries {
 }
 
 class NoopStoredQueries[M[+_]: Monad] extends StoredQueries[M] {
-  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StreamT[M, Slice]]] = sys.error("Stored query execution is not supported.")
+  def executeStoredQuery(apiKey: APIKey, path: Path, queryOptions: QueryOptions, maxAge: Option[Long], recacheAfter: Option[Long], cacheable: Boolean, onlyIfCached: Boolean): M[Validation[EvaluationError, StoredQueryResult[M]]] = sys.error("Stored query execution is not supported.")
 }
