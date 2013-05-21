2645f52b4c22c17c39c46b97ed0016dbee5b44cb
Daniel Spiewak <daniel@precog.com>
2013-05-21 15:47:11 -0600

    Fixed broken CScanner (merge artifact)

--- daze/src/main/scala/com/precog/daze/StringLib.scala
+++ daze/src/main/scala/com/precog/daze/StringLib.scala
@@ -425,78 +425,76 @@ trait StringLibModule[M[+_]] extends ColumnarTableLibModule[M] {
       }
     }
     
-    def splitScanner(quote: Boolean): CScanner[M] = new CScanner[M] {
+    def splitScanner(quote: Boolean): CScanner = new CScanner {
       type A = Unit
       
       def init = ()
       
-      def scan(a: Unit, columns: Map[ColumnRef, Column], range: Range): M[(A, Map[ColumnRef, Column])] = {
-        M point {
-          val leftM: Option[StrColumn] = columns collect {
-            case (ColumnRef(CPath(CPathIndex(0)), CString), col: StrColumn) => col
-          } headOption
-          
-          val rightM: Option[StrColumn] = columns collect {
-            case (ColumnRef(CPath(CPathIndex(1)), CString), col: StrColumn) => col
-          } headOption
-          
-          val zippedM = implicitly[Applicative[Option]].apply2(leftM, rightM) { (_, _) }
-          
-          val columnsM: Option[Map[ColumnRef, Column]] = zippedM flatMap {
-            case (left, right) => {
-              val result = new Array[Array[String]](range.length)
-              val defined = new BitSet(range.length)
-              
-              RangeUtil.loop(range) { row =>
-                if (left.isDefinedAt(row) && right.isDefinedAt(row)) {
-                  try {
-                    val pattern = if (quote) Pattern.quote(right(row)) else right(row)
+      def scan(a: Unit, columns: Map[ColumnRef, Column], range: Range): (A, Map[ColumnRef, Column]) = {
+        val leftM: Option[StrColumn] = columns collect {
+          case (ColumnRef(CPath(CPathIndex(0)), CString), col: StrColumn) => col
+        } headOption
+        
+        val rightM: Option[StrColumn] = columns collect {
+          case (ColumnRef(CPath(CPathIndex(1)), CString), col: StrColumn) => col
+        } headOption
+        
+        val zippedM = implicitly[Applicative[Option]].apply2(leftM, rightM) { (_, _) }
+        
+        val columnsM: Option[Map[ColumnRef, Column]] = zippedM flatMap {
+          case (left, right) => {
+            val result = new Array[Array[String]](range.length)
+            val defined = new BitSet(range.length)
+            
+            RangeUtil.loop(range) { row =>
+              if (left.isDefinedAt(row) && right.isDefinedAt(row)) {
+                try {
+                  val pattern = if (quote) Pattern.quote(right(row)) else right(row)
+                  
+                  // TOOD cache compiled patterns for awesome sauce
+                  result(row) =
+                    Pattern.compile(pattern).split(left(row), -1)
                     
-                    // TOOD cache compiled patterns for awesome sauce
-                    result(row) =
-                      Pattern.compile(pattern).split(left(row), -1)
-                      
-                    defined.flip(row)
-                  } catch {
-                    case _: PatternSyntaxException =>
-                  }
-                }
-              }
-              
-              if (defined.nextSetBit(0) < 0) {      // not defined anywhere
-                None
-              } else {
-                var maxIdx = -1
-                RangeUtil.loop(range) { row =>
-                  if (defined.get(row)) {
-                    maxIdx = maxIdx max (result(row).length - 1)
-                  }
-                }
-                
-                if (maxIdx < 0) {
-                  None
-                } else {
-                  val columnArray = new Array[StrColumn](maxIdx + 1)
-                  
-                  RangeUtil.loop(0 until columnArray.length) { i =>
-                    columnArray(i) = new StrColumn {
-                      def isDefinedAt(row: Int) = defined.get(row) && i < result(row).length
-                      def apply(row: Int) = result(row)(i)
-                    }
-                  }
-                  
-                  val columns: Map[ColumnRef, Column] = (0 until columnArray.length).map({ i =>
-                    ColumnRef(CPath(CPathIndex(i)), CString) -> columnArray(i)
-                  })(collection.breakOut)
-                  
-                  Some(columns)
+                  defined.flip(row)
+                } catch {
+                  case _: PatternSyntaxException =>
                 }
               }
             }
+            
+            if (defined.nextSetBit(0) < 0) {      // not defined anywhere
+              None
+            } else {
+              var maxIdx = -1
+              RangeUtil.loop(range) { row =>
+                if (defined.get(row)) {
+                  maxIdx = maxIdx max (result(row).length - 1)
+                }
+              }
+              
+              if (maxIdx < 0) {
+                None
+              } else {
+                val columnArray = new Array[StrColumn](maxIdx + 1)
+                
+                RangeUtil.loop(0 until columnArray.length) { i =>
+                  columnArray(i) = new StrColumn {
+                    def isDefinedAt(row: Int) = defined.get(row) && i < result(row).length
+                    def apply(row: Int) = result(row)(i)
+                  }
+                }
+                
+                val columns: Map[ColumnRef, Column] = (0 until columnArray.length).map({ i =>
+                  ColumnRef(CPath(CPathIndex(i)), CString) -> columnArray(i)
+                })(collection.breakOut)
+                
+                Some(columns)
+              }
+            }
           }
-          
-          ((), columnsM getOrElse Map())
         }
+        
+        ((), columnsM getOrElse Map())
       }
     }
     
