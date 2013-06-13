ebcbc1eb7afe4531c1c9469ec7a91797fd286f94
Daniel C. Sobral <dcsobral@precog.com>
2013-06-13 15:46:49 -0400

    Fix dev postinstall.sh script
    The script for shard's dev and dev-b, as opposed to all other
    postinstall.sh, did a chown -R on nihdb's root, which took a long
    time to complete.
    
    Also, fix a few other inefficiencies handling monit, making this
    script identical to the one used on special.

--- shard/configs/dev-b/postinstall.sh
+++ shard/configs/dev-b/postinstall.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Ensure data directory and ownership
-mkdir -p /opt/precog/shard/data
-mkdir -p /opt/precog/shard/scratch
-chown -R precog /opt/precog/shard/
-
 set -e
 
 # Reload the upstart config
 initctl reload-configuration
 
 # Keep monit from interrupting us
-if /etc/init.d/monit stop; then
-	echo "Monit stopped unhappy"
+if ! monit unmonitor -g shard-v2-b; then
+    echo "Monit unhappy on unmonitor of shard-v2-b"
 fi
+
 
 # Stop and start the service
 if status shard-v2-b | grep running; then
@@ -23,23 +19,28 @@ fi
 sleep 5
 
 if ! RESULT=`start shard-v2-b 2>&1` > /dev/null ; then
-        if echo "$RESULT" | grep -v "already running" > /dev/null ; then
-		echo "Failure: $RESULT"
-		exit 1
-        fi
-fi
-
+    if echo "$RESULT" | grep -v "already running" > /dev/null ; then
+	echo "Failure: $RESULT"
+	exit 1
+    fi
+    fi
 
 # Restart monit to pick up changes
-if /etc/init.d/monit start; then
-	echo "Monit started unhappy";
+if ! monit reload; then
+    echo "Monit unhappy on reload"
 fi
 
-# Wait 30 seconds for startup, then test the health URL
+sleep 5
+
+if ! monit monitor -g shard-v2-b; then
+    echo "Monit unhappy on remonitor of shard-v2-b"
+fi
+
+# Wait 30 seconds for startup, then test the health URLs
 sleep 30
 
-echo "Checking health"
-#curl -v -f -G "http://localhost:31070/analytics/v2/health"
-echo "Completed health check"
+echo "Running health checks"
+curl -v -f -G "http://localhost:31070/analytics/v2/health"
+echo "Completed health checks"
 
 exit 0
--- shard/configs/dev/postinstall.sh
+++ shard/configs/dev/postinstall.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Ensure data directory and ownership
-mkdir -p /opt/precog/shard/data
-mkdir -p /opt/precog/shard/scratch
-chown -R precog /opt/precog/shard/
-
 set -e
 
 # Reload the upstart config
 initctl reload-configuration
 
 # Keep monit from interrupting us
-if /etc/init.d/monit stop; then
-	echo "Monit stopped unhappy"
+if ! monit unmonitor -g shard-v2; then
+	echo "Monit unhappy on unmonitor of shard-v2"
 fi
 
 # Stop and start the service
@@ -31,15 +26,22 @@ fi
 
 
 # Restart monit to pick up changes
-if /etc/init.d/monit start; then
-	echo "Monit started unhappy";
+if ! monit reload; then
+	echo "Monit unhappy on reload"
 fi
 
-# Wait 30 seconds for startup, then test the health URL
+sleep 5
+
+if ! monit monitor -g shard-v2; then
+    echo "Monit unhappy on remonitor of shard-v2"
+fi
+
+
+# Wait 30 seconds for startup, then test the health URLs
 sleep 30
 
-echo "Checking health"
-#curl -v -f -G "http://localhost:30070/analytics/v2/health"
-echo "Completed health check"
+echo "Running health checks"
+curl -v -f -G "http://localhost:30070/analytics/v2/health"
+echo "Completed health checks"
 
 exit 0
